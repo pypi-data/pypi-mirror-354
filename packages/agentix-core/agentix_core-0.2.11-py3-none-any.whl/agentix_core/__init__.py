05f8b8fffec161f5af728d3e060664a8bd6a537d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:43 +0000

    Add package agentix-core/0.2.11/agentix_core-0.2.11-py3-none-any.whl

---
+++ packages/agentix-core/agentix_core-0.2.11-py3-none-any.whl/agentix_core/__init__.py
@@ -0,0 +1,8 @@
+from .v1 import Core
+
+__all__ = ["Core"]
+
+# # Dynamically read version from installed package metadata
+# from importlib.metadata import version
+
+# __version__ = version("agentix-core")
