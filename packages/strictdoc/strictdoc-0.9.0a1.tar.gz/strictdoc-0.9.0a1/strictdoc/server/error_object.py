37ca4eda9f4aadea57b3484681f2aee0ae332feb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:15 +0000

    Add package strictdoc/0.9.0a1/strictdoc-0.9.0a1.tar.gz

---
+++ packages/strictdoc/strictdoc-0.9.0a1.tar.gz/strictdoc-0.9.0a1/strictdoc/server/error_object.py
@@ -0,0 +1,16 @@
+# mypy: disable-error-code="no-untyped-def"
+class ErrorObject:
+    def __init__(self):
+        self.errors = {}
+
+    def any_errors(self):
+        return len(self.errors)
+
+    def get_errors(self, field_name):
+        if field_name not in self.errors:
+            return []
+        return self.errors[field_name]
+
+    def add_error(self, field_name, error):
+        error_container = self.errors.setdefault(field_name, [])
+        error_container.append(error)
