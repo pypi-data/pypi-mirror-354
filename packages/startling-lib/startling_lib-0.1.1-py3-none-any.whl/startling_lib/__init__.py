1167b5748febf0ef1b7d2fe8fff3cf726cd768f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:09 +0000

    Add package startling-lib/0.1.1/startling_lib-0.1.1-py3-none-any.whl

---
+++ packages/startling-lib/startling_lib-0.1.1-py3-none-any.whl/startling_lib/__init__.py
@@ -0,0 +1,11 @@
+from startling_lib.competence import Competence, Personal
+from startling_lib.engineering import *
+from startling_lib.cv import *
+
+__all__ = [
+    "Competence",
+    "cv",
+    "engineering",
+    "Personal"
+    ]
+
