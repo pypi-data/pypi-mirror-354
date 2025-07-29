107a49c426e2feb82897a142df247227f36e3d30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:25 +0000

    Add package gologin/2025.6.9102915/gologin-2025.6.9102915.tar.gz

---
+++ packages/gologin/gologin-2025.6.9102915.tar.gz/gologin-2025.6.9102915/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r") as f:
+    install_requires = [line.strip() for line in f.readlines()]
+
+setup(
+    name='gologin',
+    version='2025.06.09102915',
+    packages=find_packages(),
+    install_requires=install_requires,
+    author='GoLogin',
+    author_email='yuri@gologin.com',
+    description='Official GoLogin python package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/gologinapp/pygologin',
+    python_requires='>=3.5',
+    package_data={
+        "gologin": ["py.typed"],
+    },
+)
