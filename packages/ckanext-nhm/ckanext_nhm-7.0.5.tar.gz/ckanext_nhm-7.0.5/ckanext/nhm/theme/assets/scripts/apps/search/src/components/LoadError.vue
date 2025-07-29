7c67fc4eb06daefb0659e20701729483d75f41af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:32 +0000

    Add package ckanext-nhm/7.0.5/ckanext_nhm-7.0.5.tar.gz

---
+++ packages/ckanext-nhm/ckanext_nhm-7.0.5.tar.gz/ckanext_nhm-7.0.5/ckanext/nhm/theme/assets/scripts/apps/search/src/components/LoadError.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex-container flex-center flex-column alert-error full-width">
+    <slot>
+      <h3>Unable to load. Please try again later.</h3>
+    </slot>
+  </div>
+</template>
+
+<script></script>
