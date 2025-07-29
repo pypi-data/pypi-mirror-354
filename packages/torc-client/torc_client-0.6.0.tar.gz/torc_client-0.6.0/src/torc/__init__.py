6471f2cb40ad21c72a41721d81862ec26e9c851d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:50 +0000

    Add package torc-client/0.6.0/torc_client-0.6.0.tar.gz

---
+++ packages/torc-client/torc_client-0.6.0.tar.gz/torc_client-0.6.0/src/torc/__init__.py
@@ -0,0 +1,32 @@
+"""torc package"""
+
+import warnings
+from importlib import metadata
+
+from torc.api import (
+    add_jobs,
+    iter_documents,
+    make_api,
+    make_job_label,
+    map_function_to_jobs,
+    send_api_command,
+)
+from torc.config import torc_settings
+from torc.loggers import setup_logging
+
+
+__version__ = metadata.metadata("torc-client")["Version"]
+
+warnings.filterwarnings("once", category=DeprecationWarning)
+
+
+__all__ = (
+    "add_jobs",
+    "iter_documents",
+    "make_api",
+    "make_job_label",
+    "map_function_to_jobs",
+    "send_api_command",
+    "setup_logging",
+    "torc_settings",
+)
