5c471aba408309f244a4ab460fab4496814d638c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:42 +0000

    Add package fancy-subprocess/2.2/fancy_subprocess-2.2-py3-none-any.whl

---
+++ packages/fancy-subprocess/fancy_subprocess-2.2-py3-none-any.whl/fancy_subprocess/__init__.py
@@ -0,0 +1,6 @@
+from fancy_subprocess._compat import *
+from fancy_subprocess._print import *
+from fancy_subprocess._reconfigure import *
+from fancy_subprocess._run_core import *
+from fancy_subprocess._run_param import *
+from fancy_subprocess._run_wrappers import *
