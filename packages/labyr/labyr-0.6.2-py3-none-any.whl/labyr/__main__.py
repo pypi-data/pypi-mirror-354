a4d5d6f92424e6619ca747e4942d14e3f8b70013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:34 +0000

    Add package labyr/0.6.2/labyr-0.6.2-py3-none-any.whl

---
+++ packages/labyr/labyr-0.6.2-py3-none-any.whl/labyr/__main__.py
@@ -0,0 +1,48 @@
+import argparse
+from importlib.metadata import PackageNotFoundError, version
+
+from .__init__ import labyr
+
+
+def get_version():
+    try:
+        return version("labyr")  # pypi project name
+    except PackageNotFoundError:
+        return "unknown"
+
+
+def callback(action):
+    if action == "run":
+        labyr()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="""
+        labyr: a labyrinth game
+        
+        DISCLAIMER: if you run labyr, each frame will clear the terminal screen.
+        """
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=f"labyr {get_version()}",
+        help="Show labyr version",
+    )
+
+    allowed_actions = ["run"]
+
+    parser.add_argument(
+        "action",
+        choices=allowed_actions,
+        help=f"Action to perform. Allowed values: {', '.join(allowed_actions)}",
+    )
+
+    args = parser.parse_args()
+    callback(args.action)
+
+
+if __name__ == "__main__":
+    main()
