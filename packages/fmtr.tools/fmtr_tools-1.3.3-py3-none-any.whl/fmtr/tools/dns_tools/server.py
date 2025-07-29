327dfbcb492fc5f64521feb0732a17770426dfdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:24 +0000

    Add package fmtr.tools/1.3.3/fmtr_tools-1.3.3-py3-none-any.whl

---
+++ packages/fmtr.tools/fmtr_tools-1.3.3-py3-none-any.whl/fmtr/tools/dns_tools/server.py
@@ -0,0 +1,38 @@
+import socket
+from dataclasses import dataclass
+
+from fmtr.tools.dns_tools.dm import Exchange
+
+
+@dataclass
+class Plain:
+    """
+
+    Base for starting a plain DNS server
+
+    """
+
+    host: str
+    port: int
+
+    def __post_init__(self):
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def resolve(self, exchange: Exchange):
+        raise NotImplemented
+
+    def start(self):
+        """
+
+        Listen and resolve via overridden resolve method.
+
+        """
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((self.host, self.port))
+        print(f"Listening on {self.host}:{self.port}")
+        while True:
+            data, (ip, port) = sock.recvfrom(512)
+            exchange = Exchange.from_wire(data, ip=ip, port=port)
+            self.resolve(exchange)
+            sock.sendto(exchange.response.message.to_wire(), (ip, port))
