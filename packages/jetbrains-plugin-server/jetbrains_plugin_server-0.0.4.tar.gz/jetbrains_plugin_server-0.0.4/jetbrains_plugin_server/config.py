c92be0c1767625e7da0141ce7997b339db136971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:14 +0000

    Add package jetbrains-plugin-server/0.0.4/jetbrains_plugin_server-0.0.4.tar.gz

---
+++ packages/jetbrains-plugin-server/jetbrains_plugin_server-0.0.4.tar.gz/jetbrains_plugin_server-0.0.4/jetbrains_plugin_server/config.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+
+DL_URL_BASE = os.getenv("DL_URL_BASE", "https://fake.url/{plugin_version_id}")
+
+JETBRAINS_PLUGINS_HOST = "https://plugins.jetbrains.com"
+
+LOCAL = Path(__file__).parent.parent.joinpath("local")
+
+PLUGIN_PROD_DATA = Path(__file__).parent.parent.joinpath("plugins_prod.json")
+
+PLUGIN_TEST_DATA = Path(__file__).parent.parent.joinpath("tests", "data", "plugins_test.json")
+
+PLUGIN_SPECS_DIR = "plugin_specs"
+PLUGIN_VERSIONS_DIR = "plugin_versions"
+PLUGINS_DIR = "plugins"
+
+IS_TEST_MODE = os.getenv("TEST_MODE") == "true"
+
+FAST_API_OFFLINE = os.getenv("FAST_API_OFFLINE") == "true"
