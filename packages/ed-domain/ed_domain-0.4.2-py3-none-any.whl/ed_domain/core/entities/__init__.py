08f131db033c3d1038a68bb7052d1a607e55b6cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:07:33 +0000

    Add package ed-domain/0.4.2/ed_domain-0.4.2-py3-none-any.whl

---
+++ packages/ed-domain/ed_domain-0.4.2-py3-none-any.whl/ed_domain/core/entities/__init__.py
@@ -0,0 +1,15 @@
+from ed_domain.core.entities.bill import Bill
+from ed_domain.core.entities.car import Car
+from ed_domain.core.entities.location import Location
+from ed_domain.core.entities.notification import Notification
+from ed_domain.core.entities.otp import Otp
+from ed_domain.core.entities.parcel import Parcel
+
+__all__ = [
+    "Car",
+    "Bill",
+    "Location",
+    "Notification",
+    "Otp",
+    "Parcel",
+]
