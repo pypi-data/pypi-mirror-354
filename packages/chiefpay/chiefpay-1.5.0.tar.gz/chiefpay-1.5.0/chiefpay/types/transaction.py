1aa1901c0a31bb1c45e36a7fd798f5447f60a8e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:44 +0000

    Add package chiefpay/1.5.0/chiefpay-1.5.0.tar.gz

---
+++ packages/chiefpay/chiefpay-1.5.0.tar.gz/chiefpay-1.5.0/chiefpay/types/transaction.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, Field
+
+from chiefpay.types.wallet import Wallet
+
+
+class Transaction(BaseModel):
+    txid: str
+    chain: str
+    token: str
+    value: str
+    usd: str
+    fee: str
+    wallet: Wallet
+    created_at: str = Field(alias='createdAt')
+    block_created_at: str = Field(alias='blockCreatedAt')
+
+
+class LastTransaction(BaseModel):
+    chain: str
+    txid: str
