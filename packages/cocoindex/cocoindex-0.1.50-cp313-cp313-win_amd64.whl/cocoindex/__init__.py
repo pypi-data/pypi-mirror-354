cd63ec87db607131fedbac1cecbcb5cedee1323b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:16 +0000

    Add package cocoindex/0.1.50/cocoindex-0.1.50-cp313-cp313-win_amd64.whl

---
+++ packages/cocoindex/cocoindex-0.1.50-cp313-cp313-win_amd64.whl/cocoindex/__init__.py
@@ -0,0 +1,17 @@
+"""
+Cocoindex is a framework for building and running indexing pipelines.
+"""
+
+from . import functions, sources, storages, cli, utils
+
+from .auth_registry import AuthEntryReference, add_auth_entry, ref_auth_entry
+from .flow import FlowBuilder, DataScope, DataSlice, Flow, transform_flow
+from .flow import flow_def
+from .flow import EvaluateAndDumpOptions, GeneratedField
+from .flow import update_all_flows_async, FlowLiveUpdater, FlowLiveUpdaterOptions
+from .lib import init, start_server, stop, main_fn
+from .llm import LlmSpec, LlmApiType
+from .index import VectorSimilarityMetric, VectorIndexDef, IndexOptions
+from .setting import DatabaseConnectionSpec, Settings, ServerSettings
+from .setting import get_app_namespace
+from .typing import Float32, Float64, LocalDateTime, OffsetDateTime, Range, Vector, Json
