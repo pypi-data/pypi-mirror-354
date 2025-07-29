4cb9a957715892a8305106a318bc91736d574c83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:40 +0000

    Add package djangoldp-tems/2.0.27/djangoldp_tems-2.0.27.tar.gz

---
+++ packages/djangoldp-tems/djangoldp_tems-2.0.27.tar.gz/djangoldp_tems-2.0.27/djangoldp_onboarding/views.py
@@ -0,0 +1,18 @@
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .forms import ParticipantRegistrationForm
+from .models import RegisterParticipant
+from django.conf import settings
+
+
+class OrganisationCreateView(CreateView):
+    model = RegisterParticipant
+    form_class = ParticipantRegistrationForm
+    template_name = "onboarding_form.html"
+    success_url = getattr(settings, "ONBOARDING_PREFIX", "") + reverse_lazy("onboarding-success")
+
+    def form_valid(self, form):
+      response = super().form_valid(form)
+      response.status_code = 303
+      return response
