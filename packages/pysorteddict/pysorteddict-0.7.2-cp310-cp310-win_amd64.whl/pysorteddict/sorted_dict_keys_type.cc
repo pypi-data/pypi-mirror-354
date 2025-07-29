84f3edf112ea5f67f1920b491b62ddb2efb39418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:09 +0000

    Add package pysorteddict/0.7.2/pysorteddict-0.7.2-cp310-cp310-win_amd64.whl

---
+++ packages/pysorteddict/pysorteddict-0.7.2-cp310-cp310-win_amd64.whl/pysorteddict/sorted_dict_keys_type.cc
@@ -0,0 +1,10 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+#include "sorted_dict_keys_type.hh"
+#include "sorted_dict_view_type.hh"
+
+PyObject* SortedDictKeysIterType::next(void)
+{
+    return Py_XNewRef(this->SortedDictViewIterType::next().first);  // 🆕
+}
