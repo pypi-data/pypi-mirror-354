579e7d0b2294d58db379f6f88f785ff6136b3ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:12 +0000

    Add package crashs/0.2.11/crashs-0.2.11.tar.gz

---
+++ packages/crashs/crashs-0.2.11.tar.gz/crashs-0.2.11/src/crashs/__main__.py
@@ -0,0 +1,36 @@
+import argparse
+from crashs.df import HuggingFaceImporter
+from crashs.crashs import FitLauncher
+from crashs.build_template import BuildTemplateLauncher
+from crashs.roi_integrate import ROILauncher
+from crashs.profile_sampling import ProfileSamplingLauncher, ProfileMappingLauncher
+
+# Create a parser
+parse = argparse.ArgumentParser(
+    prog="crashs", description="CRASHS: Cortical reconstruction for ASHS")
+
+# Add subparsers for the main commands
+sub = parse.add_subparsers(dest='command', help='sub-command help', required=True)
+
+# Add the CRASHS subparser commands
+c_fit = FitLauncher(
+    sub.add_parser('fit', help='Fit ASHS segmentation to CRASHS template'))
+
+c_download = HuggingFaceImporter(
+    sub.add_parser('download', help='Download CRASHS template package from Hugging Face'))
+
+c_build = BuildTemplateLauncher(
+    sub.add_parser('build', help='Build new CRASHS template'))
+
+c_roi = ROILauncher(
+    sub.add_parser('roi', help='Integrate features over ROIs in a mesh'))
+
+c_prof_sam = ProfileSamplingLauncher(
+    sub.add_parser('profile_sample', help='Sample data into template space using CRASHS profiles'))
+    
+c_prof_map = ProfileMappingLauncher(
+    sub.add_parser('profile_map', help='Map data from template to subject using CRASHS profiles'))
+
+# Parse the arguments
+args = parse.parse_args()
+args.func(args)
