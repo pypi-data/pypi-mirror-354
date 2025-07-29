67ebe5ef2f8b1ac9ca60ac35069723a42897d2ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:21 +0000

    Add package torax/1.0.3/torax-1.0.3-py3-none-any.whl

---
+++ packages/torax/torax-1.0.3-py3-none-any.whl/torax/_src/neoclassical/conductivity/sauter.py
@@ -0,0 +1,128 @@
+# Copyright 2024 DeepMind Technologies Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Sauter conductivity model."""
+
+from typing import Literal
+
+import chex
+import jax.numpy as jnp
+from torax._src import constants
+from torax._src import jax_utils
+from torax._src import state
+from torax._src.fvm import cell_variable
+from torax._src.geometry import geometry as geometry_lib
+from torax._src.neoclassical.conductivity import base
+
+
+class SauterModel(base.ConductivityModel):
+  """Sauter conductivity model."""
+
+  def calculate_conductivity(
+      self,
+      geometry: geometry_lib.Geometry,
+      core_profiles: state.CoreProfiles,
+  ) -> base.Conductivity:
+    """Calculates conductivity."""
+    result = _calculate_conductivity(
+        Z_eff_face=core_profiles.Z_eff_face,
+        n_e=core_profiles.n_e,
+        T_e=core_profiles.T_e,
+        q_face=core_profiles.q_face,
+        geo=geometry,
+    )
+    return base.Conductivity(
+        sigma=result.sigma,
+        sigma_face=result.sigma_face,
+    )
+
+  def __eq__(self, other) -> bool:
+    return isinstance(other, self.__class__)
+
+  def __hash__(self) -> int:
+    return hash(self.__class__)
+
+
+class SauterModelConfig(base.ConductivityModelConfig):
+  """Sauter conductivity model config."""
+  model_name: Literal['sauter'] = 'sauter'
+
+  def build_model(self) -> SauterModel:
+    return SauterModel()
+
+
+@jax_utils.jit
+def _calculate_conductivity(
+    *,
+    Z_eff_face: chex.Array,
+    n_e: cell_variable.CellVariable,
+    T_e: cell_variable.CellVariable,
+    q_face: chex.Array,
+    geo: geometry_lib.Geometry,
+) -> base.Conductivity:
+  """Calculates sigma and sigma_face using the Sauter model."""
+  # pylint: disable=invalid-name
+
+  # Formulas from Sauter PoP 1999. Future work can include Redl PoP 2021
+  # corrections.
+
+  # # local r/R0 on face grid
+  epsilon = (geo.R_out_face - geo.R_in_face) / (geo.R_out_face + geo.R_in_face)
+  epseff = (
+      0.67 * (1.0 - 1.4 * jnp.abs(geo.delta_face) * geo.delta_face) * epsilon
+  )
+  aa = (1.0 - epsilon) / (1.0 + epsilon)
+  ftrap = 1.0 - jnp.sqrt(aa) * (1.0 - epseff) / (1.0 + 2.0 * jnp.sqrt(epseff))
+
+  # Spitzer conductivity
+  NZ = 0.58 + 0.74 / (0.76 + Z_eff_face)
+  lnLame = (
+      31.3 - 0.5 * jnp.log(n_e.face_value()) + jnp.log(T_e.face_value() * 1e3)
+  )
+
+  sigsptz = (
+      1.9012e04 * (T_e.face_value() * 1e3) ** 1.5 / Z_eff_face / NZ / lnLame
+  )
+
+  nuestar = (
+      6.921e-18
+      * q_face
+      * geo.R_major
+      * n_e.face_value()
+      * Z_eff_face
+      * lnLame
+      / (
+          ((T_e.face_value() * 1e3) ** 2)
+          * (epsilon + constants.CONSTANTS.eps) ** 1.5
+      )
+  )
+
+  # Neoclassical correction to spitzer conductivity
+  ft33 = ftrap / (
+      1.0
+      + (0.55 - 0.1 * ftrap) * jnp.sqrt(nuestar)
+      + 0.45 * (1.0 - ftrap) * nuestar / (Z_eff_face**1.5)
+  )
+  signeo_face = 1.0 - ft33 * (
+      1.0
+      + 0.36 / Z_eff_face
+      - ft33 * (0.59 / Z_eff_face - 0.23 / Z_eff_face * ft33)
+  )
+  sigma_face = sigsptz * signeo_face
+
+  sigmaneo_cell = geometry_lib.face_to_cell(sigma_face)
+
+  return base.Conductivity(
+      sigma=sigmaneo_cell,
+      sigma_face=sigma_face,
+  )
---
+++ packages/torax/torax-1.0.3-py3-none-any.whl/torax/_src/transport_model/bohm_gyrobohm.py
@@ -0,0 +1,194 @@
+# Copyright 2024 DeepMind Technologies Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""The BohmGyroBohmModel class."""
+import chex
+from jax import numpy as jnp
+from torax._src import array_typing
+from torax._src import constants as constants_module
+from torax._src import state
+from torax._src.config import runtime_params_slice
+from torax._src.geometry import geometry
+from torax._src.pedestal_model import pedestal_model as pedestal_model_lib
+from torax._src.transport_model import runtime_params as runtime_params_lib
+from torax._src.transport_model import transport_model
+
+
+# pylint: disable=invalid-name
+@chex.dataclass(frozen=True)
+class DynamicRuntimeParams(runtime_params_lib.DynamicRuntimeParams):
+  """Dynamic runtime params for the BgB transport model."""
+
+  chi_e_bohm_coeff: array_typing.ScalarFloat
+  chi_e_gyrobohm_coeff: array_typing.ScalarFloat
+  chi_i_bohm_coeff: array_typing.ScalarFloat
+  chi_i_gyrobohm_coeff: array_typing.ScalarFloat
+  D_face_c1: array_typing.ScalarFloat
+  D_face_c2: array_typing.ScalarFloat
+  V_face_coeff: array_typing.ScalarFloat
+  chi_e_bohm_multiplier: array_typing.ScalarFloat
+  chi_e_gyrobohm_multiplier: array_typing.ScalarFloat
+  chi_i_bohm_multiplier: array_typing.ScalarFloat
+  chi_i_gyrobohm_multiplier: array_typing.ScalarFloat
+
+
+class BohmGyroBohmTransportModel(transport_model.TransportModel):
+  """Calculates various coefficients related to particle transport according to the Bohm + gyro-Bohm Model."""
+
+  def __init__(
+      self,
+  ):
+    super().__init__()
+    self._frozen = True
+
+  def _call_implementation(
+      self,
+      transport_dynamic_runtime_params: runtime_params_lib.DynamicRuntimeParams,
+      dynamic_runtime_params_slice: runtime_params_slice.DynamicRuntimeParamsSlice,
+      geo: geometry.Geometry,
+      core_profiles: state.CoreProfiles,
+      pedestal_model_output: pedestal_model_lib.PedestalModelOutput,
+  ) -> state.CoreTransport:
+    r"""Calculates transport coefficients using the BohmGyroBohm model.
+
+    We use the implementation from Tholerus et al, Section 3.3.
+    https://doi.org/10.1088/1741-4326/ad6ea2
+
+    A description is provided in physics_models.rst.
+    https://torax.readthedocs.io/en/latest/physics_models.html
+
+    Args:
+      transport_dynamic_runtime_params: Input runtime parameters for this
+        transport model. Can change without triggering a JAX recompilation.
+      dynamic_runtime_params_slice: Input runtime parameters for all components
+        of the simulation that can change without triggering a JAX
+        recompilation.
+      geo: Geometry of the torus.
+      core_profiles: Core plasma profiles.
+      pedestal_model_output: Output of the pedestal model.
+
+    Returns:
+      coeffs: The transport coefficients
+    """
+    del pedestal_model_output  # Unused.
+    # pylint: disable=invalid-name
+    # Required for pytype
+    assert isinstance(transport_dynamic_runtime_params, DynamicRuntimeParams)
+
+    # Bohm term of heat transport
+    chi_e_B = (
+        geo.r_mid_face
+        * core_profiles.q_face**2
+        / (
+            constants_module.CONSTANTS.qe
+            * geo.B_0
+            * core_profiles.n_e.face_value()
+        )
+        * (
+            jnp.abs(core_profiles.n_e.face_grad())
+            * core_profiles.T_e.face_value()
+            + jnp.abs(core_profiles.T_e.face_grad())
+            * core_profiles.n_e.face_value()
+        )
+        * constants_module.CONSTANTS.keV2J
+        / geo.rho_b
+    )
+
+    # Set proportionality of chi_i to chi_e according to the assumptions of the
+    # Bohm model.
+    chi_i_B = 2 * chi_e_B
+
+    # Gyrobohm term of heat transport
+    chi_e_gB = (
+        jnp.sqrt(
+            dynamic_runtime_params_slice.plasma_composition.main_ion.avg_A / 2
+        )
+        * jnp.sqrt(core_profiles.T_e.face_value() * 1e3)
+        / geo.B_0**2
+        * jnp.abs(core_profiles.T_e.face_grad() * 1e3)
+        / geo.rho_b
+    )
+
+    # Set proportionality of chi_i to chi_e according to the assumptions of the
+    # GyroBohm model.
+    chi_i_gB = 0.5 * chi_e_gB
+
+    # Calibrated transport coefficients
+    chi_e_bohm = (
+        transport_dynamic_runtime_params.chi_e_bohm_coeff
+        * transport_dynamic_runtime_params.chi_e_bohm_multiplier
+        * chi_e_B
+    )
+    chi_e_gyrobohm = (
+        transport_dynamic_runtime_params.chi_e_gyrobohm_coeff
+        * transport_dynamic_runtime_params.chi_e_gyrobohm_multiplier
+        * chi_e_gB
+    )
+
+    chi_i_bohm = (
+        transport_dynamic_runtime_params.chi_i_bohm_coeff
+        * transport_dynamic_runtime_params.chi_i_bohm_multiplier
+        * chi_i_B
+    )
+    chi_i_gyrobohm = (
+        transport_dynamic_runtime_params.chi_i_gyrobohm_coeff
+        * transport_dynamic_runtime_params.chi_i_gyrobohm_multiplier
+        * chi_i_gB
+    )
+
+    # Total heat transport (combined contributions)
+    chi_e = chi_e_gyrobohm + chi_e_bohm
+    chi_i = chi_i_gyrobohm + chi_i_bohm
+
+    # Electron diffusivity
+    weighting = (
+        transport_dynamic_runtime_params.D_face_c1
+        + (
+            transport_dynamic_runtime_params.D_face_c2
+            - transport_dynamic_runtime_params.D_face_c1
+        )
+        * geo.rho_face_norm
+    )
+
+    # Diffusion: d_face_el is zero on-axis by definition.
+    # We add a small epsilon to the denominator to avoid cases where
+    # chi_i + chi_e = 0.
+    d_face_el = jnp.concatenate([
+        jnp.zeros(1),
+        weighting[1:]
+        * chi_e[1:]
+        * chi_i[1:]
+        / (chi_e[1:] + chi_i[1:] + constants_module.CONSTANTS.eps),
+    ])
+
+    # Electron convectivity set proportional to the electron diffusivity
+    v_face_el = transport_dynamic_runtime_params.V_face_coeff * d_face_el
+
+    return state.CoreTransport(
+        chi_face_ion=chi_i,
+        chi_face_el=chi_e,
+        d_face_el=d_face_el,
+        v_face_el=v_face_el,
+        chi_face_el_bohm=chi_e_bohm,
+        chi_face_el_gyrobohm=chi_e_gyrobohm,
+        chi_face_ion_bohm=chi_i_bohm,
+        chi_face_ion_gyrobohm=chi_i_gyrobohm,
+    )
+
+  def __hash__(self):
+    # All BohmGyroBohmModels are equivalent and can hash the same
+    return hash('BohmGyroBohmModel')
+
+  def __eq__(self, other):
+    return isinstance(other, BohmGyroBohmTransportModel)
