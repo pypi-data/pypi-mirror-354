0a069078aefbd02c758af65a156ef6b0d8000f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:23 +0000

    Add package evsetool/0.1.5/evsetool-0.1.5.tar.gz

---
+++ packages/evsetool/evsetool-0.1.5.tar.gz/evsetool-0.1.5/src/evsetool/__main__.py
@@ -0,0 +1,164 @@
+import argparse
+import logging
+import asyncio
+import yaml
+import scapy
+import time
+
+from .sniffer import parse, main as sniffer_main
+from .evse import simflow_transaction, simflow_diagnostics
+from .csms import serve_OCPPv16
+from .utils import *
+
+DEFAULT_ID_TAG = "01234567890123456789"
+DEFAULT_URL = "ws://127.0.0.1:8180/steve/websocket/CentralSystemService"
+DEFAULT_LOCAL_OCPP_PORT = 9000
+
+async def main():
+    parser = argparse.ArgumentParser(description='EVSE Red Team Tool')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                    help='Show full OCPP traffic output')
+    parser.add_argument('-f', '--file', type=str,
+                    help='Path to YAML configuration file')
+    parser.add_argument('--sniff', action='store_true',
+                    help='Listen passively for OCPP1.6 traffic over TCP 8180')
+    parser.add_argument('--serve', action="store_true",
+                    help='Serves OCPP1.6 on $local_ocpp_port')
+    parser.add_argument('--csms', action='store_true',
+                    help='Interact with CSMS in role of EVSE')
+    parser.add_argument('--url', type=str,
+                    help='Address of system to query')
+    parser.add_argument('-e', '--evse_id', type=str,
+                    help='20-character string uniquely identifying the EVSE')
+    parser.add_argument('--vendor', type=str, default='WitchWires',
+                    help='Company which produced the EVSE')
+    parser.add_argument('--model', type=str, default='WW1959',
+                    help='Name of the specific model of EVSE')
+    parser.add_argument('--name', type=str, default='CP_1',
+                    help='API endpoint for the EVSE')
+    parser.add_argument('--pcap', type=str,
+                    help='Read from a PCAP rather than sniffing LAN')
+    parser.add_argument('--sim', action='store_true',
+                    help='Simulate')
+    parser.add_argument('-i', '--interactive', action='store_true',
+                    help='Interactive execution mode')
+    args = parser.parse_args()
+
+    url = DEFAULT_URL
+    id_tag = DEFAULT_ID_TAG
+    local_ocpp_port = DEFAULT_LOCAL_OCPP_PORT
+    args.file = './default.config.yaml' if not args.file else args.file
+    try:
+        with open(args.file) as f:
+            cfg = yaml.load(f, Loader=yaml.FullLoader)
+            url = cfg['url'] if args.url is None else args.url
+            id_tag = cfg['id_tag'] if args.evse_id is None else args.evse_id
+            local_ocpp_port = cfg['local_ocpp_port'] # TODO add command-line option
+    except FileNotFoundError:            
+        pass
+
+    log_level = logging.INFO if args.verbose else logging.ERROR
+    logging.basicConfig(level=log_level)
+    args = parser.parse_args()
+
+    if args.sniff: sniff()
+    elif args.pcap: pcap(args.pcap) 
+    elif args.csms: await csms(url, id_tag, args.name)
+    elif args.serve: await serve(local_ocpp_port)
+    elif args.sim:
+        log("EVSETOOL::Running sim_diagnostics")
+        ip_addr = '127.0.0.1'
+        port = local_ocpp_port
+        await run_sim(ip_addr, port, id_tag, args.name)
+        #sniffer_main()
+    elif args.interactive:
+        log("EVSETOOL::Entering interactive mode")
+        await interactive()
+    else:
+        print("ERROR: Please select one of the following: [csms|sim|sniff|pcap|interactive]")
+        print("use --help for more information")
+
+async def interactive():
+    while True:
+        user_input = input("evsetool> ")
+        user_fields = user_input.split(' ')
+        match user_fields[0]:
+            case "csms": 
+                try:
+                    url = user_fields[1]
+                    id_tag = user_fields[2]
+                    evse_name = user_fields[3]
+                    await csms(url, id_tag, evse_name)
+                except IndexError:
+                    print("csms command requires inputs url, id_tag and evse_name")
+                    print("Type 'help' to see a list of possible commands and their inputs")
+            case "serve": 
+                try:
+                    lport = int(user_fields[1])
+                    await serve(lport)
+                except (IndexError, ValueError):
+                    print("sniff command requires integer input lport")
+                    print("Type 'help' to see a list of possible commands and their inputs")
+            case "sim": 
+                #sim()
+                pass
+            case "sniff":
+                sniff()
+            case "pcap":
+                try:
+                    filename = user_fields[1]
+                    pcap(filename)
+                except IndexError:
+                    print("pcap command requires input filename")
+                    print("Type 'help' to see a list of possible commands and their inputs")
+            case "q":
+                return
+            case "quit":
+                return
+            case "exit":
+                return
+            case "help": 
+                interactive_help()
+            case _: 
+                print("Invalid input.\n")
+                interactive_help()
+
+def interactive_help():
+    print("COMMANDS\n--------")
+    print("csms URL ID_TAG EVSE_NAME\n\tsimulate CSMS")
+    print("serve\n\tsimulate EVSE client node")
+    print("sim\n\tWIP simulation mode")
+    print("sniff\n\tlistens on the LAN for OCPP1.6 traffic")
+    print("pcap FILENAME\n\tparse pcap, pcapng input file")
+    print("quit (or q, or exit)\n\texit interactive shell")
+    print("help\n\tdisplay this help message")
+    print()
+
+async def csms(url, id_tag, name):
+    log("EVSETOOL::Querying CSMS...")
+    await simflow_transaction(url, id_tag, name)
+
+def pcap(filename):
+    log("EVSETOOL::Reading pcap <%s>..." % filename)
+    pkt = scapy.all.rdpcap(filename)
+    for p in map(parse, pkt): 
+        if p is not None: print(p)        
+
+async def run_sim(ip_addr, port, id_tag, name):
+    log('ere')
+    server_url = 'ws://%s:%s' % (ip_addr, port)
+    
+    loop = asyncio.get_running_loop()
+    loop.create_task(serve_OCPPv16(ip_addr, port))
+    loop.create_task(simflow_diagnostics(server_url, id_tag, name))
+
+async def serve(lport):
+    log("EVSETOOL::Serving OCPP1.6 on port %d" % lport)
+    await serve_OCPPv16('0.0.0.0', lport)
+
+def sniff():
+    log("EVSETOOL::Starting sniffer...")
+    sniffer_main()
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ packages/evsetool/evsetool-0.1.5.tar.gz/evsetool-0.1.5/tests/test_evsetool.py
@@ -0,0 +1,91 @@
+import pytest
+import evsetool
+from evsetool import sniffer, wifi_decryptor
+from scapy.all import *
+import os
+from binascii import hexlify
+
+aes_key = bytes.fromhex("C0C1C2C3C4C5C6C7C8C9CACBCCCDCECF")
+nonce = bytes.fromhex("00000003020100A0A1A2A3A4A5")
+input_data = bytes.fromhex("000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E")
+output_data = bytes.fromhex("0001020304050607588C979A61C663D2F066D0C2C0F989806D5F6B61DAC38417E8D12CFDF926E0")
+CCMPContext = wifi_decryptor.CCMPContext
+
+
+
+pkt50_aes_key = bytes.fromhex("a62ed5b6d903a776fe617d8da7973f86")
+pkt50_nonce = b'\x00' * 7 + b'\x09'
+pkt50_input_data = bytes.fromhex("aaaa0300000008060001080006040001d83addeffe9ec0a800b1000000000000c0a80001")
+pkt50_output_data = bytes.fromhex("c1efe894f6939eb71c526e68f6ee6a907fba530381d7d61b4f45d2c9a847bd45368b91c81b48a88331de05fe")
+
+def prep_websocket_tests():
+    
+    path = os.path.join('tests', 'cap.pcapng')
+    scapy_cap = rdpcap(path)
+    
+    # TODO can this be removed
+    for i,p in enumerate(scapy_cap): 
+        print(i, p)
+        if p.haslayer(sniffer.DstWebSocket): print(p[sniffer.DstWebSocket].frame_data)
+    
+    return scapy_cap
+
+def prep_wifi_tests():
+    pcap = os.path.join('var', 'capture.pcapng')
+    credfile = os.path.join('var', 'wificreds.csv')
+    with open(credfile) as fp:
+        ssid, password = fp.read().strip().split(',')
+    return pcap, ssid, password
+
+
+def test_websocket_create():
+    cap = prep_websocket_tests()
+    assert cap[24].haslayer(sniffer.DstWebSocket)
+    output = b'[3,"bad9a3f6-ca80-4901-bb76-9034b8900b5b",{"status":"Accepted","currentTime":"2024-12-12T12:14:42.958Z","interval":14400}]'
+    assert cap[24].frame_data == output
+
+    # make sure zlib state stays good throughout
+    output2 = b'[3,"cee3e36b-34fb-45a8-a6b8-f5d394f32beb",{}]'
+    assert cap[42].frame_data == output2
+'''
+def test_wifi_decryptor_decode_4way_handshake():
+    pcap, ssid, password = prep_wifi_tests()
+    pmk, ptk = wifi_decryptor.main_app(ssid, pcap, password=password)
+    assert hexlify(pmk) == b'cb26e56614e866277095b1b861a5e44ecad0c5c6bdaea434374094acec4f6960'
+    assert hexlify(ptk) == b'a62ed5b6d903a776fe617d8da7973f86'
+
+# TODO can first two lines here be combined with previous
+def test_wifi_decryptor_ccm_decryption():
+    pcap, ssid, password = prep_wifi_tests()
+    pmk, ptk = wifi_decryptor.main_app(ssid, pcap, password=password)
+    packet = rdpcap(pcap)[49]
+    ctext = getattr(packet[Dot11CCMP], 'data')
+    assert hexlify(ctext) == hexlify(pkt50_output_data)
+    ptext = wifi_decryptor.decrypt_packet(packet, ptk)
+    print(len(ctext), len(ptext))
+    assert hexlify(ptext) == b'aaaa0300000008060001080006040001d83addeffe9ec0a800b1000000000000c0a80001'
+'''
+def test_ccm_encrypt_rfc3610_testvector1():
+    ccmp = CCMPContext(input_data, aes_key, nonce, decrypt=False)
+    ctext, mac = wifi_decryptor.aes_ccm_encrypt(ccmp.key, ccmp.nonce, ccmp.ptext, ccmp.aad)
+    print(ccmp.__print__())
+    result = ccmp.aad + ctext + mac
+    assert hexlify(result) == hexlify(output_data)
+'''
+def test_ccm_decrypt_capture_pkt50():
+    mpkt50_output_data = bytes.fromhex('0001020304050607') + pkt50_output_data
+    ccmp = CCMPContext(mpkt50_output_data, pkt50_aes_key, pkt50_nonce, decrypt=True)
+    #ccmp.aad = pkt50_output_data[:8]
+    #ccmp.aad = bytes.fromhex('aaaa030000000806')
+    ptext = wifi_decryptor.aes_ccm_decrypt(ccmp.key, ccmp.nonce, ccmp.ctext, ccmp.aad, ccmp.mac)
+    print(ccmp.__print__())
+    result = ccmp.aad + ptext
+    print(hexlify(result))
+    print(hexlify(pkt50_input_data))
+    assert hexlify(result) == hexlify(pkt50_input_data)
+'''
+def test_ccm_decrypt_rfc3610_testvector1():
+    ccmp = CCMPContext(output_data, aes_key, nonce, decrypt=True)
+    _ptext = wifi_decryptor.aes_ccm_decrypt(ccmp.key, ccmp.nonce, ccmp.ctext, ccmp.aad, ccmp.mac)
+    print(ccmp.__print__())
+    assert hexlify(input_data[8:]) == hexlify(_ptext)
