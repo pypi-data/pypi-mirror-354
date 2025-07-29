3c094c707ad4ab6b0146bf4f4ba0cf656e643239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:30 +0000

    Add package srcml-caller/0.4.0/srcml_caller-0.4.0-cp311-cp311-win_amd64.whl

---
+++ packages/srcml-caller/srcml_caller-0.4.0-cp311-cp311-win_amd64.whl/srcml_caller/share/cmake/srcml/srcML-static-targets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "srcML::libsrcml_static" for configuration "Release"
+set_property(TARGET srcML::libsrcml_static APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(srcML::libsrcml_static PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libsrcml_static.lib"
+  )
+
+list(APPEND _cmake_import_check_targets srcML::libsrcml_static )
+list(APPEND _cmake_import_check_files_for_srcML::libsrcml_static "${_IMPORT_PREFIX}/lib/libsrcml_static.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
