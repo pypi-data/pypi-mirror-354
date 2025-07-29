905f0b9c3392bd0b9cf87f984498964583e05b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:37 +0000

    Add package dvlogger/1.2.0/dvlogger-1.2.0.tar.gz

---
+++ packages/dvlogger/dvlogger-1.2.0.tar.gz/dvlogger-1.2.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dvlogger',
+    version='1.2.0',
+    description='A custom logger with coloured output and uncaught exception logging.',
+    packages=find_packages(),
+    install_requires=['colorama'],
+    author='Pranav Mittal',
+    author_email='pranavmittal611@gmail.com',
+    url='https://github.com/mlpranav/dvlogger',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
