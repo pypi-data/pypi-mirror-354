81074d542c16a698895a59f61acc860a58153ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:07:45 +0000

    Add package abstract-clicks/0.0.0.32/abstract_clicks-0.0.0.32.tar.gz

---
+++ packages/abstract-clicks/abstract_clicks-0.0.0.32.tar.gz/abstract_clicks-0.0.0.32/src/abstract_clicks/managers/__init__.py
@@ -0,0 +1,7 @@
+from .importClasses import *
+from .replayManager import *
+from .eventsRecorder import *
+from .clipboardManager import *
+from .screenshotManager import *
+from .utils import *
+from .windowManager import *
