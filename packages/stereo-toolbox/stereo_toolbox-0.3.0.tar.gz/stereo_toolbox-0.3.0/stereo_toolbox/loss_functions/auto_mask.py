8152b0f97348aaa6213def0b15f98a5f5e5d5c8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:31 +0000

    Add package stereo-toolbox/0.3.0/stereo_toolbox-0.3.0.tar.gz

---
+++ packages/stereo-toolbox/stereo_toolbox-0.3.0.tar.gz/stereo_toolbox-0.3.0/stereo_toolbox/loss_functions/auto_mask.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn.functional as F
+
+from .photometric_loss import photometric_loss
+
+
+def auto_mask(left_image, right_image, disp, denorm=False, stationary=True, occlusion=False):
+    if denorm:
+        mean = torch.tensor([0.485, 0.456, 0.406], device=left_image.device).view(1, 3, 1, 1)
+        std = torch.tensor([0.229, 0.224, 0.225], device=left_image.device).view(1, 3, 1, 1)
+        left_image = left_image * std + mean
+        right_image = right_image * std + mean
+
+    reproj_error = photometric_loss(left_image, right_image, disp.detach(), enable_mask=False)
+
+    mask = torch.ones_like(reproj_error, dtype=torch.bool)
+
+    if stationary:
+        identity_error = photometric_loss(left_image, right_image, enable_mask=False)
+        mask = mask * (reproj_error < identity_error)
+
+    if occlusion:
+        occlusion_threshold = reproj_error.mean(dim=(-1,-2), keepdim=True) * 2
+        mask = mask * (reproj_error < occlusion_threshold)
+
+    return mask
