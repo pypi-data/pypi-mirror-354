d740f29d7b21129a4422628dce42b1f0f3ee2eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:28 +0000

    Add package perfect-cmaps/1.3.0/perfect_cmaps-1.3.0.tar.gz

---
+++ packages/perfect-cmaps/perfect_cmaps-1.3.0.tar.gz/perfect_cmaps-1.3.0/perfect_cmaps/__init__.py
@@ -0,0 +1,7 @@
+from .colormaps import get_cmap
+from .customize_cmap import create_cmap
+from .storage import list_cmaps
+
+
+__all__ = ["get_cmap", "create_cmap", "list_cmaps"]
+__version__ = "1.3.0"
