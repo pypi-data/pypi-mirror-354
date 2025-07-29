0a9c85a025150c90711e096ae1c50d1ca62a92ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:07:19 +0000

    Add package seven-top/1.0.48/seven_top-1.0.48.tar.gz

---
+++ packages/seven-top/seven_top-1.0.48.tar.gz/seven_top-1.0.48/seven_top/top/api/rest/OpencrmSignatureVerifyInfoGetRequest.py
@@ -0,0 +1,11 @@
+'''
+Created by auto_sdk on 2025.05.09
+'''
+from top.api.base import RestApi
+class OpencrmSignatureVerifyInfoGetRequest(RestApi):
+	def __init__(self,domain='gw.api.taobao.com',port=80):
+		RestApi.__init__(self,domain, port)
+		self.signature_request = None
+
+	def getapiname(self):
+		return 'taobao.opencrm.signature.verify.info.get'
