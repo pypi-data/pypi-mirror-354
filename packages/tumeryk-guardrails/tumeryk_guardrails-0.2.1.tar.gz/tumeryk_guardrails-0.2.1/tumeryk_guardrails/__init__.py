54b94ad34c613ed27f719d6ff360ed6b19ba8f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:06 +0000

    Add package tumeryk-guardrails/0.2.1/tumeryk_guardrails-0.2.1.tar.gz

---
+++ packages/tumeryk-guardrails/tumeryk_guardrails-0.2.1.tar.gz/tumeryk_guardrails-0.2.1/tumeryk_guardrails/__init__.py
@@ -0,0 +1,11 @@
+from .guardrails_client import TumerykGuardrailsClient
+
+client = TumerykGuardrailsClient()
+
+login = client.login
+get_policies = client.get_policies
+set_policy = client.set_policy
+tumeryk_completions = client.tumeryk_completions
+get_base_url = client.get_base_url
+set_base_url = client.set_base_url
+set_token = client.set_token
