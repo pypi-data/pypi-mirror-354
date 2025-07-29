5b9d73d88acfbb9e1eb5a87987a4d1da5d7a9738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:43 +0000

    Add package epitome-tools/0.0.1/epitome_tools-0.0.1.tar.gz

---
+++ packages/epitome-tools/epitome_tools-0.0.1.tar.gz/epitome_tools-0.0.1/setup.py
@@ -0,0 +1,30 @@
+#this is a setup.py file
+from setuptools import setup, find_packages
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Auxiliary tools for the Consensus Pituitary Atlas'
+LONG_DESCRIPTION = 'Python package containing auxiliary tools for the Consensus Pituitary Atlas. The current workflow commands allow celltyping and doublet detection with a single line of code, for more information see https://github.com/BKover99/epitome_tools'
+
+
+setup(
+    name="epitome_tools",
+    version=VERSION,
+    author="Bence Kover",
+    author_email="<kover.bence@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+	'xgboost',
+	'scipy',
+	'numpy'
+	],
+    keywords=['xgboost', 'annotation', 'celltype', 'doublet'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X"
+    ]
+)
+
