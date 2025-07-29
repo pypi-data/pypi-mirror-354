337c5a8417772e05b84c61d01bcd63434e4be668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:15 +0000

    Add package excel-list-transform/0.7.10/excel_list_transform-0.7.10.tar.gz

---
+++ packages/excel-list-transform/excel_list_transform-0.7.10.tar.gz/excel_list_transform-0.7.10/setup.py
@@ -0,0 +1,26 @@
+#! /usr/local/bin/python3
+"""Setup file specifying build of apo_tools.whl with APO tools library."""
+
+from setuptools import setup
+
+setup(
+  name='excel-list-transform',
+  version='0.7.10',
+  description='Transform a list in excel or CSV.',
+  author='Tom Björkholm',
+  author_email='klausuler_linnet0q@icloud.com',
+  python_requires='<3.12,>=3.11',
+  packages=['excel_list_transform'],
+  package_dir={'excel_list_transform': 'src/excel_list_transform'},
+  package_data={'excel_list_transform': ['src/py.typed']},
+  install_requires=[
+    'openpyxl >= 3.1.5',
+    'types-openpyxl >= 3.1.5.20250602',
+    'pylightxl >= 1.61',
+    'XlsxWriter >= 3.2.3',
+    'pip >= 25.0.1',
+    'setuptools >= 80.9.0',
+    'build >= 1.2.2.post1',
+    'wheel>=0.45.1'
+  ]
+)
