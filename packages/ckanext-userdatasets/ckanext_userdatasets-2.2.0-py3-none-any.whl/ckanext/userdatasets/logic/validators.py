a00a030db21f356ba2e2324034d04e1bcd8624fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:43 +0000

    Add package ckanext-userdatasets/2.2.0/ckanext_userdatasets-2.2.0-py3-none-any.whl

---
+++ packages/ckanext-userdatasets/ckanext_userdatasets-2.2.0-py3-none-any.whl/ckanext/userdatasets/logic/validators.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+#
+# This file is part of ckanext-userdatasets
+# Created by the Natural History Museum in London, UK
+
+from ckan.authz import users_role_for_group_or_org
+from ckan.logic.validators import owner_org_validator as default_owner_org_validator
+from ckan.plugins import toolkit
+
+
+def owner_org_validator(key, data, errors, context):
+    owner_org = data.get(key)
+    if owner_org is not toolkit.missing and owner_org is not None and owner_org != '':
+        if context.get('auth_user_obj', None) is not None:
+            username = context['auth_user_obj'].name
+        else:
+            username = context['user']
+
+        role = users_role_for_group_or_org(owner_org, username)
+        if role == 'member':
+            return
+
+    default_owner_org_validator(key, data, errors, context)
