ab12b25dc90c796f7f2aa55efd24a2457dafb602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:07:43 +0000

    Add package day2/0.2.1/day2-0.2.1.tar.gz

---
+++ packages/day2/day2-0.2.1.tar.gz/day2-0.2.1/day2/resources/__init__.py
@@ -0,0 +1,6 @@
+"""Resource implementations for the MontyCloud DAY2 SDK."""
+
+from day2.resources.assessment import AssessmentClient
+from day2.resources.tenant import TenantClient
+
+__all__ = ["TenantClient", "AssessmentClient"]
