048407fd0c8137eeca6ccc33a97840e7828aac7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:07:43 +0000

    Add package byteplus-python-sdk-v2/2.0.6/byteplus-python-sdk-v2-2.0.6.tar.gz

---
+++ packages/byteplus-python-sdk-v2/byteplus-python-sdk-v2-2.0.6.tar.gz/byteplus-python-sdk-v2-2.0.6/byteplussdkarkruntime/resources/__init__.py
@@ -0,0 +1,12 @@
+from .chat import Chat, AsyncChat
+from .context import Context, AsyncContext
+from .batch_chat import BatchChat, AsyncBatchChat
+
+__all__ = [
+    "Chat",
+    "AsyncChat",
+    "Context",
+    "AsyncContext",
+    "BatchChat",
+    "AsyncBatchChat"
+]
