cc20298cdda8e08fc327eeb15cbf5cd3c235bf52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:43 +0000

    Add package pytest-oarepo/1.0.6/pytest_oarepo-1.0.6.tar.gz

---
+++ packages/pytest-oarepo/pytest_oarepo-1.0.6.tar.gz/pytest_oarepo-1.0.6/pytest_oarepo/users.py
@@ -0,0 +1,66 @@
+import pytest
+from pytest_oarepo.functions import _index_users
+
+
+@pytest.fixture()
+def users(app, db, UserFixture):
+    """
+    Predefined user fixtures.
+    """
+    user1 = UserFixture(
+        email="user1@example.org",
+        password="password",
+        active=True,
+        confirmed=True,
+    )
+    user1.create(app, db)
+
+    user2 = UserFixture(
+        email="user2@example.org",
+        password="beetlesmasher",
+        username="beetlesmasher",
+        active=True,
+        confirmed=True,
+    )
+    user2.create(app, db)
+
+    user3 = UserFixture(
+        email="user3@example.org",
+        password="beetlesmasher",
+        username="beetlesmasherXXL",
+        user_profile={
+            "full_name": "Maxipes Fik",
+            "affiliations": "CERN",
+        },
+        active=True,
+        confirmed=True,
+    )
+    user3.create(app, db)
+
+    user4 = UserFixture(
+        email="user4@example.org",
+        password="african",
+        username="african",
+        preferences={
+            "timezone": "Africa/Dakar", # something without daylight saving time; +0.0
+        },
+        active=True,
+        confirmed=True,
+    )
+    user4.create(app, db)
+
+    user5 = UserFixture(
+        email="user5@example.org",
+        password="mexican",
+        username="mexican",
+        preferences={
+            "timezone": "America/Mexico_City", # something without daylight saving time
+        },
+        active=True,
+        confirmed=True,
+    )
+    user5.create(app, db)
+
+    db.session.commit()
+    _index_users()
+    return [user1, user2, user3, user4, user5]
