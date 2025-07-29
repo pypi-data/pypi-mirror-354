49126259b778ccb4d8ea0103a98e09bfc6f2d45b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:57 +0000

    Add package tokopedia-api-wrapper/0.0.1/tokopedia_api_wrapper-0.0.1-py3-none-any.whl

---
+++ packages/tokopedia-api-wrapper/tokopedia_api_wrapper-0.0.1-py3-none-any.whl/tokopedia_api_wrapper/__init__.py
@@ -0,0 +1,11 @@
+from .__version__ import (
+    __title__,
+    __url__,
+    __version__,
+    __author__,
+    __author_email__,
+    __license__,
+    __copyright__,
+)
+
+from .tokopedia import Tokopedia
