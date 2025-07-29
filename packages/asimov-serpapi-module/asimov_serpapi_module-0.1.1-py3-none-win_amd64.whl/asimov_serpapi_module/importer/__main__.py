179238b0fee560ac70ad457640f980164cab836a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:31 +0000

    Add package asimov-serpapi-module/0.1.1/asimov_serpapi_module-0.1.1-py3-none-win_amd64.whl

---
+++ packages/asimov-serpapi-module/asimov_serpapi_module-0.1.1-py3-none-win_amd64.whl/asimov_serpapi_module/importer/__main__.py
@@ -0,0 +1,7 @@
+import os, sys
+
+argv = [os.path.join(os.path.dirname(__file__), "asimov-serpapi-importer.exe"), *sys.argv[1:]]
+if os.name == 'posix':
+    os.execv(argv[0], argv)
+else:
+    import subprocess; sys.exit(subprocess.call(argv))
