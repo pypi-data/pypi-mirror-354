dfde6af30ecfe004579df50ac022e9ad313ff48e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:01 +0000

    Add package tutor/19.0.5/tutor-19.0.5.tar.gz

---
+++ packages/tutor/tutor-19.0.5.tar.gz/tutor-19.0.5/tutor/commands/upgrade/__init__.py
@@ -0,0 +1,14 @@
+# Note: don't forget to change this when we upgrade from sumac
+OPENEDX_RELEASE_NAMES = [
+    "ironwood",
+    "juniper",
+    "koa",
+    "lilac",
+    "maple",
+    "nutmeg",
+    "olive",
+    "palm",
+    "quince",
+    "redwood",
+    "sumac",
+]
