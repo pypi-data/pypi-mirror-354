058729b7bf3388221e8affba3eaa368ccb44a392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:30 +0000

    Add package dqtoolx/0.1/dqtoolx-0.1.tar.gz

---
+++ packages/dqtoolx/dqtoolx-0.1.tar.gz/dqtoolx-0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dqtoolx",
+    version="0.1",
+    packages=find_packages(),
+    #py_modules=["dqtoolcli"],
+    install_requires=[
+        "sqlalchemy",
+        "boto3",
+        "python-dotenv",  # 'dotenv' should be 'python-dotenv' on PyPI
+        "pyyaml",  # 'yaml' should be 'pyyaml' on PyPI
+        "psycopg2",
+        "redshift-connector",
+        "sqlalchemy-redshift",
+        "apscheduler",
+        "croniter",
+        "jinja2",
+        "tabulate",
+        "simpleeval",
+        "spacy"
+    ],
+    entry_points={
+        'console_scripts': [
+            'dqtool = cli.dqtoolcli:main',
+        ],
+    },
+)
