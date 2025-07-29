9d0f894f83e9de670870f2417a79e0c0cc0e893d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:01 +0000

    Add package prefect/3.4.6.dev1/prefect-3.4.6.dev1.tar.gz

---
+++ packages/prefect/prefect-3.4.6.dev1.tar.gz/prefect-3.4.6.dev1/src/prefect/server/ui/assets/404-CFbp_Dr-.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,o as a,n as t,dv as n}from"./index-lpMZ-Mxi.js";import{u as r}from"./usePageTitle-lR93gd3J.js";const m=e({__name:"404",setup(s){return r("Page Not Found"),(c,p)=>(a(),o(t(n)))}});export{m as default};
+//# sourceMappingURL=404-CFbp_Dr-.js.map
---
+++ packages/prefect/prefect-3.4.6.dev1.tar.gz/prefect-3.4.6.dev1/src/prefect/server/ui/assets/usePrefectApi-CBeGf8KN.js
@@ -0,0 +1,2 @@
+import{cH as r}from"./index-lpMZ-Mxi.js";import{p as e}from"./api-D7Xm_t3K.js";function o(){return r(e)}export{o as u};
+//# sourceMappingURL=usePrefectApi-CBeGf8KN.js.map
