faf3efac41a15d9c46b0c8282a4afc79d73c3f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:22 +0000

    Add package python-churn/1.1.1/python_churn-1.1.1.tar.gz

---
+++ packages/python-churn/python_churn-1.1.1.tar.gz/python_churn-1.1.1/churn/__init__.py
@@ -0,0 +1,7 @@
+"""Module for creating and running SLURM batch jobs."""
+
+from .batch import create_batch_chain, run_chain
+from .configurator import Configurator
+from .task import Task
+
+__all__ = ['Configurator', 'Task', 'create_batch_chain', 'run_chain']
