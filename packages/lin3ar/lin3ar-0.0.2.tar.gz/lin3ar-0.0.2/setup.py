f6549c695319a9fcea85b5340e9b31b89c097f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:56 +0000

    Add package lin3ar/0.0.2/lin3ar-0.0.2.tar.gz

---
+++ packages/lin3ar/lin3ar-0.0.2.tar.gz/lin3ar-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lin3ar',
+    version='0.0.2',
+    packages=find_packages(),
+    install_requires=[],
+    author='idzydek',
+    author_email='k.zydek@aol.com',
+    description='Placeholder',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/idzydek/lin3ar',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
+
