612f11916346bb0d5dfadfbebc3d0a2a385ea9b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:50 +0000

    Add package bbextras/0.1.0/bbextras-0.1.0-py3-none-any.whl

---
+++ packages/bbextras/bbextras-0.1.0-py3-none-any.whl/bblab/utils/__xinit__.py
@@ -0,0 +1,9 @@
+"""
+A collection of reusable utility functions and modules.
+
+Author: Mahmudul Hasan Rasel
+Created: 2025-06-10
+License: MIT
+Github: https://github.com/bitbytelab/bblab
+Portfolio: https://rsmahmud.github.io
+"""
