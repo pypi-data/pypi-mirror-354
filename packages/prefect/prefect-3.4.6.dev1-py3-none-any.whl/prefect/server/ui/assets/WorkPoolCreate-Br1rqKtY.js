33c287cb00b52385b892ecf1bc96d342c0c8bf50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:58 +0000

    Add package prefect/3.4.6.dev1/prefect-3.4.6.dev1-py3-none-any.whl

---
+++ packages/prefect/prefect-3.4.6.dev1-py3-none-any.whl/prefect/server/ui/assets/WorkPoolCreate-Br1rqKtY.js
@@ -0,0 +1,2 @@
+import{d as r,j as s,c as n,o as l,k as e,l as o,n as a,c$ as c,d0 as u}from"./index-lpMZ-Mxi.js";import{u as _}from"./usePageTitle-lR93gd3J.js";const k=r({__name:"WorkPoolCreate",setup(p){return _("Create Work Pool"),(d,f)=>{const t=s("p-layout-default");return l(),n(t,null,{header:e(()=>[o(a(u))]),default:e(()=>[o(a(c))]),_:1})}}});export{k as default};
+//# sourceMappingURL=WorkPoolCreate-Br1rqKtY.js.map
