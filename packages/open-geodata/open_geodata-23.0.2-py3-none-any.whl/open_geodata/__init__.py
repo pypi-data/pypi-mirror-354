83bff9c7ae2235fce99d3f40d83794190fb6b955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:23 +0000

    Add package open-geodata/23.0.2/open_geodata-23.0.2-py3-none-any.whl

---
+++ packages/open-geodata/open_geodata-23.0.2-py3-none-any.whl/open_geodata/__init__.py
@@ -0,0 +1,16 @@
+# open_geodata
+from .lyr import base
+from .functions import share_boundary
+from .folium_plus import adds, png
+from .converts import coords, files
+from .providers import br, sp
+
+__all__ = [
+    'share_boundary',
+    'base',
+    'adds',
+    'png',
+    'coords',
+    'files',
+    #'ibge',
+]
