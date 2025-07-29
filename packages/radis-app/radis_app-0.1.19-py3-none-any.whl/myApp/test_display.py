32b93950da95a428b8115e339bbba710e45813b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:07 +0000

    Add package radis-app/0.1.19/radis_app-0.1.19-py3-none-any.whl

---
+++ packages/radis-app/radis_app-0.1.19-py3-none-any.whl/myApp/test_display.py
@@ -0,0 +1,23 @@
+
+class Me:
+    def __init__(self):
+        self.name = "Radi"
+        self.hair = "Brown"
+        self.age = 26
+
+
+def set_string():
+    person = Me()
+    text = f"My name is {person.name} and I am {person.age} years old with {person.hair} hair"
+    return text
+
+
+def display_me():
+    output = set_string()
+    print(output)
+
+def main():
+    display_text = display_me()
+    simple_gui("Hello World!", display_text)
+
+main()
