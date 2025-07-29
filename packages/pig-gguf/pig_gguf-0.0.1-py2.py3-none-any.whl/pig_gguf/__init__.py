30fe3f665d3acc0060fc63151aea987765ed6520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:09:05 +0000

    Add package pig-gguf/0.0.1/pig_gguf-0.0.1-py2.py3-none-any.whl

---
+++ packages/pig-gguf/pig_gguf-0.0.1-py2.py3-none-any.whl/pig_gguf/__init__.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python3
+
+__version__ = '0.0.1'
+
+def __init__():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
+    subparsers = parser.add_subparsers(title="subcommands", dest="subcommand", help="choose a subcommand:")
+    subparsers.add_parser('d', help='[d] decomposer')
+    args = parser.parse_args()
+    if args.subcommand == 'd':
+        from pig_gguf import d
