a15e28a81db001ee62dc8df9579cd013b47bda9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:42 +0000

    Add package ckanext-statistics/3.1.15/ckanext_statistics-3.1.15.tar.gz

---
+++ packages/ckanext-statistics/ckanext_statistics-3.1.15.tar.gz/ckanext_statistics-3.1.15/ckanext/statistics/plugin.py
@@ -0,0 +1,33 @@
+# !/usr/bin/env python
+# encoding: utf-8
+#
+# This file is part of ckanext-statistics
+# Created by the Natural History Museum in London, UK
+
+
+from ckan.plugins import SingletonPlugin, implements, interfaces
+
+from ckanext.statistics.logic.action import dataset_statistics, download_statistics
+
+from . import cli
+
+
+class StatisticsPlugin(SingletonPlugin):
+    """
+    NHM Statistics.
+    """
+
+    implements(interfaces.IActions)
+    implements(interfaces.IClick)
+
+    # IActions
+    @staticmethod
+    def get_actions():
+        return {
+            'download_statistics': download_statistics,
+            'dataset_statistics': dataset_statistics,
+        }
+
+    # IClick
+    def get_commands(self):
+        return cli.get_commands()
