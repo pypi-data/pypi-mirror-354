1db7a1b49a16a75f3c2e397ad53462a07b44a9ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:33 +0000

    Add package splight-lib/5.19.6/splight_lib-5.19.6.tar.gz

---
+++ packages/splight-lib/splight_lib-5.19.6.tar.gz/splight_lib-5.19.6/splight_lib/models/_v4/bus.py
@@ -0,0 +1,24 @@
+from splight_lib.models._v4.asset import AssetParams, AssetRelationship
+from splight_lib.models._v4.attribute import Attribute
+from splight_lib.models._v4.metadata import Metadata
+from splight_lib.models.database import SplightDatabaseBaseModel
+
+
+class Bus(AssetParams, SplightDatabaseBaseModel):
+    # Input attributes
+    active_power: Attribute | None = None
+    reactive_power: Attribute | None = None
+
+    # Computed attributes
+    temperature: Attribute | None = None
+    wind_speed: Attribute | None = None
+    wind_direction: Attribute | None = None
+    irradiance: Attribute | None = None
+    forecasted_temperature_48H: Attribute | None = None
+    forecasted_wind_speed_48H: Attribute | None = None
+
+    # Metadata
+    nominal_voltage: Metadata | None = None
+
+    # Relationships
+    grid: AssetRelationship | None = None
