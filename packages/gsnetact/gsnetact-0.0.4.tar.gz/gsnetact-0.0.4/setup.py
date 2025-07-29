4e53ecb06dde2967e8b56c94ab9f32cddc44471e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:48 +0000

    Add package gsnetact/0.0.4/gsnetact-0.0.4.tar.gz

---
+++ packages/gsnetact/gsnetact-0.0.4.tar.gz/gsnetact-0.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README_PACKAGE.md", "r") as f:
+    desc = f.read()
+
+setup(name="gsnetact", 
+      
+      version="0.0.4",
+
+      description="The GSNetAct Python Package.",
+        
+      long_description=desc,
+    
+      long_description_content_type="text/markdown",
+
+      packages=find_packages(),
+            
+      )
+      
