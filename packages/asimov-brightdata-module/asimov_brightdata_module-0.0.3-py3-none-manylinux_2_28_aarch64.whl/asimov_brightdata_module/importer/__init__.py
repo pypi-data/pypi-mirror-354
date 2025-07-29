dbf3afe963c8d2e6690f820aeda792e09c672635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:06 +0000

    Add package asimov-brightdata-module/0.0.3/asimov_brightdata_module-0.0.3-py3-none-manylinux_2_28_aarch64.whl

---
+++ packages/asimov-brightdata-module/asimov_brightdata_module-0.0.3-py3-none-manylinux_2_28_aarch64.whl/asimov_brightdata_module/importer/__init__.py
@@ -0,0 +1,8 @@
+import sys
+
+__version__ = "0.0.3"
+
+def main(argv=sys.argv):
+    import os, subprocess
+    argv = [os.path.join(os.path.dirname(__file__), "asimov-brightdata-importer"), *argv[1:]]
+    subprocess.call(argv)
