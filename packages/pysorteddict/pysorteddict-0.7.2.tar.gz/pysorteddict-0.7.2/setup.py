330d853f69e5eff365fd6b8b5b326d79a0feaa31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:19 +0000

    Add package pysorteddict/0.7.2/pysorteddict-0.7.2.tar.gz

---
+++ packages/pysorteddict/pysorteddict-0.7.2.tar.gz/pysorteddict-0.7.2/setup.py
@@ -0,0 +1,16 @@
+import glob
+import os
+import sysconfig
+
+import setuptools
+
+# MSVC supports designated initialisers only in C++20.
+cxx_standard_arg = "-std=c++14" if os.name != "nt" or sysconfig.get_platform().startswith("mingw") else "/std:c++20"
+
+setuptools.setup(
+    ext_modules=[
+        setuptools.Extension(
+            name="pysorteddict", extra_compile_args=[cxx_standard_arg], sources=glob.glob("src/pysorteddict/*.cc")
+        )
+    ]
+)
