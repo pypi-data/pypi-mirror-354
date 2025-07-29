6fc004c9335fd383876df302b1d959755fbba905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:17 +0000

    Add package towles-tool/0.0.4/towles_tool-0.0.4.tar.gz

---
+++ packages/towles-tool/towles_tool-0.0.4.tar.gz/towles_tool-0.0.4/src/towles_tool/foo.py
@@ -0,0 +1,23 @@
+def main() -> None:
+    """main function"""
+
+    print("Hello, World from towles-tool!")
+
+
+def foo(bar: str) -> str:
+    """Summary line.
+
+    Extended description of function.
+
+    Args:
+        bar: Description of input argument.
+
+    Returns:
+        Description of return value
+    """
+
+    return bar
+
+
+if __name__ == "__main__":  # pragma: no cover
+    pass
