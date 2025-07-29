db8ad1bd55873abb5e114841cef23996d9b1f411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:41 +0000

    Add package PyWSGIRef/1.1.3/pywsgiref-1.1.3-py3-none-any.whl

---
+++ packages/PyWSGIRef/pywsgiref-1.1.3-py3-none-any.whl/PyWSGIRef/beta.py
@@ -0,0 +1,31 @@
+"""
+BETA mode...
+"""
+
+class beta:
+    """
+    BETA mode for PyWSGIRef.
+    """
+    def __init__(self):
+        self._beta = False
+
+    @property
+    def value(self) -> bool:
+        return self._beta
+
+    def enable(self):
+        """
+        Enables BETA mode.
+        """
+        if self._beta:
+            raise ValueError("BETA mode is already enabled.")
+        self._beta = True
+
+BETA = beta()
+
+def enableBetaMode():
+    """
+    Enables BETA mode.
+    """
+    BETA.enable()
+    print("BETA mode enabled.")
