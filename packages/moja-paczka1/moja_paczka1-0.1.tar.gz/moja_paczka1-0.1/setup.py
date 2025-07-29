f2a055e841503818865536fced5f100fe694c18d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:21 +0000

    Add package moja-paczka1/0.1/moja_paczka1-0.1.tar.gz

---
+++ packages/moja-paczka1/moja_paczka1-0.1.tar.gz/moja_paczka1-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='moja_paczka1',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Sigma',
+    description='sigma packages'
+)
