097a53d2cfc39b03da0630e15ebab8faab72f556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:22 +0000

    Add package ml-determination/0.3.0/ml_determination-0.3.0.tar.gz

---
+++ packages/ml-determination/ml_determination-0.3.0.tar.gz/ml_determination-0.3.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ml_determination",
+    version="0.3.0",
+    packages=find_packages(),
+    include_package_data=True,  # to include non-code files
+    install_requires=[
+        # Add your dependencies here
+        "spacy",
+        "numpy",
+        "torch",
+        "transformers"
+    ],
+    author="Olga Iakovenko",
+    description="A package for determining the matrix language in bilingual sentences.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
