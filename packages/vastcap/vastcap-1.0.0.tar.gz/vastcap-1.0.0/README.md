837b6d552aca7008b1e864dd33c7c74ae036e885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:18 +0000

    Add package vastcap/1.0.0/vastcap-1.0.0.tar.gz

---
+++ packages/vastcap/vastcap-1.0.0.tar.gz/vastcap-1.0.0/README.md
@@ -0,0 +1,20 @@
+# vastcap
+VastCap Python SDK
+
+- Coming Soon
+
+[GitHub](https://github.com/vastien/vastcap-python)
+
+## Installation
+
+You can install the `vastcap` package using pip:
+
+```bash
+pip install vastcap
+```
+
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+## Contributing
+Coming soon.
---
+++ packages/vastcap/vastcap-1.0.0.tar.gz/vastcap-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='vastcap',
+    version='1.0.0',
+    packages=find_packages(),
+    description='VastCap Python SDK',
+    long_description=long_description,
+    author='vastien',
+    author_email='contact@vast.sh',
+    license='MIT',
+    url='https://github.com/vastien/vastcap-python',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+)
