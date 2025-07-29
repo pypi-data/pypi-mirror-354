e24f35a15bfb6fde9405d86ec0fef811255012ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:55 +0000

    Add package listndsort/0.1.0/listndsort-0.1.0.tar.gz

---
+++ packages/listndsort/listndsort-0.1.0.tar.gz/listndsort-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='listndsort',  # Your package name
+    version='0.1.0',     # Version
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='a package that recivecs numbers and lists them before sorting them',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/reverseword',  # Optional
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
