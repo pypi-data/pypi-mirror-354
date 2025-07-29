c451bd572b76558773442e58e2f47374441d491d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:07:46 +0000

    Add package ngio/0.3.1/ngio-0.3.1-py3-none-any.whl

---
+++ packages/ngio/ngio-0.3.1-py3-none-any.whl/ngio/tables/__init__.py
@@ -0,0 +1,41 @@
+"""Ngio Tables implementations."""
+
+from ngio.tables._tables_container import (
+    ConditionTable,
+    FeatureTable,
+    GenericRoiTable,
+    MaskingRoiTable,
+    RoiTable,
+    Table,
+    TablesContainer,
+    TableType,
+    TypedTable,
+    open_table,
+    open_table_as,
+    open_tables_container,
+)
+from ngio.tables.backends import (
+    ImplementedTableBackends,
+    TableBackend,
+    TableBackendProtocol,
+)
+from ngio.tables.v1._generic_table import GenericTable
+
+__all__ = [
+    "ConditionTable",
+    "FeatureTable",
+    "GenericRoiTable",
+    "GenericTable",
+    "ImplementedTableBackends",
+    "MaskingRoiTable",
+    "RoiTable",
+    "Table",
+    "TableBackend",
+    "TableBackendProtocol",
+    "TableType",
+    "TablesContainer",
+    "TypedTable",
+    "open_table",
+    "open_table_as",
+    "open_tables_container",
+]
