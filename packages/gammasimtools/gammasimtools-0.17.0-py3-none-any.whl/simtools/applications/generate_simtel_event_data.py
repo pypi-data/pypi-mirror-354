773bf930569c4f22f189df5a91beebbba72b8596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:47 +0000

    Add package gammasimtools/0.17.0/gammasimtools-0.17.0-py3-none-any.whl

---
+++ packages/gammasimtools/gammasimtools-0.17.0-py3-none-any.whl/simtools/applications/generate_simtel_event_data.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+
+r"""
+Generate a reduced dataset of event data from simulated files.
+
+Processes sim_telarray output files (typically of type '.simtel.zst') and creates a
+reduced dataset containing shower information, array-level parameters, and data about
+triggered telescopes.
+
+Command line arguments
+----------------------
+prefix (str, required)
+    Path prefix for the input files.
+output_file (str, required)
+    Output file path.
+max_files (int, optional, default=100)
+    Maximum number of input files to process.
+print_dataset_information (int, optional, default=0)
+    Print information about the datasets in the generated reduced event dataset.
+
+Example
+-------
+Generate a reduced dataset from input files and save the result.
+
+.. code-block:: console
+
+    simtools-production-extract-mc-event-data \\
+    simtools-generate-simtel-event-data \\
+        --input 'path/to/input_files/gamma_*dark*.simtel.zst' \\
+        --output_file output_file.hdf5 \\
+        --max_files 50 \\
+        --print_dataset_information 10
+"""
+
+import logging
+from pathlib import Path
+
+import simtools.utils.general as gen
+from simtools.configuration import configurator
+from simtools.data_model.metadata_collector import MetadataCollector
+from simtools.io_operations import io_handler, io_table_handler
+from simtools.simtel.simtel_io_event_writer import SimtelIOEventDataWriter
+
+
+def _parse(label, description):
+    """
+    Parse command line arguments.
+
+    Returns
+    -------
+    dict
+        Parsed command-line arguments.
+    """
+    config = configurator.Configurator(label=label, description=description)
+
+    config.parser.add_argument(
+        "--input",
+        type=str,
+        required=True,
+        help="Input file path (wildcards allowed; e.g., '/data_path/gamma_*dark*.simtel.zst')",
+    )
+    config.parser.add_argument(
+        "--max_files", type=int, default=100, help="Maximum number of input files to process."
+    )
+    config.parser.add_argument(
+        "--print_dataset_information",
+        type=int,
+        help="Print data set information for the given number of events.",
+        default=0,
+    )
+    return config.initialize(db_config=False, output=True)
+
+
+def main():  # noqa: D103
+    label = Path(__file__).stem
+
+    args_dict, _ = _parse(
+        label=label,
+        description=(
+            "Process files and store reduced dataset with event information, "
+            "array information and triggered telescopes."
+        ),
+    )
+
+    logger = logging.getLogger()
+    logger.setLevel(gen.get_log_level_from_user(args_dict["log_level"]))
+    logger.info(f"Loading input files from: {args_dict['input']}")
+
+    input_pattern = Path(args_dict["input"])
+    files = list(input_pattern.parent.glob(input_pattern.name))
+    if not files:
+        logger.warning("No matching input files found.")
+        return
+
+    output_filepath = io_handler.IOHandler().get_output_file(args_dict["output_file"])
+    generator = SimtelIOEventDataWriter(files, args_dict["max_files"])
+    tables = generator.process_files()
+    io_table_handler.write_tables(tables, output_filepath, overwrite_existing=True)
+    MetadataCollector.dump(args_dict=args_dict, output_file=output_filepath.with_suffix(".yml"))
+
+    if args_dict["print_dataset_information"] > 0:
+        for table in tables:
+            table.pprint(max_lines=args_dict["print_dataset_information"], max_width=-1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/gammasimtools/gammasimtools-0.17.0-py3-none-any.whl/simtools/simtel/simtel_io_metadata.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python3
+"""Read metadata from sim_telarray files."""
+
+import logging
+from functools import cache
+
+from eventio import EventIOFile
+from eventio.simtel import HistoryMeta
+
+_logger = logging.getLogger(__name__)
+
+
+@cache
+def read_sim_telarray_metadata(file, encoding="utf8"):
+    """
+    Return global and per-telescope metadata from sim_telarray file.
+
+    Cached to avoid re-reading the file multiple times.
+
+    Parameters
+    ----------
+    file: str
+        Path to the sim_telarray file.
+    encoding: str
+        Encoding to use for decoding metadata. Default is 'utf8'.
+
+    Returns
+    -------
+    global_meta: dict
+        Global metadata dictionary.
+    telescope_meta: dict
+        Dictionary of telescope metadata, keyed by telescope ID.
+    """
+    global_meta = None
+    telescope_meta = {}
+
+    with EventIOFile(file) as f:
+        found_meta = False
+        for o in f:
+            if isinstance(o, HistoryMeta):
+                found_meta = True
+            else:
+                if found_meta:
+                    break
+                continue
+
+            meta = _decode_dictionary(o.parse(), encoding=encoding)
+            if o.header.id == -1:
+                global_meta = meta
+            else:
+                telescope_meta[o.header.id] = meta
+
+    def clean_meta(meta):
+        """Clean metadata dictionary."""
+        meta = {k.lower().lstrip("*"): v for k, v in meta.items()}
+        return {k: v.strip() if isinstance(v, str) else v for k, v in meta.items()}
+
+    # keys to lower case and strip leading '*', trailing spaces
+    return clean_meta(global_meta), {
+        tel_id: clean_meta(meta) for tel_id, meta in telescope_meta.items()
+    }
+
+
+def _decode_dictionary(meta, encoding="utf8"):
+    """Decode metadata dictionary."""
+
+    def safe_decode(byte_str, encoding, errors="ignore"):
+        return byte_str.decode(encoding, errors=errors)
+
+    try:
+        return {k.decode(encoding, errors="ignore"): v.decode(encoding) for k, v in meta.items()}
+    except UnicodeDecodeError as e:
+        _logger.warning(
+            f"Failed to decode metadata with encoding {encoding}: {e}. "
+            "Falling back to 'utf-8' with errors='ignore'."
+        )
+        return {safe_decode(k, encoding): safe_decode(v, encoding) for k, v in meta.items()}
+
+
+def get_sim_telarray_telescope_id(telescope_name, file):
+    """
+    Return the telescope ID for a given telescope name in a sim_telarray file.
+
+    Translates e.g. 'LSTN-01' to the corresponding telescope ID.
+
+    Parameters
+    ----------
+    telescope_name: str
+        Name of the telescope.
+    file: str
+        Path to the sim_telarray file.
+
+    Returns
+    -------
+    int, None
+        Telescope ID. Returns None if not found.
+    """
+    _, telescope_meta = read_sim_telarray_metadata(file)
+    telescope_name_to_sim_telarray_id = {}
+    for tel_id in telescope_meta.keys():
+        _optics_name = telescope_meta[tel_id].get("optics_config_name", None)
+        _camera_name = telescope_meta[tel_id].get("camera_config_name", None)
+        if _optics_name == _camera_name and _optics_name == telescope_name:
+            telescope_name_to_sim_telarray_id[telescope_name] = tel_id
+
+    return telescope_name_to_sim_telarray_id.get(telescope_name, None)
