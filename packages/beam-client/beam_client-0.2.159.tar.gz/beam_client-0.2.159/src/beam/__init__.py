647427f28310e2eb2b57b99717f5a8a58e4cb9f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:12 +0000

    Add package beam-client/0.2.159/beam_client-0.2.159.tar.gz

---
+++ packages/beam-client/beam_client-0.2.159.tar.gz/beam_client-0.2.159/src/beam/__init__.py
@@ -0,0 +1,53 @@
+from beta9 import Bot, BotContext, BotEventType, BotLocation, env, schema
+from beta9.abstractions import experimental
+from beta9.abstractions.base.container import Container
+from beta9.abstractions.endpoint import ASGI as asgi
+from beta9.abstractions.endpoint import Endpoint as endpoint
+from beta9.abstractions.endpoint import RealtimeASGI as realtime
+from beta9.abstractions.function import Function as function
+from beta9.abstractions.function import Schedule as schedule
+from beta9.abstractions.image import Image
+from beta9.abstractions.map import Map
+from beta9.abstractions.output import Output
+from beta9.abstractions.pod import Pod, PodInstance
+from beta9.abstractions.queue import SimpleQueue as Queue
+from beta9.abstractions.taskqueue import TaskQueue as task_queue
+from beta9.abstractions.volume import CloudBucket, CloudBucketConfig, Volume
+from beta9.client.deployment import Deployment
+from beta9.client.task import Task
+from beta9.type import GpuType, PythonVersion, QueueDepthAutoscaler
+
+from .client.client import Client
+
+__all__ = [
+    "Map",
+    "Image",
+    "Queue",
+    "Volume",
+    "CloudBucket",
+    "CloudBucketConfig",
+    "task_queue",
+    "function",
+    "endpoint",
+    "asgi",
+    "realtime",
+    "Container",
+    "env",
+    "PythonVersion",
+    "GpuType",
+    "Output",
+    "QueueDepthAutoscaler",
+    "experimental",
+    "schedule",
+    "integrations",
+    "Bot",
+    "BotContext",
+    "BotEventType",
+    "BotLocation",
+    "Pod",
+    "PodInstance",
+    "Client",
+    "Task",
+    "Deployment",
+    "schema",
+]
