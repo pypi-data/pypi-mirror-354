8206b4f2f0761d1baaa780fb3d0a6542a4b48135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:08 +0000

    Add package airflow-provider-coiled/0.0.21/airflow_provider_coiled-0.0.21-py3-none-any.whl

---
+++ packages/airflow-provider-coiled/airflow_provider_coiled-0.0.21-py3-none-any.whl/coiled_provider/operators/cluster.py
@@ -0,0 +1,55 @@
+from typing import TYPE_CHECKING, Callable
+from airflow.sdk.bases.decorator import task_decorator_factory
+from airflow.operators.python import PythonVirtualenvOperator, PythonOperator
+from airflow.decorators.base import DecoratedOperator
+from airflow.sdk.bases.decorator import TaskDecorator
+from airflow.providers.standard.decorators.python import _PythonDecoratedOperator
+from tempfile import TemporaryDirectory
+from airflow.utils.python_virtualenv import prepare_virtualenv, write_python_script
+import coiled
+
+
+class CoiledClusterOperator(PythonVirtualenvOperator):
+    def __init__(self, python_callable: Callable, **kwargs):
+        requirements = [
+            "coiled",
+            "dask[complete]",
+            "tornado==6.4.2",
+            "pandas==2.1.4",
+            "numpy==1.26.4",
+            "geopandas",
+            "dask_geopandas",
+            "s3fs",
+            "pyarrow",
+            "matplotlib",
+        ]
+        index_urls = ["https://pypi.org/simple"]
+
+        super().__init__(
+            requirements=requirements,
+            index_urls=index_urls,
+            python_callable=python_callable,
+            **kwargs,
+        )
+        self.original_callable = python_callable
+
+    def python_callable(self):
+        print("doing something else")
+        self.original_callable()
+
+
+class _CoiledClusterDecoratedOperator(_PythonDecoratedOperator, CoiledClusterOperator):
+    custom_operator_name = "@task.coiled_cluster"
+
+
+def coiled_cluster_task(
+    python_callable: Callable | None = None,
+    multiple_outputs: bool | None = None,
+    **kwargs,
+) -> TaskDecorator:
+    return task_decorator_factory(
+        python_callable=python_callable,
+        multiple_outputs=multiple_outputs,
+        decorated_operator_class=_CoiledClusterDecoratedOperator,
+        **kwargs,
+    )
