d28826f06d124af7a1a50ff7b4c2e9e3ed21a011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:26 +0000

    Add package dnv-sesamx/1.0.1/dnv_sesamx-1.0.1-py3-none-any.whl

---
+++ packages/dnv-sesamx/dnv_sesamx-1.0.1-py3-none-any.whl/dnv/sesamx/load_assemblies.py
@@ -0,0 +1,43 @@
+# This script is used to load SesamX assemblies and find the SesamX application executable path.
+
+import os
+from pythonnet import load
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+assembly_dir = os.path.join(script_dir, r'.dlls') 
+
+load("coreclr", runtime_config= os.path.join(script_dir, 'runtimeconfig.json'))
+
+import clr
+import json
+
+def load_assemblies():
+    """Load required assemblies for SesamX."""
+
+    # load DNV.AppVersionManager.Core to find SesamX application
+    try:
+        clr.AddReference(os.path.join(assembly_dir, 'DNV.ApplicationVersionManager.Core.dll'))
+        from DNV.ApplicationVersionManager.Core import AppVersionServiceLocator
+    except Exception as e:
+        raise RuntimeError(f"Failed to load Application Version Manager: {e}")
+
+    # locate the SesamX application executable path with the DLLs
+    try:
+        sesamX_app = AppVersionServiceLocator.Default.AppVersionSearcher.GetDefaultApplication('SesamX')
+        if sesamX_app is None:
+            raise RuntimeError("SesamX application not found. Please ensure it is installed")
+        
+        sesamX_libraries = os.path.dirname(os.path.abspath(sesamX_app.ExeFilePath))
+    except Exception as e:
+        raise RuntimeError(f"Failed to locate SesamX application: {e}")
+
+    # load SesamX assemblies
+    try:
+        # Open and read the JSON file
+        with open(os.path.join(sesamX_libraries, 'PythonScriptConfig.json'), 'r') as file:
+            dll_config = json.load(file)
+
+        for dll in dll_config['dlls']:
+            clr.AddReference(os.path.join(sesamX_libraries, dll))
+    except Exception as e:
+        raise RuntimeError(f"Failed to load SesamX assemblies: {e}")
