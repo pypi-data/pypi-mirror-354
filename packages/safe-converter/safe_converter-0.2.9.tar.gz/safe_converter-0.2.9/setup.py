663ebb06ee6f808e5e3ae38663921f78ed5c643e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:06:36 +0000

    Add package safe-converter/0.2.9/safe_converter-0.2.9.tar.gz

---
+++ packages/safe-converter/safe_converter-0.2.9.tar.gz/safe_converter-0.2.9/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='safe_converter',
+    version='0.2.9',
+    author='31',
+    author_email='xhispeco2018@gmail.com',
+    description='Универсальный конвертор',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/31iwnl/safe_converter',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
