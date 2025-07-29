f0b7045b751bb076ab898911985ff1c939ebf7cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:06 +0000

    Add package gravity-toolkit/1.2.4/gravity_toolkit-1.2.4.tar.gz

---
+++ packages/gravity-toolkit/gravity_toolkit-1.2.4.tar.gz/gravity_toolkit-1.2.4/test/test_legendre.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+u"""
+test_legendre.py (11/2021)
+"""
+import numpy as np
+import gravity_toolkit as gravtk
+
+# PURPOSE: test unnormalized Legendre polynomials
+def test_unnormalized(l=3, x=[-1.0, -0.9, -0.8]):
+    obs = gravtk.legendre(l, x)
+    expected = np.array([
+        [-1.00000, -0.47250, -0.08000],
+        [0.00000, -1.99420, -1.98000],
+        [0.00000, -2.56500, -4.32000],
+        [0.00000, -1.24229, -3.24000]
+    ])
+    assert np.isclose(obs, expected, atol=1e-05).all()
+
+# PURPOSE: test fully-normalized Legendre polynomials
+def test_normalized(l=3, x=[-1.0, -0.9, -0.8]):
+    obs = gravtk.legendre(l, x, NORMALIZE=True)
+    expected = np.array([
+        [-2.64575, -1.25012, -0.21166],
+        [-0.00000, 2.15398, 2.13864],
+        [0.00000, -0.87611, -1.47556],
+        [-0.00000, 0.17323, 0.45180]
+    ])
+    assert np.isclose(obs, expected, atol=1e-05).all()
+
+# PURPOSE: test fully-normalized zonal Legendre polynomials
+def test_zonal(l=3, x=[-1.0, -0.9, -0.8]):
+    obs,_ = gravtk.legendre_polynomials(l, x)
+    expected = np.array([
+        [1.00000, 1.00000, 1.00000],
+        [-1.73205, -1.55885, -1.38564],
+        [2.23607, 1.59879, 1.02859],
+        [-2.64575, -1.25012, -0.21166],
+    ])
+    assert np.isclose(obs, expected, atol=1e-05).all()
+
+# PURPOSE: compare fully-normalized Legendre polynomials
+def test_plms(l=240, x=0.1):
+    obs = gravtk.legendre(l, x, NORMALIZE=True)
+    # calculate associated Legendre polynomials
+    holmes,_ = gravtk.plm_holmes(l, x)
+    colombo,_ = gravtk.plm_colombo(l, x)
+    mohlenkamp,_ = gravtk.plm_mohlenkamp(l, x)
+    # compare Legendre polynomials
+    assert np.isclose(obs, holmes[l,:]).all()
+    assert np.isclose(holmes, colombo).all()
+    assert np.isclose(holmes, mohlenkamp).all()
---
+++ packages/gravity-toolkit/gravity_toolkit-1.2.4.tar.gz/gravity_toolkit-1.2.4/test/test_point_masses.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+u"""
+test_point_masses.py (01/2023)
+UPDATE HISTORY:
+    Updated 01/2023: single implicit import of gravity toolkit
+    Written 02/2021
+"""
+import pytest
+import numpy as np
+import gravity_toolkit as gravtk
+
+# parameterize the number of point masses
+@pytest.mark.parametrize("NPTS", np.random.randint(2,2000,size=1))
+def test_point_masses(NPTS):
+    # create spatial grid
+    dlon,dlat = (1.0,1.0)
+    lat = np.arange(90.0 - dlat/2.0, -90.0 - dlat/2.0, -dlat)
+    lon = np.arange(-180.0 + dlon/2.0, 180.0 + dlon/2.0, dlon)
+    gridlon,gridlat = np.meshgrid(lon,lat)
+    nlat, nlon = np.shape(gridlon)
+
+    # parameterize point masses
+    LAT = lat[0]-dlat*np.random.randint(0,nlat,size=NPTS)
+    LON  = lon[0]+dlon*np.random.randint(0,nlon,size=NPTS)
+    MASS = 100.0 - 200.0*np.random.randn(NPTS)
+
+    # create test gridded field
+    data = np.zeros((nlat, nlon))
+    for i in range(NPTS):
+        indy,indx = np.nonzero((gridlat == LAT[i]) & (gridlon == LON[i]))
+        data[indy,indx] += MASS[i]
+
+    # path to load Love numbers file
+    love_numbers_file = gravtk.utilities.get_data_path(
+        ['data','love_numbers'])
+    # read load Love numbers
+    LOVE = gravtk.read_love_numbers(love_numbers_file)
+    # calculate harmonics and degree amplitudes for each case
+    grid_Ylms = gravtk.gen_stokes(data, lon, lat,
+        LMAX=60, UNITS=2, LOVE=LOVE)
+    point_Ylms = gravtk.gen_point_load(MASS, LON, LAT,
+        LMAX=60, UNITS=2, LOVE=LOVE)
+
+    # check that harmonic data is equal to machine precision
+    difference_Ylms = grid_Ylms.copy()
+    difference_Ylms.subtract(point_Ylms)
+    harmonic_eps = np.finfo(np.float32).eps
+    assert np.all(np.abs(difference_Ylms.clm) < harmonic_eps)
+    # verify that the degree amplitudes are within tolerance
+    difference_amplitude = grid_Ylms.amplitude - point_Ylms.amplitude
+    assert np.all(np.abs(difference_amplitude) < harmonic_eps)
