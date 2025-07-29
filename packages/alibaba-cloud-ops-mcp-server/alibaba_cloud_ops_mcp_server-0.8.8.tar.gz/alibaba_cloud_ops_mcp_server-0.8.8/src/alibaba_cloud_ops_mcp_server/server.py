6fc167729b47d2d968b201483f36c584347e9bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:59 +0000

    Add package alibaba-cloud-ops-mcp-server/0.8.8/alibaba_cloud_ops_mcp_server-0.8.8.tar.gz

---
+++ packages/alibaba-cloud-ops-mcp-server/alibaba_cloud_ops_mcp_server-0.8.8.tar.gz/alibaba_cloud_ops_mcp_server-0.8.8/src/alibaba_cloud_ops_mcp_server/server.py
@@ -0,0 +1,44 @@
+from mcp.server.fastmcp import FastMCP
+import click
+import logging
+
+from alibaba_cloud_ops_mcp_server.config import config
+from alibaba_cloud_ops_mcp_server.tools import cms_tools, oos_tools, oss_tools, api_tools
+
+logger = logging.getLogger(__name__)
+
+
+@click.command()
+@click.option(
+    "--transport",
+    type=click.Choice(["stdio", "sse", "streamable-http"]),
+    default="stdio",
+    help="Transport type",
+)
+@click.option(
+    "--port",
+    type=int,
+    default=8000,
+    help="Port number",
+)
+def main(transport: str, port: int):
+    # Create an MCP server
+    mcp = FastMCP(
+        name="alibaba-cloud-ops-mcp-server",
+        port=port
+    )
+    for tool in oos_tools.tools:
+        mcp.add_tool(tool)
+    for tool in cms_tools.tools:
+        mcp.add_tool(tool)
+    for tool in oss_tools.tools:
+        mcp.add_tool(tool)
+    api_tools.create_api_tools(mcp, config)
+
+    # Initialize and run the server
+    logger.debug(f'mcp server is running on {transport} mode.')
+    mcp.run(transport=transport)
+
+
+if __name__ == "__main__":
+    main()
