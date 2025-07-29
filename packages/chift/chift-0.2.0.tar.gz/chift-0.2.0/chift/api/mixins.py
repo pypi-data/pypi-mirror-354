feae0458b2e871756a7794e70e44e0e4d62964de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:28 +0000

    Add package chift/0.2.0/chift-0.2.0.tar.gz

---
+++ packages/chift/chift-0.2.0.tar.gz/chift-0.2.0/chift/api/mixins.py
@@ -0,0 +1,181 @@
+from typing import Generic, TypeVar
+
+from chift.api.client import ChiftClient
+from chift.openapi.models import ObjectWithRawData
+
+T = TypeVar("T")
+
+
+class BaseMixin(object):
+    def __init__(self, consumer_id, connection_id):
+        self.chift_vertical = (
+            hasattr(self, "chift_vertical") and self.chift_vertical or None
+        )
+        self.chift_model = hasattr(self, "chift_model") and self.chift_model or None
+        self.extra_path = hasattr(self, "extra_path") and self.extra_path or None
+        self.consumer_id = consumer_id
+        self.connection_id = connection_id
+
+
+class DeleteMixin(BaseMixin, Generic[T]):
+    def delete(self, chift_id, client=None, params=None, client_request_id=None) -> T:
+        if not client:
+            client = ChiftClient()
+        client.consumer_id = self.consumer_id
+        client.connection_id = self.connection_id
+        client.raw_data = False
+        client.client_request_id = client_request_id
+
+        return client.delete_one(
+            self.chift_vertical,
+            self.chift_model,
+            chift_id=chift_id,
+            extra_path=self.extra_path,
+            params=params,
+        )
+
+
+class ReadMixin(BaseMixin, Generic[T]):
+    def get(
+        self, chift_id, client=None, params=None, map_model=True, raw_data=False
+    ) -> T:
+        if not client:
+            client = ChiftClient()
+        client.consumer_id = self.consumer_id
+        client.connection_id = self.connection_id
+        client.raw_data = raw_data
+        client.client_request_id = None
+
+        json_data = client.get_one(
+            self.chift_vertical,
+            self.chift_model,
+            chift_id=chift_id,
+            extra_path=self.extra_path,
+            params=params,
+        )
+        if raw_data:
+            if map_model:
+                return ObjectWithRawData(
+                    chift_data=self.model(**json_data),
+                    raw_data=json_data.get("raw_data") or {},
+                )
+            else:
+                return json_data
+        return self.model(**json_data) if map_model else json_data
+
+
+class CreateMixin(BaseMixin, Generic[T]):
+    def create(
+        self, data, client=None, params=None, map_model=True, client_request_id=None
+    ) -> T:
+        if not client:
+            client = ChiftClient()
+        client.consumer_id = self.consumer_id
+        client.connection_id = self.connection_id
+        client.raw_data = False
+        client.client_request_id = client_request_id
+
+        json_data = client.post_one(
+            self.chift_vertical,
+            self.chift_model,
+            data,
+            extra_path=self.extra_path,
+            params=params,
+        )
+
+        return self.model(**json_data) if map_model else json_data
+
+
+class UpdateMixin(BaseMixin, Generic[T]):
+    def update(
+        self,
+        chift_id,
+        data,
+        client=None,
+        params=None,
+        map_model=True,
+        client_request_id=None,
+    ) -> T:
+        if not client:
+            client = ChiftClient()
+        client.consumer_id = self.consumer_id
+        client.connection_id = self.connection_id
+        client.raw_data = False
+        client.client_request_id = client_request_id
+
+        json_data = client.update_one(
+            self.chift_vertical,
+            self.chift_model,
+            chift_id,
+            data,
+            extra_path=self.extra_path,
+            params=params,
+        )
+
+        return self.model(**json_data) if map_model else json_data
+
+
+class PaginationMixin(BaseMixin, Generic[T]):
+    def all(
+        self, params=None, client=None, map_model=True, limit=None, raw_data=False
+    ) -> list[T]:
+        if not client:
+            client = ChiftClient()
+        client.consumer_id = self.consumer_id
+        client.connection_id = self.connection_id
+        client.raw_data = raw_data
+        client.client_request_id = None
+
+        if not params:
+            params = {}
+
+        size = limit if limit and limit < 100 else 100
+
+        all_items = []
+        page = 1
+
+        while True:
+            json_data = client.get_all(
+                self.chift_vertical,
+                self.chift_model,
+                params={"page": page, "size": size} | params,
+                extra_path=self.extra_path,
+            )
+            if raw_data:
+                return json_data.get("raw_data") or {}
+            all_items.extend(
+                [
+                    self.model(**item) if map_model else item
+                    for item in json_data.get("items", [])
+                ]
+            )
+            page += 1
+
+            if limit and len(all_items) >= limit:
+                break
+
+            if len(all_items) >= json_data.get("total", 0) or not json_data.get(
+                "items"
+            ):
+                break
+
+        return all_items
+
+
+class ListMixin(BaseMixin, Generic[T]):
+    def all(self, params=None, client=None) -> list[T]:
+        if not client:
+            client = ChiftClient()
+        client.consumer_id = self.consumer_id
+        client.connection_id = self.connection_id
+        client.raw_data = False
+        client.client_request_id = None
+
+        json_data = client.get_all(
+            self.chift_vertical,
+            self.chift_model,
+            params=params,
+            extra_path=self.extra_path,
+        )
+
+        return [self.model(**item) for item in json_data]
