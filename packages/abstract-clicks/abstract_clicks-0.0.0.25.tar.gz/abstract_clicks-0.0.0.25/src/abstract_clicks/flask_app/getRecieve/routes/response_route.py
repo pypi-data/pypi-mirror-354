88eaa5ec1331e67a21ff29efbe8662cb108c59fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:41 +0000

    Add package abstract-clicks/0.0.0.25/abstract_clicks-0.0.0.25.tar.gz

---
+++ packages/abstract-clicks/abstract_clicks-0.0.0.25.tar.gz/abstract_clicks-0.0.0.25/src/abstract_clicks/flask_app/getRecieve/routes/response_route.py
@@ -0,0 +1,10 @@
+from abstract_flask import *
+from ...imports import *
+response_bp,logger = get_bp('response_bp',__name__,
+                            url_prefix=URL_PREFIX,
+                            static_folder=RESPONSE_DIR)
+@response_bp.route('/response',methods=["POST","GET"])
+def ReSpOnSe():
+    data = parse_and_spec_vars(flask_request,['response'])
+    response = data.get('response')
+    safe_dump_to_file(data=response,file_path=RESPONSE_FILE_PATH)
