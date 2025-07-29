2ac0c6b0bab54ff30c89c374b1107850fe4fbf40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:29 +0000

    Add package alation-ai-agent-langchain/0.3.0/alation_ai_agent_langchain-0.3.0.tar.gz

---
+++ packages/alation-ai-agent-langchain/alation_ai_agent_langchain-0.3.0.tar.gz/alation_ai_agent_langchain-0.3.0/alation_ai_agent_langchain/toolkit.py
@@ -0,0 +1,10 @@
+from alation_ai_agent_sdk import AlationAIAgentSDK
+
+from .tool import get_alation_context_tool, get_alation_data_products_tool
+
+
+def get_tools(sdk: AlationAIAgentSDK):
+    return [
+        get_alation_context_tool(sdk),
+        get_alation_data_products_tool(sdk),
+    ]
