44b49cb94d8f1449cfa1699a62e4c17656cbee99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:07:09 +0000

    Add package mrtutils/0.3.32/mrtutils-0.3.32-py3-none-any.whl

---
+++ packages/mrtutils/mrtutils-0.3.32-py3-none-any.whl/mrtutils/templates/gen/c.h
@@ -0,0 +1,28 @@
+/**
+  *@file ${obj.name}.h
+  *@brief header for ${obj.desc}
+  *@author ${obj.user}
+  *@date ${obj.gentime}
+  */
+
+#pragma once
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+
