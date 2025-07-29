9d7e8f0b262954f0d18ec20317f2b91198adcafe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:21 +0000

    Add package maptor/0.0.4/maptor-0.0.4-py3-none-any.whl

---
+++ packages/maptor/maptor-0.0.4-py3-none-any.whl/maptor/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.0.4"
+__author__ = "David Timothy"
+__description__ = "Multiphase Adaptive Trajectory Optimizer"
+
+def status():
+    return "Package name reserved - Full library coming soon"
