b99e9910807ce5f0f2bfe0682a9f8c4a966e0192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:51 +0000

    Add package scypy/0.0.5/scypy-0.0.5.tar.gz

---
+++ packages/scypy/scypy-0.0.5.tar.gz/scypy-0.0.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name='scypy',
+  version='0.0.5',
+  author='lalala',
+  author_email='vikki.flover@gmail.com',
+  description='///',
+  long_description='///',
+  long_description_content_type='text/markdown',
+  url='https://github.com/Vika-K228',
+  packages= ['scypy'],
+  python_requires='>=3.0'
+)
