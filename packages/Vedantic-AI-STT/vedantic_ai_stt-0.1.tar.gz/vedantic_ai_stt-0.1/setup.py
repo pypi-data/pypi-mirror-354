8dd3e30bd28b1c8d3444d465c021ec58205aa704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:17 +0000

    Add package Vedantic-AI-STT/0.1/vedantic_ai_stt-0.1.tar.gz

---
+++ packages/Vedantic-AI-STT/vedantic_ai_stt-0.1.tar.gz/vedantic_ai_stt-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='Vedantic_AI_STT',
+    version='0.1',
+    author='VYR Indrustries',
+    author_email="mrscientist1813@gmail.com",
+    description="this is a Vedantic AI STT package created by VYR Indurstries",
+    
+)
+packages = find_packages(),
+install_requirments = [ 'selenium', 'webdriver-manager', ]
+
+
+
