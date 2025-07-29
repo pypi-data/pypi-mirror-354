09707261b694333d5f90fb9c6e86b05ef663c9b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:35 +0000

    Add package ai-parrot/0.8.8/ai_parrot-0.8.8-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/ai-parrot/ai_parrot-0.8.8-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/parrot/loaders/__init__.py
@@ -0,0 +1,9 @@
+####
+# Copyright 2023 Jesus Lara.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#
+# Loaders.
+# Open, extract and load data from different sources.
+#####
+from langchain.docstore.document import Document
+from .abstract import AbstractLoader
