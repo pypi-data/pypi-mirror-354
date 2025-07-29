b1948d7b389b98a2802f3aa4ec4d93793a4ab08c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:30 +0000

    Add package lin3ar/0.0.1/lin3ar-0.0.1.tar.gz

---
+++ packages/lin3ar/lin3ar-0.0.1.tar.gz/lin3ar-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lin3ar',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Twoje Imię',
+    author_email='twoj@email.com',
+    description='Placeholder for lin3ar ML library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/twoj-user/lin3ar',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
+
