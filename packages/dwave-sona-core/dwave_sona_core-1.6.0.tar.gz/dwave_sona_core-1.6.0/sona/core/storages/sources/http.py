265811c4a85070a8997bdf6cb7809f1da37caa74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:47 +0000

    Add package dwave-sona-core/1.6.0/dwave_sona_core-1.6.0.tar.gz

---
+++ packages/dwave-sona-core/dwave_sona_core-1.6.0.tar.gz/dwave_sona_core-1.6.0/sona/core/storages/sources/http.py
@@ -0,0 +1,29 @@
+from .base import SourceBase
+from .gdrive import GoogleDriveSource
+from .youtube import YoutubeSource
+
+try:
+    import wget
+except ImportError:
+    pass
+
+
+class HttpSource(SourceBase):
+    @classmethod
+    def download(cls, file):
+        return file.mutate(path=wget.download(file.path, out=str(cls.tmp_dir)))
+
+    @classmethod
+    def verify(cls, file):
+        if not file.path:
+            return False
+
+        is_google_drive_link = GoogleDriveSource.verify(file)
+        is_youtube_link = YoutubeSource.verify(file)
+        if is_google_drive_link or is_youtube_link:
+            return False
+
+        is_http = file.path.startswith("http://")
+        is_https = file.path.startswith("https://")
+        if_ftp = file.path.startswith("ftp://")
+        return is_http or is_https or if_ftp
