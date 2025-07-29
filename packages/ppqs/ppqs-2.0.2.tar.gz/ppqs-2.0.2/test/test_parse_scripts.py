a6b0645f3399956ad0c46ea66feef574aaad574f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:23 +0000

    Add package ppqs/2.0.2/ppqs-2.0.2.tar.gz

---
+++ packages/ppqs/ppqs-2.0.2.tar.gz/ppqs-2.0.2/test/test_parse_scripts.py
@@ -0,0 +1,248 @@
+from contextlib import nullcontext
+from pathlib import Path
+
+import pytest
+
+from py_proj_quick_scripts import (
+    InvalidScriptError,
+    parse_scripts,
+)
+
+from . import in_dir, write_file
+
+
+@pytest.mark.parametrize(
+    "script_toml,expectation",
+    [
+        pytest.param(
+            """
+            [some.section]
+            """,
+            pytest.raises(
+                InvalidScriptError, match=r"non-empty '\[tool.ppqs.scripts\]' section"
+            ),
+            id="missing section",
+        ),
+        pytest.param(
+            """
+            [tool.ppqs.scripts]
+            """,
+            pytest.raises(
+                InvalidScriptError, match=r"non-empty '\[tool.ppqs.scripts\]' section"
+            ),
+            id="empty section",
+        ),
+        pytest.param(
+            """
+            [tool.ppqs.scripts]
+            init = 123
+            """,
+            pytest.raises(
+                InvalidScriptError, match="either a string or a list of lists"
+            ),
+            id="wrong type",
+        ),
+        pytest.param(
+            """
+            [tool.ppqs.scripts]
+            init = "pip install -r requirements.txt"
+            """,
+            nullcontext(
+                {
+                    "init": {
+                        "description": "Run init script",
+                        "print-header": False,
+                        "commands": [["pip", "install", "-r", "requirements.txt"]],
+                    }
+                }
+            ),
+            id="string script",
+        ),
+        pytest.param(
+            """
+            [tool.ppqs.scripts]
+            init = '''
+            pip install -r requirements.txt
+            pre-commit install
+            '''
+            """,
+            nullcontext(
+                {
+                    "init": {
+                        "description": "Run init script",
+                        "print-header": False,
+                        "commands": [
+                            ["pip", "install", "-r", "requirements.txt"],
+                            ["pre-commit", "install"],
+                        ],
+                    }
+                }
+            ),
+            id="multi-line string script",
+        ),
+        pytest.param(
+            """
+            [tool.ppqs.scripts]
+            -invalid-name = "something"
+            """,
+            pytest.raises(InvalidScriptError, match="may not start with '-'"),
+            id="script name starts with dash",
+        ),
+        pytest.param(
+            """
+            [tool.ppqs.scripts]
+            name_contains-2-invalid-characters = "something"
+            """,
+            pytest.raises(InvalidScriptError, match="may not contain characters '_2'"),
+            id="script name invalid characters",
+        ),
+        pytest.param(
+            """
+            [tool.ppqs.scripts]
+            some-script = { description = "something", another_key = "invalid"}
+            """,
+            pytest.raises(
+                InvalidScriptError, match="may not contain keys 'another_key'"
+            ),
+            id="script invalid keys",
+        ),
+        pytest.param(
+            """
+            [tool.ppqs.scripts.init]
+            description = "Initialise project"
+            script = "pip install -r requirements.txt"
+            """,
+            nullcontext(
+                {
+                    "init": {
+                        "description": "Initialise project",
+                        "print-header": False,
+                        "commands": [["pip", "install", "-r", "requirements.txt"]],
+                    }
+                }
+            ),
+            id="string script with description",
+        ),
+        pytest.param(
+            """
+            [tool.ppqs.scripts.init]
+            description = "Initialise project"
+            script = [
+                "some_commands",
+            ]
+            """,
+            pytest.raises(
+                InvalidScriptError, match="either a string or a list of lists"
+            ),
+            id="script is not list of lists",
+        ),
+        pytest.param(
+            """
+            [tool.ppqs.scripts]
+            init = [
+                ["pip", "install", "-r", "requirements.txt"],
+            ]
+            """,
+            nullcontext(
+                {
+                    "init": {
+                        "description": "Run init script",
+                        "print-header": False,
+                        "commands": [["pip", "install", "-r", "requirements.txt"]],
+                    }
+                }
+            ),
+            id="list script",
+        ),
+        pytest.param(
+            """
+            [tool.ppqs.scripts.init]
+            description = "Initialise project"
+            print-header = true
+            script = [
+                ["pip", "install", "-r", "requirements.txt"],
+            ]
+            """,
+            nullcontext(
+                {
+                    "init": {
+                        "description": "Initialise project",
+                        "print-header": True,
+                        "commands": [["pip", "install", "-r", "requirements.txt"]],
+                    }
+                }
+            ),
+            id="list script with description",
+        ),
+        pytest.param(
+            """
+            [tool.ppqs.scripts.init]
+            description = "Initialise project"
+            script = [
+                ["pip", "install", "-r", "requirements.txt"],
+                ["pre-commit", "install"],
+            ]
+            """,
+            nullcontext(
+                {
+                    "init": {
+                        "description": "Initialise project",
+                        "print-header": False,
+                        "commands": [
+                            ["pip", "install", "-r", "requirements.txt"],
+                            ["pre-commit", "install"],
+                        ],
+                    }
+                }
+            ),
+            id="multi-line list script with description",
+        ),
+        pytest.param(
+            """
+            [tool.ppqs.scripts]
+            paths = [
+                ["echo", ["/tmp"]],
+            ]
+            """,
+            pytest.raises(InvalidScriptError, match="must be a relative path"),
+            id="list script with absolute path",
+        ),
+        pytest.param(
+            """
+            [tool.ppqs.scripts]
+            paths = [
+                ["echo", ["a", "b"]],
+            ]
+            """,
+            nullcontext(
+                {
+                    "paths": {
+                        "description": "Run paths script",
+                        "print-header": False,
+                        "commands": [["echo", str(Path("a", "b"))]],
+                    }
+                }
+            ),
+            id="list script with relative path",
+        ),
+    ],
+)
+def test_parse_scripts(tmp_path, script_toml, expectation):
+    """
+    Test parse_scripts().
+    """
+
+    pyproject_path = tmp_path / "pyproject.toml"
+    pyproject_toml = f"""
+    [project]
+    name = "test"
+    {script_toml}
+    """
+
+    write_file(pyproject_path, pyproject_toml)
+
+    with in_dir(tmp_path):
+        with expectation as expected_result:
+            project_name, scripts = parse_scripts(pyproject_path)
+            assert project_name == "test"
+            assert scripts == expected_result
