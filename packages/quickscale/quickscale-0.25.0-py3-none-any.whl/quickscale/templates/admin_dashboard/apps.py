a299041416734bdf1f17cf1a7af9451b69fd80f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:06:04 +0000

    Add package quickscale/0.25.0/quickscale-0.25.0-py3-none-any.whl

---
+++ packages/quickscale/quickscale-0.25.0-py3-none-any.whl/quickscale/templates/admin_dashboard/apps.py
@@ -0,0 +1,7 @@
+"""Admin dashboard application configuration."""
+from django.apps import AppConfig
+
+class AdminDashboardConfig(AppConfig):
+    """Configure the admin dashboard application."""
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'admin_dashboard'
