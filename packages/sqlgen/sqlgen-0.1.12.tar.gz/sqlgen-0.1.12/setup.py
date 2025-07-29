54bb88ade7f01a9bb753a34f649cf81cf7a724c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:46 +0000

    Add package sqlgen/0.1.12/sqlgen-0.1.12.tar.gz

---
+++ packages/sqlgen/sqlgen-0.1.12.tar.gz/sqlgen-0.1.12/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="sqlgen",
+    version="0.1.12",
+    author="ryohei-iwamoto",
+    author_email="celeron5576@gmail.com",
+    description="Convert CSV or pandas DataFrame to SQL INSERT statements with batching, null handling, and size-limited output.",
+    url="https://github.com/ryohei-iwamoto/sqlgen",
+    py_modules=["sqlgen"],
+    install_requires=["pandas"],
+    extras_require={"progress": ["tqdm"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
