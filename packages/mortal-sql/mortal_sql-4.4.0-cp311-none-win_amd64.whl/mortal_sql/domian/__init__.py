4e22ce7bf2ce7b244b0cf30936099e13096928d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:07:09 +0000

    Add package mortal-sql/4.4.0/mortal_sql-4.4.0-cp311-none-win_amd64.whl

---
+++ packages/mortal-sql/mortal_sql-4.4.0-cp311-none-win_amd64.whl/mortal_sql/domian/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Author: MaJian
+@Time: 2024/5/18 13:31
+@SoftWare: PyCharm
+@Project: mortal
+@File: __init__.py
+"""
---
+++ packages/mortal-sql/mortal_sql-4.4.0-cp311-none-win_amd64.whl/mortal_sql/utils/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Author: MaJian
+@Time: 2024/2/6 15:31
+@SoftWare: PyCharm
+@Project: mortal
+@File: __init__.py
+"""
