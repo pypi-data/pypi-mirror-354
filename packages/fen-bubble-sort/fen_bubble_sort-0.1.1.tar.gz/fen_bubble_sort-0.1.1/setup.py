0e3e0d28da99b677cd5a29131b33d3fddbf4af5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:08:23 +0000

    Add package fen-bubble-sort/0.1.1/fen_bubble_sort-0.1.1.tar.gz

---
+++ packages/fen-bubble-sort/fen_bubble_sort-0.1.1.tar.gz/fen_bubble_sort-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fen-bubble-sort",
+    version="0.1.1",
+    author="Fenil",
+    description="A simple bubble sort implementation",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
