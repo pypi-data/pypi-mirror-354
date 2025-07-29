4adf73ebe1a77239a9b08f9f385fe44f236b1b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:42 +0000

    Add package iptvpy/1.0.1/iptvpy-1.0.1-py3-none-any.whl

---
+++ packages/iptvpy/iptvpy-1.0.1-py3-none-any.whl/iptvpy/__init__.py
@@ -0,0 +1,12 @@
+'''
+iptvpy - A modular Python library for IPTV portal interaction.
+Author : भाग्य ज्योति (Bhagya Jyoti)
+License : Apache License Version 2.0
+Homepage: https://github.com/BhagyaJyoti22006/iptvpy
+'''
+
+from .IPTV import IPTV
+
+__all__ = ["IPTV"]
+__version__ = "1.0.1"
+
