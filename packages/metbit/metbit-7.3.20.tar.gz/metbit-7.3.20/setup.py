d12245a938437de323fd8a46d34ec2a9d283fc84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:53 +0000

    Add package metbit/7.3.20/metbit-7.3.20.tar.gz

---
+++ packages/metbit/metbit-7.3.20.tar.gz/metbit-7.3.20/setup.py
@@ -0,0 +1,46 @@
+import io
+from os.path import abspath, dirname, join
+from setuptools import find_packages, setup
+
+
+HERE = dirname(abspath(__file__))
+LOAD_TEXT = lambda name: io.open(join(HERE, name), encoding='UTF-8').read()
+DESCRIPTION = '\n\n'.join(LOAD_TEXT(_) for _ in [
+    'README.rst'
+])
+
+setup(
+  name = 'metbit',      
+  packages = ['metbit'], 
+  version = '7.3.20',  
+  license='MIT', 
+  description = 'Metabolomics data analysis and visualization tools.',
+  author = 'aeiwz',                 
+  author_email = 'theerayut_aeiw_123@hotmail.com',   
+  url = 'https://github.com/aeiwz/metbit.git',  
+  download_url = 'https://github.com/aeiwz/metbit/archive/refs/tags/V7.3.20.tar.gz',  
+  keywords = ['Omics', 'Multivariate analysis', 'Visualization', 'Data Analysis', 'Metabolomics', 'Chemometrics'],
+  install_requires=[            
+          'scikit-learn',
+          'pandas',
+          'numpy',
+          'matplotlib',
+          'seaborn',
+          'scipy',
+          'statsmodels',
+          'plotly',
+          'pyChemometrics',
+          'lingress',
+          'tqdm',
+          'dash',
+          'pingouin',
+          'nmrglue',
+          ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Education',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',        
+    'Programming Language :: Python :: 3.12',
+  ],
+)
