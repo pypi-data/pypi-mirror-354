5d07ae44881fecae904b7e15e409890cf04c6aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:28 +0000

    Add package test-soft-assertion-tool/0.1.0/test_soft_assertion_tool-0.1.0.tar.gz

---
+++ packages/test-soft-assertion-tool/test_soft_assertion_tool-0.1.0.tar.gz/test_soft_assertion_tool-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="test_soft_assertion_tool",
+    version="0.1.0",
+    description="A pytest plugin for soft assertions with automatic report.",
+    author="WYK",
+    packages=find_packages(),
+    install_requires=[
+        "loguru",
+        "pytest",
+    ],
+    entry_points={
+        "pytest11": [
+            "assertion = assertion.pytest_softassert",
+        ],
+    },
+    classifiers=[
+        "Framework :: Pytest",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
