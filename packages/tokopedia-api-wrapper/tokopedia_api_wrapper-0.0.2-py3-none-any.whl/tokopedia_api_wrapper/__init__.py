4835a6cc29647a816f773f15b9ea704086dc2e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:06:59 +0000

    Add package tokopedia-api-wrapper/0.0.2/tokopedia_api_wrapper-0.0.2-py3-none-any.whl

---
+++ packages/tokopedia-api-wrapper/tokopedia_api_wrapper-0.0.2-py3-none-any.whl/tokopedia_api_wrapper/__init__.py
@@ -0,0 +1,13 @@
+from .__version__ import (
+    __title__,
+    __url__,
+    __version__,
+    __author__,
+    __author_email__,
+    __license__,
+    __copyright__,
+)
+
+from .tokopedia import Tokopedia
+from .schemas.response_schema import ResponseSchema
+from .utils.faker import TokopediaFaker
