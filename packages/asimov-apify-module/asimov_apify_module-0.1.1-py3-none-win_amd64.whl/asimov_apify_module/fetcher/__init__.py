c7374524410652439effecf272cc36cf8331936d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:47 +0000

    Add package asimov-apify-module/0.1.1/asimov_apify_module-0.1.1-py3-none-win_amd64.whl

---
+++ packages/asimov-apify-module/asimov_apify_module-0.1.1-py3-none-win_amd64.whl/asimov_apify_module/fetcher/__init__.py
@@ -0,0 +1,8 @@
+import sys
+
+__version__ = "0.1.1"
+
+def main(argv=sys.argv):
+    import os, subprocess
+    argv = [os.path.join(os.path.dirname(__file__), "asimov-apify-fetcher.exe"), *argv[1:]]
+    subprocess.call(argv)
