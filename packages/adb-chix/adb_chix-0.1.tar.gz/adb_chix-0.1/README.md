4acdb8c09894e09bd7522a345f28a0b41dc043dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:33 +0000

    Add package adb-chix/0.1/adb_chix-0.1.tar.gz

---
+++ packages/adb-chix/adb_chix-0.1.tar.gz/adb_chix-0.1/README.md
@@ -0,0 +1,9 @@
+### 请使用termux 并且运行以下命令确保环境完整
+```shell
+pkg update #初次安装termux更新源
+pkg install android-tools #安装adb
+pkg install python #安装python
+pip install flask #安装flask
+pip install adb-chix #安装炽心adb
+adb-chix #启动adb web服务
+```
---
+++ packages/adb-chix/adb_chix-0.1.tar.gz/adb_chix-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup,find_packages
+setup(
+    name='adb-chix',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'flask'
+    ],
+    entry_points={
+        'console_scripts':[
+            'adb-chix=adb_chix:run_server'
+        ]
+    }
+)
