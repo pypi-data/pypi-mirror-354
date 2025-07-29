0cd8cc800b3d1da93d8388ba78e7a61b92fd6d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:06:55 +0000

    Add package fusion-bench/0.2.17/fusion_bench-0.2.17.tar.gz

---
+++ packages/fusion-bench/fusion_bench-0.2.17.tar.gz/fusion_bench-0.2.17/fusion_bench/utils/__init__.py
@@ -0,0 +1,15 @@
+# flake8: noqa: F401
+import importlib
+from typing import Iterable
+
+from . import data, functools, path
+from .cache_utils import *
+from .devices import *
+from .dtype import parse_dtype
+from .fabric import seed_everything_by_time
+from .instantiate_utils import instantiate, is_instantiable
+from .misc import *
+from .packages import import_object
+from .parameters import *
+from .timer import timeit_context
+from .lazy_state_dict import LazyStateDict
