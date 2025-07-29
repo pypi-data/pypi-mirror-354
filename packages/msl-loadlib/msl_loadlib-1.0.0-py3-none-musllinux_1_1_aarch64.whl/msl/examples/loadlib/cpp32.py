397076518fa1dc62bfd85ca081dc6553a3ed4b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:07:39 +0000

    Add package msl-loadlib/1.0.0/msl_loadlib-1.0.0-py3-none-musllinux_1_1_aarch64.whl

---
+++ packages/msl-loadlib/msl_loadlib-1.0.0-py3-none-musllinux_1_1_aarch64.whl/msl/examples/loadlib/cpp32.py
@@ -0,0 +1,368 @@
+"""Wrapper around a 32-bit C++ library.
+
+Example of a server that loads a 32-bit library, [cpp_lib][cpp-lib],
+in a 32-bit Python interpreter to host the library. The corresponding [Cpp64][] class
+is created in a 64-bit Python interpreter and the [Cpp64][] class sends requests
+to the [Cpp32][] class which calls the 32-bit library to execute the request and
+then returns the response from the library.
+"""
+
+from __future__ import annotations
+
+import ctypes
+import math
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from msl.loadlib import Server32
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+    from ctypes import Array
+
+
+class Cpp32(Server32):
+    """Wrapper around a 32-bit C++ library."""
+
+    def __init__(self, host: str, port: int) -> None:
+        """Wrapper around a 32-bit C++ library.
+
+        This class demonstrates how to send/receive various data types to/from a
+        32-bit C++ library via [ctypes][]{:target="_blank"}.
+
+        Args:
+            host: The IP address (or hostname) to use for the server.
+            port: The port to open for the server.
+        """
+        # By not specifying the extension of the library file the server will open
+        # the appropriate file based on the operating system.
+        path = Path(__file__).parent / "cpp_lib32"
+        super().__init__(path, "cdll", host, port)
+
+    def add(self, a: int, b: int) -> int:
+        """Add two integers.
+
+        The corresponding C++ code is
+
+        ```cpp
+        int add(int a, int b) {
+            return a + b;
+        }
+        ```
+
+        See the corresponding [Cpp64.add][msl.examples.loadlib.Cpp64.add] method.
+
+        Args:
+            a: First integer.
+            b: Second integer.
+
+        Returns:
+            The sum, `a + b`.
+        """
+        # restype and argtypes should be defined elsewhere, shown here for illustrative purposes
+        self.lib.add.restype = ctypes.c_int32
+        self.lib.add.argtypes = [ctypes.c_int32, ctypes.c_int32]
+        result: int = self.lib.add(a, b)
+        return result
+
+    def subtract(self, a: float, b: float) -> float:
+        """Subtract two floating-point numbers *('float' refers to the C++ data type)*.
+
+        The corresponding C++ code is
+
+        ```cpp
+        float subtract(float a, float b) {
+            return a - b;
+        }
+        ```
+
+        See the corresponding [Cpp64.subtract][msl.examples.loadlib.Cpp64.subtract] method.
+
+        Args:
+            a: First floating-point number.
+            b: Second floating-point number.
+
+        Returns:
+            The difference, `a - b`.
+        """
+        # restype and argtypes should be defined elsewhere, shown here for illustrative purposes
+        self.lib.subtract.restype = ctypes.c_float
+        self.lib.subtract.argtypes = [ctypes.c_float, ctypes.c_float]
+        result: float = self.lib.subtract(a, b)
+        return result
+
+    def add_or_subtract(self, a: float, b: float, *, do_addition: bool) -> float:
+        """Add or subtract two double-precision numbers *('double' refers to the C++ data type)*.
+
+        The corresponding C++ code is
+
+        ```cpp
+        double add_or_subtract(double a, double b, bool do_addition) {
+            if (do_addition) {
+                return a + b;
+            } else {
+                return a - b;
+            }
+        }
+        ```
+
+        See the corresponding [Cpp64.add_or_subtract][msl.examples.loadlib.Cpp64.add_or_subtract] method.
+
+        Args:
+            a: First double-precision number.
+            b: Second double-precision number.
+            do_addition: Whether to add or subtract the numbers.
+
+        Returns:
+            `a + b` if `do_addition` is `True` else `a - b`.
+        """
+        # restype and argtypes should be defined elsewhere, shown here for illustrative purposes
+        self.lib.add_or_subtract.restype = ctypes.c_double
+        self.lib.add_or_subtract.argtypes = [ctypes.c_double, ctypes.c_double, ctypes.c_bool]
+        result: float = self.lib.add_or_subtract(a, b, do_addition)
+        return result
+
+    def scalar_multiply(self, a: float, xin: Sequence[float]) -> list[float]:
+        """Multiply each element in an array by a number.
+
+        The corresponding C++ code is
+
+        ```cpp
+        void scalar_multiply(double a, double* xin, int n, double* xout) {
+            for (int i = 0; i < n; i++) {
+                xout[i] = a * xin[i];
+            }
+        }
+        ```
+
+        See the corresponding [Cpp64.scalar_multiply][msl.examples.loadlib.Cpp64.scalar_multiply] method.
+
+        Args:
+            a: Scalar value.
+            xin: Array to modify.
+
+        Returns:
+            A new array with each element in `xin` multiplied by `a`.
+        """
+        # restype and argtypes should be defined elsewhere, shown here for illustrative purposes
+        self.lib.scalar_multiply.restype = None
+        self.lib.scalar_multiply.argtypes = [
+            ctypes.c_double,
+            ctypes.POINTER(ctypes.c_double),
+            ctypes.c_int32,
+            ctypes.POINTER(ctypes.c_double),
+        ]
+
+        n = len(xin)
+        c_xin = (ctypes.c_double * n)(*xin)  # convert input array to ctypes
+        c_xout = (ctypes.c_double * n)()  # allocate memory for output array
+        self.lib.scalar_multiply(a, c_xin, n, c_xout)
+        return list(c_xout)
+
+    def reverse_string_v1(self, original: str) -> str:
+        """Reverse a string (version 1).
+
+        In this method Python allocates the memory for the reversed string and
+        passes the string to C++.
+
+        The corresponding C++ code is
+
+        ```cpp
+        void reverse_string_v1(const char* original, int n, char* reversed) {
+            for (int i = 0; i < n; i++) {
+                reversed[i] = original[n-i-1];
+            }
+        }
+        ```
+
+        See the corresponding [Cpp64.reverse_string_v1][msl.examples.loadlib.Cpp64.reverse_string_v1] method.
+
+        Args:
+            original: The original string.
+
+        Returns:
+            The string reversed.
+        """
+        # restype and argtypes should be defined elsewhere, shown here for illustrative purposes
+        self.lib.reverse_string_v1.restype = None
+        self.lib.reverse_string_v1.argtypes = [ctypes.c_char_p, ctypes.c_int32, ctypes.c_char_p]
+
+        n = len(original)
+        rev = ctypes.create_string_buffer(n)
+        self.lib.reverse_string_v1(original.encode(), n, rev)
+        return rev.raw.decode()
+
+    def reverse_string_v2(self, original: str) -> str:
+        """Reverse a string (version 2).
+
+        In this method C++ allocates the memory for the reversed string and passes
+        the string to Python.
+
+        The corresponding C++ code is
+
+        ```cpp
+        char* reverse_string_v2(char* original, int n) {
+            char* reversed = new char[n];
+            for (int i = 0; i < n; i++) {
+                reversed[i] = original[n - i - 1];
+            }
+            return reversed;
+        }
+        ```
+
+        See the corresponding [Cpp64.reverse_string_v2][msl.examples.loadlib.Cpp64.reverse_string_v2] method.
+
+        Args:
+            original: The original string.
+
+        Returns:
+            The string reversed.
+        """
+        # restype and argtypes should be defined elsewhere, shown here for illustrative purposes
+        self.lib.reverse_string_v2.restype = ctypes.c_char_p
+        self.lib.reverse_string_v2.argtypes = [ctypes.c_char_p, ctypes.c_int32]
+
+        n = len(original)
+        rev = self.lib.reverse_string_v2(original.encode(), n)
+        return ctypes.string_at(rev, n).decode()
+
+    def distance_4_points(self, four_points: FourPoints) -> float:
+        """Calculates the total distance connecting 4 [Point][msl.examples.loadlib.cpp32.Point]s.
+
+        The corresponding C++ code is
+
+        ```cpp
+        double distance_4_points(FourPoints p) {
+            double d = distance(p.points[0], p.points[3]);
+            for (int i = 1; i < 4; i++) {
+                d += distance(p.points[i], p.points[i-1]);
+            }
+            return d;
+        }
+        ```
+
+        See the corresponding [Cpp64.distance_4_points][msl.examples.loadlib.Cpp64.distance_4_points] method.
+
+        Args:
+            four_points: The points to use to calculate the total distance.
+
+        Returns:
+            The total distance connecting the 4 points.
+        """
+        # restype should be defined elsewhere, shown here for illustrative purposes
+        self.lib.distance_4_points.restype = ctypes.c_double
+        result: float = self.lib.distance_4_points(four_points)
+        return result
+
+    def circumference(self, radius: float, n: int) -> float:
+        """Estimates the circumference of a circle.
+
+        This method calls the `distance_n_points` function in [cpp_lib][cpp-lib].
+
+        The corresponding C++ code uses the [NPoints][msl.examples.loadlib.cpp32.NPoints]
+        struct as the input parameter to sum the distance between adjacent points on the circle.
+
+        ```cpp
+        double distance_n_points(NPoints p) {
+            if (p.n < 2) {
+                return 0.0;
+            }
+            double d = distance(p.points[0], p.points[p.n-1]);
+            for (int i = 1; i < p.n; i++) {
+                d += distance(p.points[i], p.points[i-1]);
+            }
+            return d;
+        }
+        ```
+
+        See the corresponding [Cpp64.circumference][msl.examples.loadlib.Cpp64.circumference] method.
+
+        Args:
+            radius: The radius of the circle.
+            n: The number of points to use to estimate the circumference.
+
+        Returns:
+            The estimated circumference of the circle.
+        """
+        # restype and argtypes should be defined elsewhere, shown here for illustrative purposes
+        self.lib.distance_n_points.restype = ctypes.c_double
+        self.lib.distance_n_points.argtypes = [NPoints]
+
+        theta = 0.0
+        delta = (2.0 * math.pi) / float(n) if n != 0 else 0
+
+        pts = NPoints()
+        pts.n = n
+        pts.points = (Point * n)()
+        for i in range(n):
+            pts.points[i] = Point(radius * math.cos(theta), radius * math.sin(theta))
+            theta += delta
+        result: float = self.lib.distance_n_points(pts)
+        return result
+
+
+class Point(ctypes.Structure):
+    """C++ struct that is a fixed size in memory.
+
+    This object can be [pickle][]{:target="_blank"}d.
+
+    ```cpp
+    struct Point {
+        double x;
+        double y;
+    };
+    ```
+    """
+
+    _fields_ = (  # pyright: ignore[reportUnannotatedClassAttribute]
+        ("x", ctypes.c_double),
+        ("y", ctypes.c_double),
+    )
+
+
+class FourPoints(ctypes.Structure):
+    """C++ struct that is a fixed size in memory."""
+
+    _fields_ = (  # pyright: ignore[reportUnannotatedClassAttribute]
+        ("points", (Point * 4)),
+    )
+
+    def __init__(self, point1: Point, point2: Point, point3: Point, point4: Point) -> None:
+        """C++ struct that is a fixed size in memory.
+
+        This object can be [pickle][]{:target="_blank"}d.
+
+        ```cpp
+        struct FourPoints {
+            Point points[4];
+        };
+        ```
+
+        Args:
+            point1: The first point.
+            point2: The second point.
+            point3: The third point.
+            point4: The fourth point.
+        """
+        super().__init__()
+        self.points: Array[Point] = (Point * 4)(point1, point2, point3, point4)
+
+
+class NPoints(ctypes.Structure):
+    """C++ struct that is **not** a fixed size in memory.
+
+    This object cannot be [pickle][]{:target="_blank"}d because it contains a pointer.
+    A 32-bit process and a 64-bit process cannot share a pointer.
+
+    ```cpp
+    struct NPoints {
+        int n;
+        Point *points;
+    };
+    ```
+    """
+
+    _fields_ = (  # pyright: ignore[reportUnannotatedClassAttribute]
+        ("n", ctypes.c_int),
+        ("points", ctypes.POINTER(Point)),
+    )
---
+++ packages/msl-loadlib/msl_loadlib-1.0.0-py3-none-musllinux_1_1_aarch64.whl/msl/loadlib/utils.py
@@ -0,0 +1,415 @@
+"""Common utility functions."""
+
+from __future__ import annotations
+
+import logging
+import os
+import socket
+import subprocess
+import time
+from pathlib import Path
+from typing import TYPE_CHECKING
+from xml.etree import ElementTree as ET
+
+try:
+    import winreg
+except ImportError:
+    winreg = None  # type: ignore[assignment]
+
+from ._constants import IS_LINUX, IS_WINDOWS
+from .exceptions import ConnectionTimeoutError
+
+if TYPE_CHECKING:
+    from types import ModuleType
+    from typing import Any
+
+    from ._types import PathLike
+
+logger = logging.getLogger(__package__)
+
+_NET_FRAMEWORK_DESCRIPTION: str = """
+<!--
+  Created by the MSL-LoadLib package.
+
+  By default, applications that target the .NET Framework version 4.0+ cannot load
+  assemblies from older .NET Framework versions. You must create and modify the
+  <app>.config file and set the useLegacyV2RuntimeActivationPolicy property to be
+  "true" for the application to load an assembly from .NET < 4.0.
+
+  For Python.NET to load an assembly from .NET < 4.0, this corresponds to creating
+  a python.exe.config (Windows) or python.config (Linux) configuration file that
+  is saved in the same directory as the executable.
+
+  See https://support.microsoft.com/kb/2572158 for more information about the
+  "Mixed mode assembly" error.
+-->
+"""
+
+_NET_FRAMEWORK_FIX: str = """
+    <startup useLegacyV2RuntimeActivationPolicy="true">
+        <supportedRuntime version="v4.0" />
+        <supportedRuntime version="v2.0.50727" />
+    </startup>
+"""
+
+
+def is_pythonnet_installed() -> bool:
+    """Checks if `pythonnet` is installed.
+
+    Returns:
+        Whether `pythonnet` is installed.
+    """
+    try:
+        import clr  # type: ignore[import-untyped] # pyright: ignore[reportMissingTypeStubs, reportUnusedImport] # noqa: F401
+    except ImportError:
+        return False
+    return True
+
+
+def is_py4j_installed() -> bool:
+    """Checks if `py4j` is installed.
+
+    Returns:
+        Whether `py4j` is installed.
+
+    !!! note "Added in version 0.4"
+    """
+    try:
+        import py4j  # type: ignore[import-untyped] # pyright: ignore[reportMissingTypeStubs, reportUnusedImport] # noqa: F401
+    except ImportError:
+        return False
+    return True
+
+
+def is_comtypes_installed() -> bool:
+    """Checks if `comtypes` is installed.
+
+    Returns:
+        Whether `comtypes` is installed.
+
+    !!! note "Added in version 0.5"
+    """
+    try:
+        import comtypes  # type: ignore[import-untyped] # pyright: ignore[reportMissingTypeStubs, reportUnusedImport] # noqa: F401
+    except ImportError:
+        return False
+    return True
+
+
+def check_dot_net_config(py_exe_path: PathLike) -> tuple[int, str]:
+    """Checks if the **useLegacyV2RuntimeActivationPolicy** property is enabled.
+
+    By default, [Python.NET](https://pythonnet.github.io/){:target="_blank"} works
+    with .NET 4.0+ and therefore it cannot automatically load a library that was compiled
+    with .NET &lt; 4.0.
+
+    This function ensures that the **useLegacyV2RuntimeActivationPolicy** property is
+    defined in the *py_exe_path*.config file and that it is enabled.
+
+    This [link](https://stackoverflow.com/questions/14508627/){:target="_blank"} provides
+    an overview explaining why the **useLegacyV2RuntimeActivationPolicy** property is required.
+
+    The *py_exe_path*.config file that is created is
+
+    ```xml
+    <?xml version="1.0" encoding="utf-8" ?>
+    <configuration>
+        <startup useLegacyV2RuntimeActivationPolicy="true">
+            <supportedRuntime version="v4.0" />
+            <supportedRuntime version="v2.0.50727" />
+        </startup>
+    </configuration>
+    ```
+
+    Args:
+        py_exe_path: The path to a Python executable.
+
+    Returns:
+        A status flag and a message describing the outcome.
+
+            The flag will be one of the following values:
+
+            * -1: if there was a problem
+            * 0: if the .NET property was already enabled, or
+            * 1: if the property was created successfully.
+    """
+    config_path: str = os.fsdecode(py_exe_path) + ".config"
+
+    if Path(config_path).is_file():
+        try:
+            tree = ET.parse(config_path)  # noqa: S314
+        except ET.ParseError:
+            msg = f"Invalid XML file {config_path}\nCannot create the useLegacyV2RuntimeActivationPolicy property.\n"
+            logger.warning(msg)
+            return -1, msg
+
+        root = tree.getroot()
+
+        if root.tag != "configuration":
+            msg = (
+                f"The root tag in {config_path} is <{root.tag}>.\n"
+                f"It must be <configuration> in order to create a .NET Framework config\n"
+                f"file which enables the useLegacyV2RuntimeActivationPolicy property.\n"
+                f"To load an assembly from a .NET Framework version < 4.0 the following\n"
+                f"must be in {config_path}\n\n"
+                f"<configuration>{_NET_FRAMEWORK_FIX}</configuration>\n"
+            )
+            logger.warning(msg)
+            return -1, msg
+
+        # check if the policy exists
+        policy = root.find("startup/[@useLegacyV2RuntimeActivationPolicy]")
+        if policy is None:
+            lines = Path(config_path).read_text().splitlines()
+            lines.insert(-1, _NET_FRAMEWORK_FIX)
+            _ = Path(config_path).write_text("\n".join(lines))
+            msg = (
+                f"Added the useLegacyV2RuntimeActivationPolicy property to\n"
+                f"{config_path}\n"
+                f"Try again to see if Python can now load the .NET library.\n"
+            )
+            return 1, msg
+
+        if policy.attrib["useLegacyV2RuntimeActivationPolicy"].lower() != "true":
+            msg = (
+                f'The useLegacyV2RuntimeActivationPolicy attribute is "false" in\n'
+                f"{config_path}\n"
+                f"Cannot load an assembly from a .NET Framework version < 4.0.\n"
+            )
+            logger.warning(msg)
+            return -1, msg
+
+        return 0, "The useLegacyV2RuntimeActivationPolicy property is enabled"
+
+    text = (
+        f'<?xml version="1.0" encoding="utf-8" ?>'
+        f"{_NET_FRAMEWORK_DESCRIPTION}"
+        f"<configuration>"
+        f"{_NET_FRAMEWORK_FIX}"
+        f"</configuration>\n"
+    )
+    _ = Path(config_path).write_text(text)
+
+    msg = (
+        f"The library appears to be from a .NET Framework version < 4.0.\n"
+        f'To fix the "Mixed mode assembly is built against ..." error\n'
+        f"the useLegacyV2RuntimeActivationPolicy property was added to\n"
+        f"{config_path}\n\n"
+        f"Rerun the script, or restart the interactive console, to see if\n"
+        f"Python can now load the .NET library.\n"
+    )
+    return 1, msg
+
+
+def is_port_in_use(port: int) -> bool:
+    """Checks whether the TCP port is in use.
+
+    Args:
+        port: The port number to test.
+
+    Returns:
+        Whether the TCP port is in use.
+
+    !!! note "Changed in version 0.10"
+        Only check TCP ports (instead of both TCP and UDP ports).
+        Uses the ``ss`` command instead of ``netstat`` on Linux.
+
+    !!! note "Changed in version 0.7"
+        Renamed from `port_in_use` and added support for macOS.
+    """
+    flags = 0
+    if IS_WINDOWS:
+        flags = 0x08000000  # fixes issue 31, CREATE_NO_WINDOW = 0x08000000
+        cmd = ["netstat", "-a", "-n", "-p", "TCP"]
+    elif IS_LINUX:
+        cmd = ["ss", "-ant"]
+    else:
+        cmd = ["lsof", "-nPw", "-iTCP"]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, creationflags=flags)  # noqa: S603
+    out, err = p.communicate()
+    if err:
+        raise RuntimeError(err.decode(errors="ignore"))
+    return out.find(b":%d " % port) > 0
+
+
+def get_available_port() -> int:
+    """[int][] &mdash; Returns a port number that is available."""
+    port: int
+    with socket.socket() as sock:
+        sock.bind(("", 0))
+        _, port = sock.getsockname()
+        return port
+
+
+def wait_for_server(host: str, port: int, timeout: float) -> None:
+    """Wait for the 32-bit server to start.
+
+    Args:
+        host: The hostname or IP address of the server.
+        port: The port number of the server.
+        timeout: The maximum number of seconds to wait to establish a connection to the server.
+
+    Raises:
+        ConnectionTimeoutError: If a timeout occurred.
+    """
+    stop = time.time() + max(0.0, timeout)
+    while True:
+        if is_port_in_use(port):
+            return
+
+        if time.time() > stop:
+            msg = f"Timeout after {timeout:.1f} second(s). Could not connect to {host}:{port}"
+            raise ConnectionTimeoutError(msg)
+
+
+def get_com_info(*additional_keys: str) -> dict[str, dict[str, str | None]]:  # noqa: C901
+    """Reads the registry for the [COM]{:target="_blank"} libraries that are available.
+
+    [COM]: https://learn.microsoft.com/en-us/windows/win32/com/component-object-model--com--portal
+
+    !!! attention
+        This function is only supported on Windows.
+
+    Args:
+        additional_keys: The Program ID (`ProgID`) key is returned automatically.
+            You can include additional keys (e.g., `Version`, `InprocHandler32`, `ToolboxBitmap32`,
+            `VersionIndependentProgID`, ...) if you also want this additional
+            information to be returned for each [Class ID]{:target="_blank"}.
+
+            [Class ID]: https://docs.microsoft.com/en-us/windows/desktop/com/clsid-key-hklm
+
+    **Example:**
+
+    ```python
+    from msl.loadlib import utils
+
+    info = utils.get_com_info()
+    more_info = utils.get_com_info("Version", "ToolboxBitmap32")
+    ```
+
+    Returns:
+        The keys are the Class ID's and each value is a [dict][] of the information that was requested.
+
+    !!! note "Added in version 0.5"
+    """
+    if winreg is None:
+        return {}  # type: ignore[unreachable]
+
+    results: dict[str, dict[str, str | None]] = {}
+    for item in ["CLSID", r"Wow6432Node\CLSID"]:
+        try:
+            key = winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, item)
+        except OSError:
+            continue
+        else:
+            logger.debug(r"Parsing HKEY_CLASSES_ROOT\%s\...", item)
+
+        index = -1
+        while True:
+            index += 1
+            try:
+                clsid = winreg.EnumKey(key, index)
+            except OSError:
+                break
+
+            sub_key = winreg.OpenKey(key, clsid)
+
+            # ProgID is mandatory, if this fails then ignore
+            # this CLSID and go to the next index in the registry
+            try:
+                progid = winreg.QueryValue(sub_key, "ProgID")
+            except OSError:
+                pass
+            else:
+                results[clsid] = {}
+                results[clsid]["ProgID"] = progid
+
+                for name in additional_keys:
+                    try:
+                        results[clsid][name] = winreg.QueryValue(sub_key, name)
+                    except OSError:  # noqa: PERF203
+                        results[clsid][name] = None
+            finally:
+                winreg.CloseKey(sub_key)
+
+        winreg.CloseKey(key)
+
+    return results
+
+
+def generate_com_wrapper(lib: Any, out_dir: PathLike | None = None) -> ModuleType:
+    """Generate a Python wrapper module around a [COM]{:target="_blank"} library.
+
+    For more information see [Accessing type libraries]{:target="_blank"}.
+
+    [COM]: https://learn.microsoft.com/en-us/windows/win32/com/component-object-model--com--portal
+    [Accessing type libraries]: https://comtypes.readthedocs.io/en/stable/client.html#accessing-type-libraries
+
+    !!! attention
+        This function is only supported on Windows.
+
+    Args:
+        lib: The COM library to create a wrapper of.
+
+            Can be any of the following:
+
+            * a [LoadLibrary][] instance
+            * the `ProgID` or `CLSID` of a registered COM library as a [str][]
+            * a `comtypes` pointer instance
+            * an `ITypeLib` COM pointer from a loaded type library
+            * the path to a library file (.tlb, .exe or .dll) as a [str][]
+            * a sequence specifying the `GUID` of a library, a major and a minor
+                version number, plus optionally an `LCID` number, e.g.,
+
+                `["{EAB22AC0-30C1-11CF-A7EB-0000C05BAE0B}", 1, 1]`
+
+            * an object with `_reg_libid_` and `_reg_version_` attributes
+
+        out_dir: The output directory to save the wrapper to. If not specified,
+            the module is saved to the `../site-packages/comtypes/gen` directory.
+
+    Returns:
+        The wrapper module that was generated.
+
+    !!! note "Added in version 0.9"
+    """
+    if not is_comtypes_installed():
+        msg = "Cannot create a COM wrapper because comtypes is not installed, run\n  pip install comtypes"
+        raise OSError(msg)
+
+    import comtypes.client  # type: ignore[import-untyped] # pyright: ignore[reportMissingTypeStubs]
+
+    # cache the value of gen_dir to reset it later
+    assert isinstance(comtypes.client.gen_dir, str)  # pyright: ignore[reportUnknownMemberType] # noqa: S101
+    cached_gen_dir: str = comtypes.client.gen_dir
+    if out_dir is not None:
+        gen_dir = Path(os.fsdecode(out_dir)).resolve()
+        gen_dir.mkdir(parents=True, exist_ok=True)
+        comtypes.client.gen_dir = str(gen_dir)
+
+    def from_pointer(p: Any) -> ModuleType:
+        info = p.GetTypeInfo(0)
+        type_lib, _ = info.GetContainingTypeLib()
+        module: ModuleType = comtypes.client.GetModule(type_lib)  # pyright: ignore[reportUnknownMemberType,reportUnknownVariableType]
+        return module  # pyright: ignore[reportUnknownVariableType]
+
+    mod: ModuleType
+    try:
+        mod = comtypes.client.GetModule(lib)  # pyright: ignore[reportUnknownMemberType,reportUnknownVariableType]
+    except OSError:
+        if not isinstance(lib, str):
+            raise
+        obj = comtypes.client.CreateObject(lib)  # pyright: ignore[reportUnknownMemberType,reportUnknownVariableType]
+        mod = from_pointer(obj)
+    except (AttributeError, TypeError) as e:
+        if "LoadLibrary" in str(e):
+            mod = from_pointer(lib.lib)
+        elif hasattr(lib, "__com_interface__"):
+            mod = from_pointer(lib)
+        else:
+            raise
+
+    if out_dir is not None:
+        comtypes.client.gen_dir = cached_gen_dir
+
+    return mod  # pyright: ignore[reportUnknownVariableType]
