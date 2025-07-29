0ae2c1bc84bd8e222cd374f3f5e5e40d63367a64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:19 +0000

    Add package ecss-chat-client/1.3.8/ecss_chat_client-1.3.8.tar.gz

---
+++ packages/ecss-chat-client/ecss_chat_client-1.3.8.tar.gz/ecss_chat_client-1.3.8/ecss_chat_client/auth.py
@@ -0,0 +1,44 @@
+from http import HTTPStatus
+
+import requests
+
+from .scheme import AuthResponse
+
+
+def auth(username, password, base_url, proto, verify=True):
+    session = requests.Session()
+    login_data = {
+        'user': username,
+        'password': password,
+    }
+    if proto == 'https':
+        response = session.post(
+            f'{base_url}/login',
+            json=login_data,
+            verify=verify,
+        )
+    if proto == 'http':
+        response = session.post(
+                f'{base_url}/login',
+                json=login_data,
+            )
+
+    assert response.status_code == HTTPStatus.OK
+    auth_response = AuthResponse(**response.json())
+    token = auth_response.data.authToken
+    uid = auth_response.data.me.id
+    return token, uid
+
+
+class Auth():
+    def session(self, username, password, base_url, proto, verify):
+        auth_token, uid = auth(username, password, base_url, proto, verify)
+        session = requests.Session()
+        headers = {
+            'X-Auth-Token': auth_token,
+            'X-User-Id': uid,
+        }
+        session.headers.update(headers)
+        session.username = username
+        session.uid = uid
+        return session
