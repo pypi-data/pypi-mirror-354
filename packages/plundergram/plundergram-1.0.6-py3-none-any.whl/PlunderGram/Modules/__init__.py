27c3746d1aacb7ad496b7c6b7207191cf3648f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:05 +0000

    Add package plundergram/1.0.6/plundergram-1.0.6-py3-none-any.whl

---
+++ packages/plundergram/plundergram-1.0.6-py3-none-any.whl/PlunderGram/Modules/__init__.py
@@ -0,0 +1,17 @@
+# PlunderGram/Modules/__init__.py
+
+# Package metadata
+__version__ = "1.0.6"
+__author__ = "kpwnther"
+
+# Importing classes and functions
+from .configread import load
+from .Spyglass import spyglass
+from .Recon import recon 
+from .Raid import raid
+from .Boarding import boarding
+from .JollyRoger import Flag
+
+__all__ = [configread, Spyglass, Recon, Raid, Boarding, JollyRoger]
+# Optional initialization code
+print("PlunderGram has been initialized.")
---
+++ packages/plundergram/plundergram-1.0.6-py3-none-any.whl/PlunderGram/__init__.py
@@ -0,0 +1,7 @@
+#PlunderGram/__init__.py
+__version__ = "1.0.6"
+__author__ = "kpwnther"
+
+from .Plunder import main
+
+__all__= ['main']
