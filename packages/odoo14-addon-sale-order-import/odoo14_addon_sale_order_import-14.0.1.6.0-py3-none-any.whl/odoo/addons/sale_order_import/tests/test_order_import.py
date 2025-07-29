a4df3eccc007b536a4a6daa3f7f7f87150c5d0c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:22 +0000

    Add package odoo14-addon-sale-order-import/14.0.1.6.0/odoo14_addon_sale_order_import-14.0.1.6.0-py3-none-any.whl

---
+++ packages/odoo14-addon-sale-order-import/odoo14_addon_sale_order_import-14.0.1.6.0-py3-none-any.whl/odoo/addons/sale_order_import/tests/test_order_import.py
@@ -0,0 +1,79 @@
+# Copyright 2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2022 Camptocamp SA
+# @author: Simone Orsi <simahawk@gmail.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from .common import TestCommon
+
+
+class TestOrderImport(TestCommon):
+    """Test order create/update."""
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.parsed_order = {
+            "partner": {"email": "deco.addict82@example.com"},
+            "date": "2018-08-14",
+            "order_ref": "TEST1242",
+            "lines": [
+                {
+                    "product": {"code": "FURN_8888"},
+                    "qty": 2,
+                    "uom": {"unece_code": "C62"},
+                    "price_unit": 12.42,
+                }
+            ],
+            "chatter_msg": [],
+            "doc_type": "rfq",
+        }
+
+    def test_order_import(self):
+        order = self.wiz_model.create_order(self.parsed_order, "pricelist")
+        self.assertEqual(order.client_order_ref, self.parsed_order["order_ref"])
+        self.assertEqual(
+            order.order_line[0].product_id.default_code,
+            self.parsed_order["lines"][0]["product"]["code"],
+        )
+        self.assertEqual(int(order.order_line[0].product_uom_qty), 2)
+        # Now update the order
+        parsed_order_up = dict(
+            self.parsed_order,
+            partner={"email": "agrolait@yourcompany.example.com"},
+            lines=[
+                {
+                    "product": {"code": "FURN_8888"},
+                    "qty": 3,
+                    "uom": {"unece_code": "C62"},
+                    "price_unit": 12.42,
+                },
+                {
+                    "product": {"code": "FURN_9999"},
+                    "qty": 1,
+                    "uom": {"unece_code": "C62"},
+                    "price_unit": 1.42,
+                },
+            ],
+        )
+        self.wiz_model.update_order_lines(parsed_order_up, order, "pricelist")
+        self.assertEqual(len(order.order_line), 2)
+        self.assertEqual(int(order.order_line[0].product_uom_qty), 3)
+
+    def test_order_import_action(self):
+        wiz = self.wiz_model.create({})
+        action = wiz.create_order_return_action(self.parsed_order, "order.ref")
+        order = self.env["sale.order"].browse(action["res_id"])
+        self.assertEqual(order.state, "draft")
+
+    def test_order_import_confirm(self):
+        wiz = self.wiz_model.create({"confirm_order": True})
+        action = wiz.create_order_return_action(self.parsed_order, "order.ref")
+        order = self.env["sale.order"].browse(action["res_id"])
+        self.assertEqual(order.state, "sale")
+
+    def test_order_import_default_so_vals(self):
+        default = {"client_order_ref": "OVERRIDE"}
+        order = self.wiz_model.with_context(
+            sale_order_import__default_vals=dict(order=default)
+        ).create_order(self.parsed_order, "pricelist")
+        self.assertEqual(order.client_order_ref, "OVERRIDE")
