e8aeb91652bc0f4a8a48ffb9f783b0dc928fbd94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:47 +0000

    Add package auto-localization/0.1.0/auto_localization-0.1.0.tar.gz

---
+++ packages/auto-localization/auto_localization-0.1.0.tar.gz/auto_localization-0.1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name='auto_localization',
+    description='A Discord bot extension for automatic localization of slash commands',
+    version='0.1.0',
+    author='Lucky',
+    packages=find_packages(),
+)
