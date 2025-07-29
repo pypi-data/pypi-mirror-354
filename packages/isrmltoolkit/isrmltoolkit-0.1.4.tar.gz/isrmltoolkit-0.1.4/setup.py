fa4a4f3d860b327c36987a1e9b0d138fa5f43867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:36 +0000

    Add package isrmltoolkit/0.1.4/isrmltoolkit-0.1.4.tar.gz

---
+++ packages/isrmltoolkit/isrmltoolkit-0.1.4.tar.gz/isrmltoolkit-0.1.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='isrmltoolkit',  # Nombre que usará pip install
+    version='0.1.4', 
+    author='Isra',
+    author_email='ismael@email.com',
+    description='Funciones y utilidades útiles para proyectos de Machine Learning',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Israelamat/pip_install',  # Actualízalo si es necesario
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'scikit-learn',
+        'matplotlib',
+        'seaborn'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
