eed294c004be874eae478ca15db89c134e55cac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:41 +0000

    Add package bitssh/3.5.0/bitssh-3.5.0-py3-none-any.whl

---
+++ packages/bitssh/bitssh-3.5.0-py3-none-any.whl/bitssh/__init__.py
@@ -0,0 +1,8 @@
+try:
+    import tomllib
+except ModuleNotFoundError:
+    # Third party imports
+    pass
+
+
+__version__ = "3.5.0"
---
+++ packages/bitssh/bitssh-3.5.0-py3-none-any.whl/bitssh/argument_parser.py
@@ -0,0 +1,18 @@
+from argparse import ArgumentParser
+
+
+class Config:
+    def __init__(self) -> None:
+        parser: ArgumentParser = ArgumentParser(
+            description="A New and Modern SSH connector written in Python."
+        )
+        parser.add_argument(
+            "--version",
+            "-v",
+            action="store_true",
+            default=False,
+            help="Show the bitssh version.",
+        )
+
+        args, _ = parser.parse_known_args()
+        self.version = args.version
