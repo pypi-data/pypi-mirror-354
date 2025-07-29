a170a4e0aff5f69fd31b64bc5cb951252524a53a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:41 +0000

    Add package seele-sdk/0.1.5/seele_sdk-0.1.5.tar.gz

---
+++ packages/seele-sdk/seele_sdk-0.1.5.tar.gz/seele_sdk-0.1.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="seele-sdk",
+    version="0.1.5",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.1",
+    ],
+    author="PineField Inc",
+    author_email="zhangzhichao@pinefield.com",
+    description="A Python SDK for interacting with Seele Agent services",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/pinefield-inc/seele-sdk",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.7",
+)
