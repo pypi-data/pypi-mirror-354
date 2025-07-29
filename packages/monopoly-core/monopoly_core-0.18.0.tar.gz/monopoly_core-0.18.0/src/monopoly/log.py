a32a16892ac07d7a10ddb02edf0c36c555c6b7c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:10 +0000

    Add package monopoly-core/0.18.0/monopoly_core-0.18.0.tar.gz

---
+++ packages/monopoly-core/monopoly_core-0.18.0.tar.gz/monopoly_core-0.18.0/src/monopoly/log.py
@@ -0,0 +1,30 @@
+import functools
+import logging
+import sys
+
+
+def get_logger() -> logging.Logger:
+    logger = logging.getLogger("root")
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+    logging.getLogger("pdf2john").setLevel(logging.ERROR)
+    logging.getLogger("pyhanko").setLevel(logging.ERROR)
+    logging.getLogger("tzlocal").setLevel(logging.ERROR)
+    logging.getLogger("pikepdf").setLevel(logging.ERROR)
+    return logger
+
+
+def setup_logs(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if kwargs["verbose"]:
+            logger = logging.getLogger("root")
+            logger.setLevel(logging.DEBUG)
+        return func(*args, **kwargs)
+
+    return wrapper
