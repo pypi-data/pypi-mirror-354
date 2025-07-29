54885170e86fb587ce4255e4876280fcc33757ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:47 +0000

    Add package pPEGpy/0.3.9/ppegpy-0.3.9.tar.gz

---
+++ packages/pPEGpy/ppegpy-0.3.9.tar.gz/ppegpy-0.3.9/examples/same_again.py
@@ -0,0 +1,69 @@
+import pPEGpy as peg
+
+# Context Sensitive Grammars
+
+# using <@name> to match a name rule result the same-again
+
+# basic test...
+
+code = peg.compile("""
+    s = x ':' <@x>
+    x = [a-z]*
+""")
+
+print(code.parse("abc:abc"))
+
+# middle test...
+
+code = peg.compile("""
+    p  = x m x_
+    x  = [a-z]
+    x_ = <@x>
+    m  = (x &x)*
+""")
+
+p = code.parse("abba")
+# p.dump()
+print(p)
+
+# Markdown code quotes...
+
+code = peg.compile("""
+    Code = tics code tics
+    code = ~<@tics>*
+    tics = [`]+
+""")
+
+p = code.parse("```abc``def```")
+# p.dump(0)
+print(p)
+
+# Rust raw string syntax:
+
+raw = peg.compile("""
+    Raw   = fence '"' raw '"' fence
+    raw   = ~('"' <@fence>)*
+    fence = '#'+
+""")
+
+print(raw.parse("""##"abcc#"x"#def"##"""))
+
+# indented blocks...
+
+blocks = peg.compile("""
+    Blk    = inset line (next / inlay)*
+    next   = <@inset> !' ' line
+    inlay  = &(<@inset> ' ') Blk
+    inset  = ' '+
+    line   = ~[\n\r]* '\r'? '\n'?
+""")
+
+p = blocks.parse("""  line one
+  line two
+    inset 2.1
+      inset 3.1
+    inset 2.2
+  line three
+""")
+
+print(p)
