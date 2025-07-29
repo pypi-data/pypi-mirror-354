226fcdbbfd99ce6ad7e3faf12dc574c0bbc67b39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:05 +0000

    Add package ada-agentic/0.1.0/ada_agentic-0.1.0.tar.gz

---
+++ packages/ada-agentic/ada_agentic-0.1.0.tar.gz/ada_agentic-0.1.0/__main__.py
@@ -0,0 +1,6 @@
+# main.py
+from agentic.server import mcp
+
+# Entry point to run the server
+if __name__ == "__main__":
+    mcp.run()
---
+++ packages/ada-agentic/ada_agentic-0.1.0.tar.gz/ada_agentic-0.1.0/server.py
@@ -0,0 +1,3 @@
+from mcp.server.fastmcp import FastMCP
+# This is the shared MCP server instance
+mcp = FastMCP("ada_agentic")
