52380c93fc26c3cd6d672fc9aa9010490778b0cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:06:57 +0000

    Add package autoelicit/0.1.9/autoelicit-0.1.9.tar.gz

---
+++ packages/autoelicit/autoelicit-0.1.9.tar.gz/autoelicit-0.1.9/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='autoelicit',
+    version='0.1.9',    
+    description='A python package for eliciting prior knowledge from experts',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/alexcapstick/autoelicit',
+    author='Alexander Capstick',
+    author_email='alexander.capstick19@imperial.ac.uk',
+    license='MIT',
+    packages=['autoelicit'],
+    install_requires=[
+        "numpy",
+        "scipy",
+        "pandas",
+        "scikit-learn",
+        "tqdm",
+        "pyarrow",
+        "ucimlrepo",
+        "openai>=1.51.2"        
+    ],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
