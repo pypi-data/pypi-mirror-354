2c4e60cbcb49839e8a6e28ea0cd5a203dec3229f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:01 +0000

    Add package number-guess-game-2/0.1.0/number_guess_game_2-0.1.0.tar.gz

---
+++ packages/number-guess-game-2/number_guess_game_2-0.1.0.tar.gz/number_guess_game_2-0.1.0/README.md
@@ -0,0 +1,3 @@
+a game that genrates a random number and asks the user to guess it 
+
+usage { import it with from {pkgname} import {pkgname}, and then print(pkgname()) }
---
+++ packages/number-guess-game-2/number_guess_game_2-0.1.0.tar.gz/number_guess_game_2-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='number_guess_game_2',  # Your package name
+    version='0.1.0',     # Version
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='a game that genrates a random number and asks the user to guess it ',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/reverseword',  # Optional
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
