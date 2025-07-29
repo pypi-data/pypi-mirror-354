3c1b8500bf9d1fc9614126c409159f25a85158be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:53 +0000

    Add package radis-app/0.1.1/radis_app-0.1.1.tar.gz

---
+++ packages/radis-app/radis_app-0.1.1.tar.gz/radis_app-0.1.1/radis-app/__init__.py
@@ -0,0 +1,16 @@
+# myapp/appOnly/__init__.py
+
+# from radis-app.radis-app import main
+# from radis-app.radis-app.main import set_false_string
+#
+# __all__ = ['set_false_string', 'main']
+
+
+
+# Configs
+
+# CONFIG = {
+#     'debug': False
+# }
+#
+# print("myapp package initialized")
