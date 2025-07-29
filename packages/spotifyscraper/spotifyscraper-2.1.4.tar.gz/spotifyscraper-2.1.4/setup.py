37c005532acf2be54cf7990d0c0aef2f194e7278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:35 +0000

    Add package spotifyscraper/2.1.4/spotifyscraper-2.1.4.tar.gz

---
+++ packages/spotifyscraper/spotifyscraper-2.1.4.tar.gz/spotifyscraper-2.1.4/setup.py
@@ -0,0 +1,9 @@
+"""
+Setup script for SpotifyScraper package.
+All metadata is now defined in pyproject.toml.
+"""
+
+from setuptools import setup
+
+# Minimal setup() call since all metadata is in pyproject.toml
+setup()
