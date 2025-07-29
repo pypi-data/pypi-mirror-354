4539e9559c3dc601a2e9e3013eea9d5a711b05c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:19 +0000

    Add package airflow-provider-coiled/0.0.22/airflow_provider_coiled-0.0.22-py3-none-any.whl

---
+++ packages/airflow-provider-coiled/airflow_provider_coiled-0.0.22-py3-none-any.whl/coiled_provider/get_provider_info.py
@@ -0,0 +1,26 @@
+__version__ = "1.0.0"
+
+# This is needed to allow Airflow to pick up specific metadata fields it needs for certain features.
+
+
+def get_provider_info():
+    return {
+        "package-name": "airflow-provider-coiled",  # Required
+        "name": "Sample",  # Required
+        "description": "An airflow provider for coiled",  # Required
+        "versions": [__version__],  # Required
+        "task-decorators": [
+            {
+                "name": "coiled_cluster",
+                # "Import path" and function name of the `foo_task`
+                "class-name": "coiled_provider.operators.cluster.coiled_cluster_task",
+            }
+        ],
+        # "connection-types": [
+        #     {
+        #         "connection-type": "sample",
+        #         "hook-class-name": "sample_provider.hooks.sample.SampleHook",
+        #     }
+        # ],
+        # "extra-links": ["sample_provider.operators.sample.SampleOperatorExtraLink"],
+    }
