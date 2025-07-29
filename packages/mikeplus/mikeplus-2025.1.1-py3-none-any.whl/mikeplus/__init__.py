8608ecbb5ceb71a0b573f54291b9d2b37b3195a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:08:59 +0000

    Add package mikeplus/2025.1.1/mikeplus-2025.1.1-py3-none-any.whl

---
+++ packages/mikeplus/mikeplus-2025.1.1-py3-none-any.whl/mikeplus/__init__.py
@@ -0,0 +1,40 @@
+"""MIKE+Py package."""
+
+__version__ = "2025.1.1"
+
+import clr
+from .conflicts import check_conflicts
+from .utils import setup_bin_path
+
+check_conflicts()
+setup_bin_path()
+
+#  keep here for backward compatibility (mikeio1d uses) ... remove in 2026.0.0
+from DHI.Mike.Install import MikeImport         # noqa: E402, F401
+from DHI.Mike.Install import MikeProducts       # noqa: E402, F401
+
+clr.AddReference("System")
+clr.AddReference("System.Runtime")
+clr.AddReference("System.Runtime.InteropServices")
+clr.AddReference("System.Data")
+clr.AddReference("NetTopologySuite")
+clr.AddReference("DHI.Amelia.DataModule")
+clr.AddReference("DHI.Amelia.DataModule.Interface")
+clr.AddReference("DHI.Amelia.Infrastructure.Interface")
+clr.AddReference("DHI.Amelia.GlobalUtility")
+clr.AddReference("DHI.Amelia.Tools.EngineTool")
+
+from .datatableaccess import DataTableAccess  # noqa: E402
+from .datatableaccess import DataTableDemoAccess  # noqa: E402
+from .database import Database  # noqa: E402
+from .shortcuts import open, create  # noqa: E402
+from .utils import to_sql  # noqa: E402
+
+__all__ = [
+    "Database",
+    "DataTableAccess",
+    "DataTableDemoAccess",
+    "open",
+    "create",
+    "to_sql",
+]
