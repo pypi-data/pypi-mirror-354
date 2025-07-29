413e9f1fd13362383e181faaee088d6b47d35dcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:07:45 +0000

    Add package eeg-eyetracking-parser/0.16.6/eeg_eyetracking_parser-0.16.6-py3-none-any.whl

---
+++ packages/eeg-eyetracking-parser/eeg_eyetracking_parser-0.16.6-py3-none-any.whl/eeg_eyetracking_parser/__init__.py
@@ -0,0 +1,13 @@
+"""A Python module for reading concurrently recorded EEG and eye-tracking data,
+and parsing this data into convenient objects for further analysis.
+"""
+import logging
+from ._triggers import trial_trigger, epoch_trigger
+from ._parsing import read_subject
+from ._custom_epochs import PupilEpochs, autoreject_epochs, epochs_to_series, \
+    tfr_to_surface
+
+
+__version__ = '0.16.6'
+logger = logging.getLogger('eeg_eyetracking_parser')
+logger.info(f'eeg_eyetracking_parser {__version__}')
---
+++ packages/eeg-eyetracking-parser/eeg_eyetracking_parser-0.16.6-py3-none-any.whl/eeg_eyetracking_parser/_triggers.py
@@ -0,0 +1,130 @@
+import numpy as np
+import logging
+
+logger = logging.getLogger('eeg_eyetracking_parser')
+
+TRIAL_TRIGGERS = list(range(128, 256))
+TRIGGER_ANNOTATION_PREFIX = 'Stimulus/OVTK_StimulationId_Label_'
+
+
+def epoch_trigger(events, trigger):
+    """Selects a single epoch trigger from a tuple with event information.
+    Epoch triggers have values between 1 and 127 (inclusive).
+
+    Parameters
+    ----------
+    events: tuple
+        Event information as returned by `read_subject()`.
+    trigger: int
+        A trigger code, which is a positive value.
+
+    Returns
+    -------
+    array:
+        A numpy array with events as expected by mne.Epochs().
+    """
+    events = _validate_events(events)
+    if not 128 > trigger > 0:
+        raise ValueError('trigger should be a number between 1 and 127')
+    triggers = events
+    return triggers[triggers[:, 2] == trigger]
+    
+
+def trial_trigger(events):
+    """Selects all trial triggers from event information. Trial triggers have
+    values between 128 and 255 (inclusive).
+    
+    Parameters
+    ----------
+    events: tuple
+        Event information as returned by `read_subject()`.
+
+    Returns
+    -------
+    array:
+        A numpy array with events as expected by mne.Epochs().
+    """    
+    events = _validate_events(events)
+    triggers = events
+    return triggers[triggers[:, 2] >= 128]
+
+
+def _parse_triggers(label):
+    """An internal function that converts labels as stored in our data."""
+    if not label.startswith(TRIGGER_ANNOTATION_PREFIX):
+        return None
+    return 255 - int(label[-2:], 16)
+    
+    
+def _validate_events(events, repair=False):
+    """Checks whether the events are in the correct format."""
+    if isinstance(events, tuple):
+        if len(events) != 2:
+            raise ValueError(
+                'events should be a numpy array or tuple of length 2')
+        events = events[0]
+    if not isinstance(events, np.ndarray):
+        raise TypeError('events should be a numpy array')
+    if not repair:
+        return events
+    # Remove ghost triggers
+    valid_events = []
+    for i in range(len(events) - 1):
+        dt = events[i + 1, 0] - events[i, 0]
+        if dt == 1:
+            logger.warning(
+                f'ignoring ghost trigger: {events[i]} before {events[i + 1]}')
+            continue
+        valid_events.append(events[i])
+    events = np.array(valid_events)
+    # Keep only non-zero triggers
+    events = events[events[:,2] != 0]
+    codes = events[:, 2]
+    if np.any((codes < 1) | (codes > 255)):
+        raise ValueError('trigger codes should be values between 1 and 255')
+    # Check for duplicate triggers within trials
+    trialid = -1
+    triggers_in_trial = []
+    select_triggers = []
+    for code in codes:
+        # Detect trial triggers and always keep them
+        if code >= 128:
+            trialid += 1
+            triggers_in_trial = []
+            select_triggers.append(True)
+            continue
+        # Skip triggers that precede a trial onset
+        if trialid == -1:
+            logger.warning(f'trigger {code} precedes first trial')
+            select_triggers.append(False)
+            continue
+        # Skip duplicate triggers within a trial
+        if code in triggers_in_trial:
+            logger.warning(
+                f'duplicate trigger {code} in trial {trialid}, label {hex(255 - 128 - trialid % 128)}')
+            select_triggers.append(False)
+        else:
+            select_triggers.append(True)
+        triggers_in_trial.append(code)
+    events = events[select_triggers]
+    # If the recording is aborted, it is possible that some event triggers are
+    # missing for the last trial. If so, then we exclude the last trial
+    # altogether
+    event_codes = np.unique(events[:, 2][events[:, 2] < 128])
+    n_trials = sum(events[:, 2] >= 128)
+    logger.info(f'found event triggers: {event_codes}')
+    trim_last_trial = False
+    for code in event_codes:
+        n_events = sum(events[:, 2] == code)
+        if n_events == n_trials - 1:
+            logger.warning(
+                f'last occurence of event trigger {code} is missing')
+            trim_last_trial = True
+        elif n_events < n_trials - 1:
+            logger.warning(f'not enough occurences of event trigger {code}')
+    if trim_last_trial:
+        logger.warning(
+            'trimming last trial to ensure equal number of event triggers')
+        last_trigger_index = np.where(events[:, 2] >= 128)[0][-1]
+        events = events[:last_trigger_index - 1]
+    return events
