e0b2944f796916501165fab3a62d8b0871cb4adf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:14 +0000

    Add package scrapli/2.0.0a4/scrapli-2.0.0a4-py3-none-musllinux_1_1_x86_64.whl

---
+++ packages/scrapli/scrapli-2.0.0a4-py3-none-musllinux_1_1_x86_64.whl/scrapli/__init__.py
@@ -0,0 +1,31 @@
+"""scrapli"""
+
+from scrapli.auth import LookupKeyValue
+from scrapli.auth import Options as AuthOptions
+from scrapli.cli import Cli
+from scrapli.netconf import Netconf
+from scrapli.netconf import Options as NetconfOptions
+from scrapli.session import Options as SessionOptions
+from scrapli.transport import BinOptions as TransportBinOptions
+from scrapli.transport import Options as TransportOptions
+from scrapli.transport import Ssh2Options as TransportSsh2Options
+from scrapli.transport import TelnetOptions as TransportTelnetOptions
+from scrapli.transport import TestOptions as TransportTestOptions
+
+__version__ = "2.0.0-alpha.4"
+__calendar_version__ = "2025.6.7"
+__definitions_version__ = "471f12e"
+
+__all__ = (
+    "AuthOptions",
+    "Cli",
+    "LookupKeyValue",
+    "Netconf",
+    "NetconfOptions",
+    "SessionOptions",
+    "TransportBinOptions",
+    "TransportOptions",
+    "TransportSsh2Options",
+    "TransportTelnetOptions",
+    "TransportTestOptions",
+)
