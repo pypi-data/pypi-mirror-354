361739505f9bafc2044c8946709d6635977cf154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:38 +0000

    Add package install-release/0.4.9/install_release-0.4.9.tar.gz

---
+++ packages/install-release/install_release-0.4.9.tar.gz/install_release-0.4.9/InstallRelease/constants.py
@@ -0,0 +1,60 @@
+import os
+import platform
+
+
+HOME = os.path.expanduser("~")
+
+__bin_at__ = "bin"
+__dir_name__ = "install_release"
+
+__state_at__ = f"{__dir_name__}/state.json"
+__config_at__ = f"{__dir_name__}/config.json"
+
+_colors = {
+    "green": "#8CC265",
+    "light_green": "#D0FF5E bold",
+    "blue": "#4AA5F0",
+    "cyan": "#76F6FF",
+    "yellow": "#F0A45D bold",
+    "red": "#E8678A",
+    "purple": "#8782E9 bold",
+}
+
+
+state_path = {
+    "linux": f"{HOME}/.config/{__state_at__}",
+    "darwin": f"{HOME}/Library/.config/{__state_at__}",
+}
+
+config_path = {
+    "linux": f"{HOME}/.config/{__config_at__}",
+    "darwin": f"{HOME}/Library/.config/{__config_at__}",
+}
+
+bin_path = {
+    "linux": f"{HOME}/{__bin_at__}",
+    "darwin": f"{HOME}/{__bin_at__}",
+}
+
+
+def platform_words() -> list:
+    aliases = {
+        "x86_64": ["x86", "x64", "amd64", "amd", "x86_64"],
+        "aarch64": ["arm64", "aarch64", "arm"],
+    }
+
+    words = [platform.system().lower(), platform.architecture()[0]]
+
+    for alias in aliases:
+        if platform.machine().lower() in aliases[alias]:
+            words += aliases[alias]
+
+    try:
+        sys_alias = platform.platform().split("-")[0].lower()
+
+        if platform.system().lower() != sys_alias:
+            words.append(sys_alias)
+    except Exception:
+        ...
+
+    return words
