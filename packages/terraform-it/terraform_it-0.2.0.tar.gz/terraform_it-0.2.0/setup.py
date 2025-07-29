77ed453269718d41792881e127c5f2338d7e1997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:31 +0000

    Add package terraform-it/0.2.0/terraform_it-0.2.0.tar.gz

---
+++ packages/terraform-it/terraform_it-0.2.0.tar.gz/terraform_it-0.2.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="terraform-it",
+    version="0.2.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "pyyaml>=6.0",
+        "jinja2>=3.0.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "tfit=terraform_it.cli:main",
+        ],
+    },
+    python_requires=">=3.6",
+    author="Deep Shah",
+    author_email="shahdeep855@gmail.com",
+    description="A tool to generate Terraform providers from OpenAPI specs and Go SDKs.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Deep070203/terraform-it",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={
+        "terraform_it": ["templates/*", "templates/*/*"],
+    },
+)
---
+++ packages/terraform-it/terraform_it-0.2.0.tar.gz/terraform_it-0.2.0/terraform_it/__init__.py
@@ -0,0 +1,3 @@
+"""Terraform-It: A tool to generate Terraform providers from OpenAPI specs."""
+
+__version__ = "0.1.0"
