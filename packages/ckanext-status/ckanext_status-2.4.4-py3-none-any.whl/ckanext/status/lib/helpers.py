1fc2000cb49d65e615fa1301b5d2f86029d1119a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:41 +0000

    Add package ckanext-status/2.4.4/ckanext_status-2.4.4-py3-none-any.whl

---
+++ packages/ckanext-status/ckanext_status-2.4.4-py3-none-any.whl/ckanext/status/lib/helpers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# This file is part of ckanext-status
+# Created by the Natural History Museum in London, UK
+
+import markdown
+from ckan.plugins import toolkit
+
+
+def status_enable_html():
+    return toolkit.asbool(toolkit.config.get('ckanext.status.enable_html', True))
+
+
+def status_get_message():
+    """
+    Retrieve status message from config file.
+
+    :returns: status message specified in config file (or None if not specified)
+    :rtype: string
+    """
+
+    status_message = toolkit.config.get('ckanext.status.message', None)
+    if status_message and status_enable_html():
+        status_message = markdown.markdown(status_message)
+    return status_message
---
+++ packages/ckanext-status/ckanext_status-2.4.4-py3-none-any.whl/ckanext/status/plugin.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# This file is part of ckanext-status
+# Created by the Natural History Museum in London, UK
+
+from ckan.plugins import SingletonPlugin, implements, interfaces, toolkit
+from ckantools.loaders import create_actions, create_auth
+
+from ckanext.status import routes
+from ckanext.status.lib.helpers import status_enable_html, status_get_message
+
+
+class StatusPlugin(SingletonPlugin):
+    """
+    Provides status bar banner at the top of pages.
+    """
+
+    implements(interfaces.IActions, inherit=True)
+    implements(interfaces.IAuthFunctions, inherit=True)
+    implements(interfaces.IBlueprint)
+    implements(interfaces.IConfigurer, inherit=True)
+    implements(interfaces.ITemplateHelpers)
+
+    # IActions
+    def get_actions(self):
+        from ckanext.status.logic import actions
+
+        return create_actions(actions)
+
+    # IAuthFunctions
+    def get_auth_functions(self):
+        from ckanext.status.logic import auth
+
+        return create_auth(auth)
+
+    # IBlueprint
+    def get_blueprint(self):
+        """
+        IBlueprint hook.
+        """
+        return routes.blueprints
+
+    # IConfigurer
+    def update_config(self, config):
+        toolkit.add_template_directory(config, 'theme/templates')
+        toolkit.add_resource('theme/assets', 'ckanext-status')
+
+    # IConfigurer
+    def update_config_schema(self, schema):
+        ignore_missing = toolkit.get_validator('ignore_missing')
+        schema.update(
+            {
+                'ckanext.status.message': [ignore_missing, str],
+            }
+        )
+        return schema
+
+    # ITemplateHelpers
+    def get_helpers(self):
+        return {
+            'status_get_message': status_get_message,
+            'status_enable_html': status_enable_html,
+        }
