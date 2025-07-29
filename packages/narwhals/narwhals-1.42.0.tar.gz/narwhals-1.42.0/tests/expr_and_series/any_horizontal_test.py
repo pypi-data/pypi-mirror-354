4a49a0d9a7d8d726ad9559e357a932993265d09f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:15 +0000

    Add package narwhals/1.42.0/narwhals-1.42.0.tar.gz

---
+++ packages/narwhals/narwhals-1.42.0.tar.gz/narwhals-1.42.0/tests/expr_and_series/any_horizontal_test.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from typing import Any
+
+import pytest
+
+import narwhals as nw
+from tests.utils import Constructor, assert_equal_data
+
+
+@pytest.mark.parametrize("expr1", ["a", nw.col("a")])
+@pytest.mark.parametrize("expr2", ["b", nw.col("b")])
+def test_anyh(constructor: Constructor, expr1: Any, expr2: Any) -> None:
+    data = {"a": [False, False, True], "b": [False, True, True]}
+    df = nw.from_native(constructor(data))
+    result = df.select(any=nw.any_horizontal(expr1, expr2))
+
+    expected = {"any": [False, True, True]}
+    assert_equal_data(result, expected)
+
+
+def test_anyh_all(constructor: Constructor) -> None:
+    data = {"a": [False, False, True], "b": [False, True, True]}
+    df = nw.from_native(constructor(data))
+    result = df.select(any=nw.any_horizontal(nw.all()))
+    expected = {"any": [False, True, True]}
+    assert_equal_data(result, expected)
+    result = df.select(nw.any_horizontal(nw.all()))
+    expected = {"a": [False, True, True]}
+    assert_equal_data(result, expected)
