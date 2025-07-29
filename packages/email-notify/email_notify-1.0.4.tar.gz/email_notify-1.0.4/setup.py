743206f802fdb4bc12c476529e6d7b4db04198b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:07:34 +0000

    Add package email-notify/1.0.4/email_notify-1.0.4.tar.gz

---
+++ packages/email-notify/email_notify-1.0.4.tar.gz/email_notify-1.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='email_notify',
+    version='1.0.4',
+    description='Email notifications when a code block or function finishes or ends unexpectedly',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Chao-Ning Hu',
+    packages=find_packages(),
+    python_requires='>=3.7',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+    ],
+    install_requires=[
+        'cryptography',
+    ],
+)
