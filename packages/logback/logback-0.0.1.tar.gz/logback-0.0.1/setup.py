0928344efeaba3e139b6349a075e2957dc977ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:17 +0000

    Add package logback/0.0.1/logback-0.0.1.tar.gz

---
+++ packages/logback/logback-0.0.1.tar.gz/logback-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='logback',
+    version='0.0.1',
+    packages=['logback', 'logback.logging', 'logback.colorama'],
+    url='',
+    license='',
+    author='zjj',
+    author_email='',
+    description=''
+)
