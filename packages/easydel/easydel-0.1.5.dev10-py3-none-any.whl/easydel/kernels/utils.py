02605b0e60ade17bbe5aba4df668bf0b803c6b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:59 +0000

    Add package easydel/0.1.5.dev10/easydel-0.1.5.dev10-py3-none-any.whl

---
+++ packages/easydel/easydel-0.1.5.dev10-py3-none-any.whl/easydel/kernels/utils.py
@@ -0,0 +1,23 @@
+# Copyright 2023 The EASYDEL Author @erfanzar (Erfan Zare Chavoshi).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+MAX_FUSED_SIZE = 65536
+
+
+def get_stride(array):
+    strides = [1]
+    for i in range(len(array.shape) - 1, 0, -1):  # Iterate in reverse shape order
+        strides.insert(0, strides[0] * array.shape[i])
+    return strides
---
+++ packages/easydel/easydel-0.1.5.dev10-py3-none-any.whl/easydel/trainers/__init__.py
@@ -0,0 +1,59 @@
+# Copyright 2023 The EASYDEL Author @erfanzar (Erfan Zare Chavoshi).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .base_trainer import BaseTrainer
+from .direct_preference_optimization_trainer import DPOConfig, DPOTrainer
+from .distillation_trainer import DistillationConfig, DistillationTrainer
+from .group_relative_policy_optimization import GRPOConfig, GRPOTrainer
+from .odds_ratio_preference_optimization_trainer import ORPOConfig, ORPOTrainer
+from .packer import pack_sequences
+from .ray_scaler import RayDistributedTrainer
+from .reward_trainer import RewardConfig, RewardTrainer
+from .supervised_fine_tuning_trainer import SFTConfig, SFTTrainer
+from .trainer import Trainer
+from .training_configurations import TrainingArguments
+from .utils import (
+    JaxDistributedConfig,
+    conversations_formatting_function,
+    create_constant_length_dataset,
+    create_prompt_creator,
+    get_formatting_func_from_dataset,
+    instructions_formatting_function,
+)
+
+__all__ = (
+    "BaseTrainer",
+    "DPOConfig",
+    "DPOTrainer",
+    "DistillationConfig",
+    "DistillationTrainer",
+    "GRPOConfig",
+    "GRPOTrainer",
+    "JaxDistributedConfig",
+    "ORPOConfig",
+    "ORPOTrainer",
+    "RayDistributedTrainer",
+    "RewardConfig",
+    "RewardTrainer",
+    "SFTConfig",
+    "SFTTrainer",
+    "Trainer",
+    "TrainingArguments",
+    "conversations_formatting_function",
+    "create_constant_length_dataset",
+    "create_prompt_creator",
+    "get_formatting_func_from_dataset",
+    "instructions_formatting_function",
+    "pack_sequences",
+)
