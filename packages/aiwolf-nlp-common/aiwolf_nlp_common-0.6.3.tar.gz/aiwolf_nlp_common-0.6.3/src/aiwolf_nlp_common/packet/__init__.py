858c32cbe97e1b40446589d20ca000a234af7792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:14:20 +0000

    Add package aiwolf-nlp-common/0.6.3/aiwolf_nlp_common-0.6.3.tar.gz

---
+++ packages/aiwolf-nlp-common/aiwolf_nlp_common-0.6.3.tar.gz/aiwolf_nlp_common-0.6.3/src/aiwolf_nlp_common/packet/__init__.py
@@ -0,0 +1,9 @@
+from .info import Info
+from .judge import Judge
+from .packet import Packet
+from .request import Request
+from .role import Role, Species, Team
+from .setting import Setting
+from .status import Status
+from .talk import Talk
+from .vote import Vote
