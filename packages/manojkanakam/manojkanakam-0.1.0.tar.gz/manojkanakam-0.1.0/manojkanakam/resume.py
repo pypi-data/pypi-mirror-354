a7cb33a157f6ec3e821ab563aa6184d557287c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:59 +0000

    Add package manojkanakam/0.1.0/manojkanakam-0.1.0.tar.gz

---
+++ packages/manojkanakam/manojkanakam-0.1.0.tar.gz/manojkanakam-0.1.0/manojkanakam/resume.py
@@ -0,0 +1,10 @@
+def resume():
+    print(
+        """Manoj Kanakam
++91 7981419024
+Warangal, Telangana (506005)
+manojkanakam9@gmail.com
+LinkedIn: linkedin.com/in/manojkanakam
+GitHub: github.com/manojkanakam
+..."""
+    )
