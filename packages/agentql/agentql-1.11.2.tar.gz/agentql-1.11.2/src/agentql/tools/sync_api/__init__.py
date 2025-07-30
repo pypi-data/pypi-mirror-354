40132d5766ca60f672295109a561b5d85a7f5b7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:46:10 +0000

    Add package agentql/1.11.2/agentql-1.11.2.tar.gz

---
+++ packages/agentql/agentql-1.11.2.tar.gz/agentql-1.11.2/src/agentql/tools/sync_api/__init__.py
@@ -0,0 +1,7 @@
+from agentql.ext.playwright.tools.sync_api._pagination.pagination_tool import paginate
+from agentql.tools.sync_api._query_document.query_document import query_document
+
+__all__ = [
+    "paginate",
+    "query_document",
+]
