ae956ab43362f6802c63b0c0ca8f3c1abe1fa467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:39 +0000

    Add package querypipz/0.1.12/querypipz-0.1.12.tar.gz

---
+++ packages/querypipz/querypipz-0.1.12.tar.gz/querypipz-0.1.12/src/querypipz/__init__.py
@@ -0,0 +1,17 @@
+""" init.py """
+import os
+from llama_index.core import Settings
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.llms.openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("BIANXIE_API_KEY")
+api_base = os.getenv("BIANXIE_BASE")
+
+Settings.embed_model = OpenAIEmbedding(model_name="text-embedding-3-small",
+                                       api_key=api_key,api_base =api_base)
+Settings.llm = OpenAI(model="gpt-4o",api_base=api_base,api_key=api_key)
+
+from querypipz.director import Director
+from querypipz.builder import BuilderFactory, BuilderType
