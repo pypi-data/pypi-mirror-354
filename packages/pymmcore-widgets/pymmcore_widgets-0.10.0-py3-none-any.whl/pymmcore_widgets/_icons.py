a6d5dc3c17c5eb4f8f1d6f9876822d107a88176b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:23 +0000

    Add package pymmcore-widgets/0.10.0/pymmcore_widgets-0.10.0-py3-none-any.whl

---
+++ packages/pymmcore-widgets/pymmcore_widgets-0.10.0-py3-none-any.whl/pymmcore_widgets/_icons.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from pymmcore_plus import DeviceType
+
+ICONS: dict[DeviceType, str] = {
+    DeviceType.Any: "mdi:devices",
+    DeviceType.AutoFocus: "mdi:auto-upload",
+    DeviceType.Camera: "mdi:camera",
+    DeviceType.Core: "mdi:checkbox-blank-circle-outline",
+    DeviceType.Galvo: "mdi:mirror-variant",
+    DeviceType.Generic: "mdi:dev-to",
+    DeviceType.Hub: "mdi:hubspot",
+    DeviceType.ImageProcessor: "mdi:image-auto-adjust",
+    DeviceType.Magnifier: "mdi:magnify-plus",
+    DeviceType.Shutter: "mdi:camera-iris",
+    DeviceType.SignalIO: "mdi:signal",
+    DeviceType.SLM: "mdi:view-comfy",
+    DeviceType.Stage: "mdi:arrow-up-down",
+    DeviceType.State: "mdi:state-machine",
+    DeviceType.Unknown: "mdi:dev-to",
+    DeviceType.XYStage: "mdi:arrow-all",
+    DeviceType.Serial: "mdi:serial-port",
+}
