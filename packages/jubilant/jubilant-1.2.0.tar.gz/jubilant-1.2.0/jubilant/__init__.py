6bcbfd557f858355349c08bfb0092c1d9df62c1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:33 +0000

    Add package jubilant/1.2.0/jubilant-1.2.0.tar.gz

---
+++ packages/jubilant/jubilant-1.2.0.tar.gz/jubilant-1.2.0/jubilant/__init__.py
@@ -0,0 +1,46 @@
+"""Jubilant is a Pythonic wrapper around the Juju CLI for writing charm integration tests."""
+
+from . import statustypes
+from ._all_any import (
+    all_active,
+    all_agents_idle,
+    all_blocked,
+    all_error,
+    all_maintenance,
+    all_waiting,
+    any_active,
+    any_blocked,
+    any_error,
+    any_maintenance,
+    any_waiting,
+)
+from ._juju import CLIError, ConfigValue, Juju, SecretURI, WaitError
+from ._task import Task, TaskError
+from ._test_helpers import temp_model
+from .statustypes import Status
+
+__all__ = [
+    'CLIError',
+    'ConfigValue',
+    'Juju',
+    'SecretURI',
+    'Status',
+    'Task',
+    'TaskError',
+    'WaitError',
+    'all_active',
+    'all_agents_idle',
+    'all_blocked',
+    'all_error',
+    'all_maintenance',
+    'all_waiting',
+    'any_active',
+    'any_blocked',
+    'any_error',
+    'any_maintenance',
+    'any_waiting',
+    'statustypes',
+    'temp_model',
+]
+
+__version__ = '1.2.0'
