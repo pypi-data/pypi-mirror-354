03df3edfef43397906408f87c7577ce7c9162d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:42 +0000

    Add package omlish/0.0.0.dev333/omlish-0.0.0.dev333-py3-none-any.whl

---
+++ packages/omlish/omlish-0.0.0.dev333-py3-none-any.whl/omlish/lang/classes/restrict.py
@@ -0,0 +1,157 @@
+import abc
+import functools
+import typing as ta
+
+from .abstract import Abstract
+
+
+##
+
+
+class FinalTypeError(TypeError):
+    def __init__(self, _type: type) -> None:
+        super().__init__()
+
+        self._type = _type
+
+    def __repr__(self) -> str:
+        return f'{type(self).__name__}({self._type})'
+
+
+class Final(Abstract):
+    __slots__ = ()
+
+    def __init_subclass__(cls, **kwargs: ta.Any) -> None:
+        super().__init_subclass__(**kwargs)
+
+        for base in cls.__bases__:
+            if base is Abstract:
+                raise FinalTypeError(base)
+            elif base is Final:
+                continue
+            elif Final in base.__mro__:
+                raise FinalTypeError(base)
+
+
+##
+
+
+class SealedError(TypeError):
+    def __init__(self, _type) -> None:
+        super().__init__()
+
+        self._type = _type
+
+    def __repr__(self) -> str:
+        return f'{type(self).__name__}({self._type})'
+
+
+class Sealed:
+    __slots__ = ()
+
+    def __init_subclass__(cls, **kwargs: ta.Any) -> None:
+        for base in cls.__bases__:
+            if base is not Abstract:
+                if Sealed in base.__bases__:
+                    if cls.__module__ != base.__module__:
+                        raise SealedError(base)
+        super().__init_subclass__(**kwargs)
+
+
+class PackageSealed:
+    __slots__ = ()
+
+    def __init_subclass__(cls, **kwargs: ta.Any) -> None:
+        for base in cls.__bases__:
+            if base is not Abstract:
+                if PackageSealed in base.__bases__:
+                    pfx = base.__module__.split('.')[:-1]
+                    if cls.__module__.split('.')[:len(pfx)] != pfx:
+                        raise SealedError(base)
+        super().__init_subclass__(**kwargs)
+
+
+##
+
+
+class NotInstantiable(Abstract):
+    __slots__ = ()
+
+    def __new__(cls, *args, **kwargs):  # noqa
+        raise TypeError
+
+
+class NotPicklable:
+    __slots__ = ()
+
+    @ta.final
+    def __reduce__(self) -> ta.NoReturn:
+        raise TypeError
+
+    @ta.final
+    def __reduce_ex__(self, protocol) -> ta.NoReturn:
+        raise TypeError
+
+    @ta.final
+    def __getstate__(self) -> ta.NoReturn:
+        raise TypeError
+
+    @ta.final
+    def __setstate__(self, state) -> ta.NoReturn:
+        raise TypeError
+
+
+##
+
+
+class NoBool:
+    def __bool__(self) -> bool:
+        raise TypeError
+
+
+class _NoBoolDescriptor:
+    def __init__(self, fn, instance=None, owner=None) -> None:
+        super().__init__()
+        self._fn = fn
+        self._instance = instance
+        self._owner = owner
+        functools.update_wrapper(self, fn)
+
+    def __bool__(self) -> bool:
+        raise TypeError
+
+    def __get__(self, instance, owner=None):
+        if instance is self._instance and owner is self._owner:
+            return self
+        return _NoBoolDescriptor(self._fn.__get__(instance, owner), instance, owner)
+
+    def __call__(self, *args, **kwargs):
+        return self._fn(*args, **kwargs)
+
+
+def no_bool(fn):  # noqa
+    return _NoBoolDescriptor(fn)
+
+
+##
+
+
+SENSITIVE_ATTR = '__sensitive__'
+
+
+class Sensitive:
+    __sensitive__ = True
+
+
+class _AnySensitiveMeta(abc.ABCMeta):
+    @classmethod
+    def __instancecheck__(cls, instance: object) -> bool:
+        return hasattr(instance, SENSITIVE_ATTR) or super().__instancecheck__(AnySensitive, instance)
+
+    @classmethod
+    def __subclasscheck__(cls, subclass: type) -> bool:
+        return hasattr(subclass, SENSITIVE_ATTR) or super().__subclasscheck__(AnySensitive, subclass)
+
+
+class AnySensitive(NotInstantiable, Final, metaclass=_AnySensitiveMeta):
+    pass
---
+++ packages/omlish/omlish-0.0.0.dev333-py3-none-any.whl/omlish/marshal/global_.py
@@ -0,0 +1,62 @@
+import typing as ta
+
+from .. import lang
+from .base import MarshalContext
+from .base import MarshalerFactory
+from .base import UnmarshalContext
+from .base import UnmarshalerFactory
+from .registries import Registry
+from .registries import RegistryItem
+from .standard import new_standard_marshaler_factory
+from .standard import new_standard_unmarshaler_factory
+from .values import Value
+
+
+T = ta.TypeVar('T')
+
+
+##
+
+
+GLOBAL_REGISTRY: Registry = Registry()
+
+
+##
+
+
+@lang.cached_function(lock=True)
+def global_marshaler_factory() -> MarshalerFactory:
+    return new_standard_marshaler_factory()
+
+
+def marshal(obj: ta.Any, ty: ta.Any | None = None, **kwargs: ta.Any) -> Value:
+    mc = MarshalContext(GLOBAL_REGISTRY, factory=global_marshaler_factory(), **kwargs)
+    return mc.make(ty if ty is not None else type(obj)).marshal(mc, obj)
+
+
+##
+
+
+@lang.cached_function(lock=True)
+def global_unmarshaler_factory() -> UnmarshalerFactory:
+    return new_standard_unmarshaler_factory()
+
+
+def unmarshal(v: Value, ty: type[T], **kwargs: ta.Any) -> T:
+    uc = UnmarshalContext(GLOBAL_REGISTRY, factory=global_unmarshaler_factory(), **kwargs)
+    return uc.make(ty).unmarshal(uc, v)
+
+
+##
+
+
+def register_global(
+        key: ta.Any,
+        *items: RegistryItem,
+        identity: bool = False,
+) -> None:
+    GLOBAL_REGISTRY.register(
+        key,
+        *items,
+        identity=identity,
+    )
