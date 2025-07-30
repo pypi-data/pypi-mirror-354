fb95b3f8540d6539eed488c3fedeb496140e02e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:46:21 +0000

    Add package pyvalhalla-git/3.5.1.post132/pyvalhalla_git-3.5.1.post132-cp312-cp312-win_amd64.whl

---
+++ packages/pyvalhalla-git/pyvalhalla_git-3.5.1.post132-cp312-cp312-win_amd64.whl/valhalla/__init__.py
@@ -0,0 +1,22 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_10_1():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, '.'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_10_1()
+del _delvewheel_patch_1_10_1
+# end delvewheel patch
+
+from pathlib import Path
+
+try:
+    from ._valhalla import *
+except ModuleNotFoundError:
+    from _valhalla import *
+from .actor import Actor
+from .config import get_config, get_help
+from .__version__ import __version__
+
+PYVALHALLA_DIR = Path(__file__).parent.resolve()
