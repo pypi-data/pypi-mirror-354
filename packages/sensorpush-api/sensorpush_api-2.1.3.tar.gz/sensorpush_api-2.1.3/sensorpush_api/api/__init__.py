59547a54a5b570aeef2a3e8e344377734131d9a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:00 +0000

    Add package sensorpush-api/2.1.3/sensorpush_api-2.1.3.tar.gz

---
+++ packages/sensorpush-api/sensorpush_api-2.1.3.tar.gz/sensorpush_api-2.1.3/sensorpush_api/api/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+
+# import apis into api package
+from sensorpush_api.api.api_api import ApiApi
+
---
+++ packages/sensorpush-api/sensorpush_api-2.1.3.tar.gz/sensorpush_api-2.1.3/setup.py
@@ -0,0 +1,54 @@
+import json
+import os.path
+from setuptools import find_packages, setup
+
+dirname = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(dirname, "README.md")) as f:
+    long_description = f.read()
+
+with open(os.path.join(dirname, "options.json")) as f:
+    options = json.load(f)
+
+with open(os.path.join(dirname, "requirements.txt")) as f:
+    install_requires = f.readlines()
+
+with open(os.path.join(dirname, "test-requirements.txt")) as f:
+    test_requires = f.readlines()
+
+with open(os.path.join(dirname, "release-requirements.txt")) as f:
+    release_requires = f.readlines()
+
+setup(name=options["projectName"],
+      version=options["packageVersion"],
+      description="SensorPush Public API",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author="Steven Stallion",
+      author_email="sstallion@gmail.com",
+      url="https://github.com/sstallion/sensorpush-api",
+      packages=find_packages(),
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Web Environment",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: 3.9",
+          "Programming Language :: Python :: 3.10",
+          "Programming Language :: Python :: 3.11",
+          "Programming Language :: Python :: 3.12",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Software Development :: Libraries",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      license="BSD-2-Clause",
+      keywords="sensorpush swagger",
+      install_requires=install_requires,
+      extras_require={
+          "test": test_requires,
+          "release": release_requires
+      })
