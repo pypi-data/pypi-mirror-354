3cdb407d6bbc871b691c0c63c9b97ead4abd608a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:07:15 +0000

    Add package ArtusAPI/1.3.14/ArtusAPI-1.3.14-py3-none-any.whl

---
+++ packages/ArtusAPI/ArtusAPI-1.3.14-py3-none-any.whl/ArtusAPI/robot/artus_lite/artus_lite_left.py
@@ -0,0 +1,16 @@
+
+# import sys
+# from pathlib import Path
+# # Current file's directory
+# current_file_path = Path(__file__).resolve()
+# # Add the desired path to the system path
+# desired_path = current_file_path.parent.parent.parent.parent.parent
+# sys.path.append(str(desired_path))
+# # print(desired_path)
+
+# from ArtusAPI.robot.artus_lite.artus_lite import ArtusLite
+from .artus_lite import ArtusLite
+
+class ArtusLite_LeftHand(ArtusLite): # change any properties for the left hand
+    def __init__(self):
+        super().__init__()
