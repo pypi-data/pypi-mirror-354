14607b874618b2013df4c50f6e4a88d21e21fc4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:43 +0000

    Add package pychisel/0.3/pychisel-0.3.tar.gz

---
+++ packages/pychisel/pychisel-0.3.tar.gz/pychisel-0.3/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+File: setup.py
+Location: pychisel
+Created at: 09/06/2025
+Author: Anderson Alves Monteiro <https://www.github.com/tekoryu>
+
+Setup file for PyChisel - Automated tools for quick preparation of Pandas DataFrames.
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='pychisel',
+    version='0.3',
+    description='Automated tools for quick preparation of Pandas DataFrames.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Anderson Monteiro',
+    author_email='alvesmonteiroanderson@gmail.com',
+    py_modules=['core', 'utils', 'exceptions'],  # List individual modules
+    packages=find_packages(where='.'),
+    package_dir={'': '.'},
+    install_requires=[
+        'pandas>=2.0.0',
+    ],
+    python_requires='>=3.11',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+    ],
+)
