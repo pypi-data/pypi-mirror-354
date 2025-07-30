4b961f0ea4ae0d379a3f897c606e6a65892e34ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:49 +0000

    Add package pychisel/0.3.1/pychisel-0.3.1-py3-none-any.whl

---
+++ packages/pychisel/pychisel-0.3.1-py3-none-any.whl/pychisel/exceptions.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+File: exceptions.py
+Location: pychisel
+Created at: 09/06/2025
+Author: Anderson Alves Monteiro <https://www.github.com/tekoryu>
+
+Custom exceptions for the PyChisel package.
+"""
+
+class SplittingError(Exception):
+    """Exception raised when there is an error during the splitting process."""
+    pass
---
+++ packages/pychisel/pychisel-0.3.1-py3-none-any.whl/pychisel/utils.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+File: utils.py
+Location: PyChisel
+Created at: 09/06/2025
+Author: Anderson Alves Monteiro <https://www.github.com/tekoryu>
+
+Utility functions for the PyChisel package.
+"""
+
+# This module will contain utility functions in future versions
