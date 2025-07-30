c10cf20b58580c563778016d371dfdf56f545b29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:04 +0000

    Add package xtquant/250516.1.0/xtquant-250516.1.0.tar.gz

---
+++ packages/xtquant/xtquant-250516.1.0.tar.gz/xtquant-250516.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import requests
+
+setup(
+    name='xtquant',
+    version='250516.1.0',
+    packages=find_packages(),
+    description='xtquant_250516',
+    long_description=open('README.md',encoding="utf-8").read(),
+    long_description_content_type='text/markdown',
+    author='Willows',
+    author_email='972210116@qq.com',
+    url='http://dict.thinktrader.net/?id=7zqjlm',
+    package_data={
+        # 如果你的pyd文件在包的子目录中
+        'xtquant': ['*.*', '**/*.*'],
+    },
+    python_requires='>=3.6, <3.14',
+    license='GNU GPLv3 ',
+    install_requires=[
+        # 依赖列表
+        "requests",
+        "pandas",
+        "numpy",
+        "tqdm"
+    ],
+)
