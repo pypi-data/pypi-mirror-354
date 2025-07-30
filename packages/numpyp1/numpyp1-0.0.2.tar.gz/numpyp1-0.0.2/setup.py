f398dd6c47df14e36542adb50999dbd9b82cfcbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:54 +0000

    Add package numpyp1/0.0.2/numpyp1-0.0.2.tar.gz

---
+++ packages/numpyp1/numpyp1-0.0.2.tar.gz/numpyp1-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md', encoding='utf-8') as f:
+    return f.read()
+
+
+setup(
+  name='numpyp1',
+  version='0.0.2',
+  description='This is the simplest module for quick work with files.',
+  license='MIT',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  packages=find_packages(),
+  package_data={
+    'numpyp1.theory': ['*.png','*.pdf'], 'numpyp1': ['.env'],
+    },
+  install_requires=[
+    'IPython','pyperclip','python-telegram-bot',
+    'python-dotenv'
+  ],
+)
+
+
