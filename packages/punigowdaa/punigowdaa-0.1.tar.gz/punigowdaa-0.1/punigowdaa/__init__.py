99b8a104ada03ada01d02c083a9ac1e25f69452d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:36 +0000

    Add package punigowdaa/0.1/punigowdaa-0.1.tar.gz

---
+++ packages/punigowdaa/punigowdaa-0.1.tar.gz/punigowdaa-0.1/punigowdaa/__init__.py
@@ -0,0 +1,3 @@
+
+# Makes the directory a Python package
+__version__ = "0.1"
---
+++ packages/punigowdaa/punigowdaa-0.1.tar.gz/punigowdaa-0.1/tests/test_genai_programs.py
@@ -0,0 +1,4 @@
+from punigowdaa.genai_programs import print_genai_programs
+
+def test_say_hello():
+    assert test_say_hello("World") == "Hello, World!"
