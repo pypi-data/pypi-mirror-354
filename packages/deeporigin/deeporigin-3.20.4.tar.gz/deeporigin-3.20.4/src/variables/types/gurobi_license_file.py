5d8bd815346e14ad90276c7b18ba08c1ce7ea88b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:40 +0000

    Add package deeporigin/3.20.4/deeporigin-3.20.4.tar.gz

---
+++ packages/deeporigin/deeporigin-3.20.4.tar.gz/deeporigin-3.20.4/src/variables/types/gurobi_license_file.py
@@ -0,0 +1,15 @@
+"""class to handle gurobi license file"""
+
+from .secret_file_value import SecretFileValue
+
+
+class GurobiLicenseFile(SecretFileValue):
+    """Gurobi license file"""
+
+    class Meta:
+        platform_id = "Gurobi license file"
+
+    @classmethod
+    @property
+    def FILENAME(cls) -> str:
+        return "~/gurobi.lic"
