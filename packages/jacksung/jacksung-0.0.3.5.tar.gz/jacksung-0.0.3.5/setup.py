c320b4bcf7eeaf40b1513ed43d62ecfb1a672242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:05 +0000

    Add package jacksung/0.0.3.5/jacksung-0.0.3.5.tar.gz

---
+++ packages/jacksung/jacksung-0.0.3.5.tar.gz/jacksung-0.0.3.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jacksung',
+    version='0.0.3.5',
+    author='Zijiang Song',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'tqdm',
+        'requests',
+        'pymysql',
+        'pytz',
+        'selenium',
+        'termcolor',
+        'einops',
+        'rasterio',
+        'netCDF4',
+        'pyyaml',
+        'opencv-python',
+        'Pillow',
+        'openai'
+    ],
+    entry_points={
+        'console_scripts': [
+            'ecnu_login = jacksung.utils.login:main',
+            'watch_gpu = jacksung.utils.nvidia:main'
+        ]
+    }
+)
