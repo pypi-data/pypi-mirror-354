5490c096ba8526bcaf330fb44fe9d270c4ba868a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:36:07 +0000

    Add package YouTubeMusic/7.5.0/youtubemusic-7.5.0.tar.gz

---
+++ packages/YouTubeMusic/youtubemusic-7.5.0.tar.gz/youtubemusic-7.5.0/YouTubeMusic/__init__.py
@@ -0,0 +1,9 @@
+from .YtSearch import Search
+from .Models import format_dur, process_video, extract_artist
+from .Utils import parse_dur, format_ind, format_views
+from .Search import Search
+
+
+
+__version__ = "7.5.0"
+__author__ = "ABHISHEK THAKUR"
