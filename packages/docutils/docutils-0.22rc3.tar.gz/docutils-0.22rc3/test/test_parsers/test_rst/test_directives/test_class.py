781d0d7b36ba43ffab3e5557218d9b1b0b406730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:42 +0000

    Add package docutils/0.22rc3/docutils-0.22rc3.tar.gz

---
+++ packages/docutils/docutils-0.22rc3.tar.gz/docutils-0.22rc3/test/test_parsers/test_rst/test_directives/test_class.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python3
+
+# $Id: test_class.py 9425 2023-06-30 14:56:47Z milde $
+# Author: Lea Wiemann <LeWiemann@gmail.com>
+# Copyright: This module has been placed in the public domain.
+
+"""
+Tests for the 'class' directive.
+"""
+
+from pathlib import Path
+import sys
+import unittest
+
+if __name__ == '__main__':
+    # prepend the "docutils root" to the Python library path
+    # so we import the local `docutils` package.
+    sys.path.insert(0, str(Path(__file__).resolve().parents[4]))
+
+from docutils.frontend import get_default_settings
+from docutils.parsers.rst import Parser
+from docutils.utils import new_document
+
+
+class ParserTestCase(unittest.TestCase):
+    def test_parser(self):
+        parser = Parser()
+        settings = get_default_settings(Parser)
+        settings.warning_stream = ''
+        for name, cases in totest.items():
+            for casenum, (case_input, case_expected) in enumerate(cases):
+                with self.subTest(id=f'totest[{name!r}][{casenum}]'):
+                    document = new_document('test data', settings.copy())
+                    parser.parse(case_input, document)
+                    output = document.pformat()
+                    self.assertEqual(case_expected, output)
+
+
+totest = {}
+
+totest['class'] = [
+["""\
+.. class:: class1  class2
+""",
+"""\
+<document source="test data">
+    <pending>
+        .. internal attributes:
+             .transform: docutils.transforms.misc.ClassAttribute
+             .details:
+               class: ['class1', 'class2']
+               directive: 'class'
+"""],
+["""\
+.. class:: class1  class2
+
+   The classes are applied to this paragraph.
+
+   And this one.
+""",
+"""\
+<document source="test data">
+    <paragraph classes="class1 class2">
+        The classes are applied to this paragraph.
+    <paragraph classes="class1 class2">
+        And this one.
+"""],
+]
+
+
+if __name__ == '__main__':
+    unittest.main()
