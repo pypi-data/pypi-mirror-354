4a41df650cafc157b014103890c64d1f7e3dfc1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:07:19 +0000

    Add package pgai/0.11.1/pgai-0.11.1.tar.gz

---
+++ packages/pgai/pgai-0.11.1.tar.gz/pgai-0.11.1/pgai/vectorizer/__init__.py
@@ -0,0 +1,10 @@
+from .create_vectorizer import CreateVectorizer
+from .vectorizer import Executor, Vectorizer
+from .worker import Worker
+
+__all__ = [
+    "Vectorizer",
+    "Executor",
+    "CreateVectorizer",
+    "Worker",
+]
