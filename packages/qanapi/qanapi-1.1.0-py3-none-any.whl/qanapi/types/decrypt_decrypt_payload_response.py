39518f6610cfd99d9e9023d4edefaacc4155cd4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:11:04 +0000

    Add package qanapi/1.1.0/qanapi-1.1.0-py3-none-any.whl

---
+++ packages/qanapi/qanapi-1.1.0-py3-none-any.whl/qanapi/types/decrypt_decrypt_payload_response.py
@@ -0,0 +1,8 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Dict, List, Union
+from typing_extensions import TypeAlias
+
+__all__ = ["DecryptDecryptPayloadResponse"]
+
+DecryptDecryptPayloadResponse: TypeAlias = Union[str, Dict[str, object], List[object]]
