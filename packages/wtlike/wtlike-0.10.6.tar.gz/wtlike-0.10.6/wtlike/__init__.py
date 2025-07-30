79f22a0233fefe4c5b9ca0584ac32f07ff57048c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:42 +0000

    Add package wtlike/0.10.6/wtlike-0.10.6.tar.gz

---
+++ packages/wtlike/wtlike-0.10.6.tar.gz/wtlike-0.10.6/wtlike/__init__.py
@@ -0,0 +1,21 @@
+"""
+# wtlike--make FermiLAT gamma-ray Light curves
+
+see https://tburnett.github.io/wtlike/tutorial
+"""
+
+__version__ = "0.10.5"
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+from .config import Config, UTC, MJD, Timer, FermiInterval
+from .data_man import check_data, update_data, DataView
+from .sources import PointSource, SourceFinder
+from .exposure import WeightedAeff
+from .loglike import PoissonRep
+from .simulation import Simulation
+from .time_series import TimeSeries
+from .main import WtLike
