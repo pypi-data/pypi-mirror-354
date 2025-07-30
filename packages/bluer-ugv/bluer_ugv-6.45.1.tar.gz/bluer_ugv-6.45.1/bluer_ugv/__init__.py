c59658a7925378d5f836cf7e82c1a582be3b92dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:55 +0000

    Add package bluer-ugv/6.45.1/bluer_ugv-6.45.1.tar.gz

---
+++ packages/bluer-ugv/bluer_ugv-6.45.1.tar.gz/bluer_ugv-6.45.1/bluer_ugv/__init__.py
@@ -0,0 +1,17 @@
+NAME = "bluer_ugv"
+
+ICON = "🐬"
+
+DESCRIPTION = f"{ICON} AI x ROS."
+
+VERSION = "6.45.1"
+
+REPO_NAME = "bluer-ugv"
+
+MARQUEE = "https://raw.githubusercontent.com/kamangir/assets2/refs/heads/main/bluer-swallow/20250608_144453.jpg"
+
+ALIAS = "@ugv"
+
+
+def fullname() -> str:
+    return f"{NAME}-{VERSION}"
