5bde19ba1f722fb62872b88dda61fbc9ab5d0cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:21 +0000

    Add package sparse-ir/1.1.3/sparse-ir-1.1.3.tar.gz

---
+++ packages/sparse-ir/sparse-ir-1.1.3.tar.gz/sparse-ir-1.1.3/src/sparse_ir/__init__.py
@@ -0,0 +1,21 @@
+
+"""
+Intermediate representation (IR) for many-body propagators
+==========================================================
+
+This library provides routines for constructing and working with the
+intermediate representation of correlation functions.  It provides:
+
+ - on-the-fly computation of basis functions for arbitrary cutoff Λ
+ - basis functions and singular values are accurate to full precision
+ - routines for sparse sampling
+"""
+__copyright__ = "2020-2025 Markus Wallerberger, Hiroshi Shinaoka, and others"
+__license__ = "MIT"
+__version__ = "1.1.3"
+
+from .kernel import RegularizedBoseKernel, LogisticKernel
+from .sve import compute as compute_sve, SVEResult
+from .basis import FiniteTempBasis, finite_temp_bases
+from .basis_set import FiniteTempBasisSet
+from .sampling import TauSampling, MatsubaraSampling
