96e126155337491d92f4c7311df815d29b0479c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:09:20 +0000

    Add package oarepo-vocabularies/2.2.3/oarepo_vocabularies-2.2.3-py3-none-any.whl

---
+++ packages/oarepo-vocabularies/oarepo_vocabularies-2.2.3-py3-none-any.whl/oarepo_vocabularies/ui/theme/assets/semantic-ui/js/oarepo_vocabularies_ui/form/components/VocabularySelectField/VocabularySelectField.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { RelatedSelectField } from "@js/oarepo_ui";
+import { serializeVocabularySuggestions } from "@js/oarepo_vocabularies";
+import PropTypes from "prop-types";
+
+// for adding free text items
+const serializeAddedValue = (value) => {
+  return { text: value, value, key: value, name: value, id: value };
+};
+
+export const VocabularySelectField = ({
+  type,
+  fieldPath,
+  externalAuthority,
+  multiple,
+  filterFunction,
+  ...restProps
+}) => {
+  const suggestionsConfig = {
+    suggestionAPIUrl: `/api/vocabularies/${type}`,
+  };
+  if (externalAuthority) {
+    suggestionsConfig.externalSuggestionApi = `${suggestionsConfig.suggestionAPIUrl}/authoritative`;
+  }
+
+  function _serializeSuggestions(suggestions) {
+    // We need to do post-filtering here (it seems impossible to add pre-filter to suggestion API query)
+    return serializeVocabularySuggestions(suggestions).filter((opt) =>
+      filterFunction(opt)
+    );
+  }
+
+  return (
+    <RelatedSelectField
+      fieldPath={fieldPath}
+      {...suggestionsConfig}
+      selectOnBlur={false}
+      serializeSuggestions={_serializeSuggestions}
+      multiple={multiple}
+      deburr
+      serializeAddedValue={serializeAddedValue}
+      {...restProps}
+    />
+  );
+};
+
+VocabularySelectField.propTypes = {
+  type: PropTypes.string.isRequired,
+  fieldPath: PropTypes.string.isRequired,
+  externalAuthority: PropTypes.bool,
+  multiple: PropTypes.bool,
+  filterFunction: PropTypes.func,
+};
+
+VocabularySelectField.defaultProps = {
+  multiple: false,
+  externalAuthority: false,
+  filterFunction: (opt) => opt,
+};
