7d690115376bb7e2cf60176e10f3b0218b7e7be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:07:17 +0000

    Add package foundry-sdk/0.0.85/foundry_sdk-0.0.85-py3-none-any.whl

---
+++ packages/foundry-sdk/foundry_sdk-0.0.85-py3-none-any.whl/foundry_sdk/etl/constants.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class DummyNames(Enum):
+    DUMMY_PRODUCT = "dummy_product"
+    DUMMY_CATEGORY_LEVEL = "dummy_category_level"
+    DUMMY_CATEGORY = "dummy_category"
+    DUMMY_STORE = "dummy_store"
+
+class FlagLevels(Enum):
+    MISSING_VALUE = "missing_value"
+    NOT_FOR_SALE = "not_for_sale"
+ 
