2945365f0471dea3fde8896fec14a79d296c7f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:30 +0000

    Add package cosmopower-jax/0.5.7/cosmopower_jax-0.5.7.tar.gz

---
+++ packages/cosmopower-jax/cosmopower_jax-0.5.7.tar.gz/cosmopower_jax-0.5.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import shutil
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+PACKAGENAME = 'cosmopower_jax'
+
+setup(
+    name='cosmopower_jax',
+    version="0.5.7",
+    author='Davide Piras',
+    author_email='davide.piras@unige.ch',
+    description='Differentiable cosmological emulators',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/dpiras/cosmopower-jax',
+    license='GNU General Public License v3.0 (GPLv3)',
+    packages=find_packages(),
+    package_data= {'cosmopower_jax': ['trained_models/*.pkl', 'trained_models/*.npz', 'pca_utils/*.npy']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    install_requires=['jax',
+                      'jaxlib',
+                      'matplotlib>=3.1.2',
+                      'numpy>=1.17.4',
+                      ]
+                      )
