523f95b7652fb892d9dc245bb1b8c7a08c02fc64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:26 +0000

    Add package speedsense/0.1.0/speedsense-0.1.0.tar.gz

---
+++ packages/speedsense/speedsense-0.1.0.tar.gz/speedsense-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+setup(
+    name='speedsense',
+    packages=find_packages(include=['speedsense']),
+    version='0.1.0',
+    description='Time complexity analyser',
+    author='Anant Terkar',
+    url="https://github.com/anantterkar/SpeedSense",
+    author_email="20bcs014@iiitdwd.ac.in",
+    license="MIT"
+)
