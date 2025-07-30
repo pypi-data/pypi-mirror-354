661510ef32a13ed587a0c65e43cc5840d475591d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:11 +0000

    Add package tastytrade/10.2.3/tastytrade-10.2.3-py3-none-any.whl

---
+++ packages/tastytrade/tastytrade-10.2.3-py3-none-any.whl/tastytrade/__init__.py
@@ -0,0 +1,26 @@
+import logging
+
+API_URL = "https://api.tastyworks.com"
+BACKTEST_URL = "https://backtester.vast.tastyworks.com"
+CERT_URL = "https://api.cert.tastyworks.com"
+VAST_URL = "https://vast.tastyworks.com"
+VERSION = "10.2.3"
+
+__version__ = VERSION
+version_str: str = f"tastyware/tastytrade:v{VERSION}"
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# ruff: noqa: E402
+
+from .account import Account
+from .session import Session
+from .streamer import AlertStreamer, DXLinkStreamer
+
+__all__ = [
+    "Account",
+    "AlertStreamer",
+    "DXLinkStreamer",
+    "Session",
+]
