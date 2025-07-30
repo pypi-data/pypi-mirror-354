28167adc6a50e0bcb83d2aff1a9170752a6cedda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:09:57 +0000

    Add package modelconv/0.4.0/modelconv-0.4.0-py3-none-any.whl

---
+++ packages/modelconv/modelconv-0.4.0-py3-none-any.whl/modelconverter/__init__.py
@@ -0,0 +1,18 @@
+import pkg_resources
+from luxonis_ml.utils import PUT_FILE_REGISTRY
+
+from .hub import convert
+
+__version__ = "0.4.0"
+
+__all__ = ["convert"]
+
+
+def load_put_file_plugins() -> None:
+    """Registers any external put file plugins."""
+    for entry_point in pkg_resources.iter_entry_points("put_file_plugins"):
+        plugin_class = entry_point.load()
+        PUT_FILE_REGISTRY.register_module(module=plugin_class)
+
+
+load_put_file_plugins()
