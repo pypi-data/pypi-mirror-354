c399c4542814b56412ba0caebc845de30c1ad73e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:15 +0000

    Add package nemo-library-ui/0.1.1/nemo_library_ui-0.1.1.tar.gz

---
+++ packages/nemo-library-ui/nemo_library_ui-0.1.1.tar.gz/nemo_library_ui-0.1.1/README.md
@@ -0,0 +1,3 @@
+# nemo_library_ui
+
+A web-based UI for the NEMO AI library using FastAPI and Jinja2.
---
+++ packages/nemo-library-ui/nemo_library_ui-0.1.1.tar.gz/nemo_library_ui-0.1.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+# Import the version from nemo_library.version
+from nemo_library_ui.version import __version__
+
+setup(
+    name="nemo_library_ui",
+    version=__version__,
+    author="Gunnar Schug",
+    author_email="gunnar.schug@nemo-ai.com",
+    description="UI for the NEMO Python library",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/NEMOGunnar/nemo_library_ui",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "fastapi",
+        "uvicorn",
+        "jinja2",
+        "nemo_library",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: FastAPI",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.13",
+    entry_points={
+        "console_scripts": [
+            "nemo-library-ui = nemo_library_ui.ui:main"
+        ]
+    }
+)
