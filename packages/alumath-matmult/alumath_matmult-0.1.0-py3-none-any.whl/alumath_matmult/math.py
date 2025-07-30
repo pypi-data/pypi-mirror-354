08d2addbcc2948915a7c36c19307e1722d7b7d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:19 +0000

    Add package alumath-matmult/0.1.0/alumath_matmult-0.1.0-py3-none-any.whl

---
+++ packages/alumath-matmult/alumath_matmult-0.1.0-py3-none-any.whl/alumath_matmult/math.py
@@ -0,0 +1,14 @@
+def matrix_multiply(A, B):
+    if len(A[0]) != len(B):
+        raise ValueError("You can't multiply these matrices")
+    
+    result = []
+    for i in range(len(A)):
+        row = []
+        for j in range(len(B[0])):
+            sum_val = 0
+            for k in range(len(B)):
+                sum_val += A[i][k] * B[k][j]
+            row.append(sum_val)
+        result.append(row)
+    return result
