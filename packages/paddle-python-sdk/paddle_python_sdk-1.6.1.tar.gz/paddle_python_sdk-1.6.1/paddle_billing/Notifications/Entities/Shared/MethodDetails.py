beb28a1c726c81acc1b9cf909774e41663ae2cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:08:21 +0000

    Add package paddle-python-sdk/1.6.1/paddle_python_sdk-1.6.1.tar.gz

---
+++ packages/paddle-python-sdk/paddle_python_sdk-1.6.1.tar.gz/paddle_python_sdk-1.6.1/paddle_billing/Notifications/Entities/Shared/MethodDetails.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import Any
+
+from paddle_billing.Notifications.Entities.Shared.Card import Card
+from paddle_billing.Notifications.Entities.Shared.PaymentMethodType import PaymentMethodType
+
+
+@dataclass
+class MethodDetails:
+    type: PaymentMethodType
+    card: Card | None
+
+    @staticmethod
+    def from_dict(data: dict[str, Any]) -> MethodDetails:
+        return MethodDetails(
+            PaymentMethodType(data["type"]),
+            Card.from_dict(data["card"]) if data.get("card") else None,
+        )
