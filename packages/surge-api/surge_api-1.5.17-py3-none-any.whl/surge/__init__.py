1b3a5fddf9b49a274a6b25e4773a97395e70d47e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:43 +0000

    Add package surge-api/1.5.17/surge_api-1.5.17-py3-none-any.whl

---
+++ packages/surge-api/surge_api-1.5.17-py3-none-any.whl/surge/__init__.py
@@ -0,0 +1,9 @@
+import os
+
+from surge.projects import Project
+from surge.tasks import Task
+from surge.teams import Team
+from surge.reports import Report
+
+api_key = os.environ.get("SURGE_API_KEY", None)
+base_url = os.environ.get("SURGE_BASE_URL", "https://app.surgehq.ai/api")
