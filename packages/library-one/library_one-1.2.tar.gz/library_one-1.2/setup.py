ac9bd781a43993f7d7c9d92d2a19762f4407220e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:21 +0000

    Add package library-one/1.2/library_one-1.2.tar.gz

---
+++ packages/library-one/library_one-1.2.tar.gz/library_one-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="library-one",
+    description="practice code 1",
+    version=1.02,
+    author="ashjaa khan",
+    author_email="ashjaakhan@gmai.com"
+)
