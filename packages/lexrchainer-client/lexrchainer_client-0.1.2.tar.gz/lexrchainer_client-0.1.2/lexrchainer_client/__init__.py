2cbf9e079446a4cca6d6817d14382b87543ffd93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:34 +0000

    Add package lexrchainer-client/0.1.2/lexrchainer_client-0.1.2.tar.gz

---
+++ packages/lexrchainer-client/lexrchainer_client-0.1.2.tar.gz/lexrchainer_client-0.1.2/lexrchainer_client/__init__.py
@@ -0,0 +1,21 @@
+"""
+LexrChainer Client Package
+This package contains client-side code for interacting with the LexrChainer API.
+"""
+
+from .client_interface import ClientInterface, ClientConfig, ClientMode
+from .agent_builder import AgentBuilder, MultiAgentBuilder, AgentWrapper, MultiAgentWrapper
+from .models import ModelInfo, FunctionDefinition, FunctionParameters
+
+__all__ = [
+    'ClientInterface',
+    'ClientConfig',
+    'ClientMode',
+    'AgentBuilder',
+    'MultiAgentBuilder',
+    'AgentWrapper',
+    'MultiAgentWrapper',
+    'ModelInfo',
+    'FunctionDefinition',
+    'FunctionParameters'
+] 
