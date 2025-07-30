db3325b7800f5f6bb17fcc1e4c933d31da723447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:50 +0000

    Add package multiphysics/0.0.2/multiphysics-0.0.2.tar.gz

---
+++ packages/multiphysics/multiphysics-0.0.2.tar.gz/multiphysics-0.0.2/multiphysics/__init__.py
@@ -0,0 +1,3 @@
+def test_dummy():
+    """A dummy test function."""
+    return True
---
+++ packages/multiphysics/multiphysics-0.0.2.tar.gz/multiphysics-0.0.2/tests/test_dummy.py
@@ -0,0 +1,6 @@
+import multiphysics as mpx
+
+
+def test_dummy():
+    """dummy test to check if multiphysics is installed correctly"""
+    assert mpx.test_dummy()
