2e57176334f834fdb0d5c171b645b3d863704a80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:07 +0000

    Add package ease-grid/0.3/ease_grid-0.3.tar.gz

---
+++ packages/ease-grid/ease_grid-0.3.tar.gz/ease_grid-0.3/setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Setup file for ease_grid.
+This file is kept for backward compatibility.
+All configuration is now in pyproject.toml.
+"""
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
