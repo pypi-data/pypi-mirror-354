a458ec21191d3d75d3706750f925e72535fb42dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:07:30 +0000

    Add package RepoAuditor/0.2.35/repoauditor-0.2.35.tar.gz

---
+++ packages/RepoAuditor/repoauditor-0.2.35.tar.gz/repoauditor-0.2.35/src/RepoAuditor/Plugins/GitHub/StandardQueryRequirements/SquashCommitMerge.py
@@ -0,0 +1,44 @@
+# -------------------------------------------------------------------------------
+# |
+# |  Copyright (c) 2024 Scientific Software Engineering Center at Georgia Tech
+# |  Distributed under the MIT License.
+# |
+# -------------------------------------------------------------------------------
+"""Contains the SquashCommitMerge object."""
+
+import textwrap
+
+from RepoAuditor.Plugins.GitHub.StandardQueryRequirements.Impl.StandardEnableRequirementImpl import (
+    StandardEnableRequirementImpl,
+)
+
+
+# ----------------------------------------------------------------------
+class SquashCommitMerge(StandardEnableRequirementImpl):
+    """Allow squash merging."""
+
+    # ----------------------------------------------------------------------
+    def __init__(self) -> None:
+        super().__init__(
+            "SquashCommitMerge",
+            False,
+            "true",
+            "settings",
+            "Pull Requests",
+            "Allow squash merging",
+            lambda data: data["standard"].get("allow_squash_merge", None),
+            textwrap.dedent(
+                """\
+                The default behavior is to not allow squash merging.
+
+                Reasons for this Default
+                ------------------------
+                - Rebase merging is not compatible with signed commits, as GitHub creates a new commit when squashing.
+
+                Reasons to Override this Default
+                --------------------------------
+                - Your repository does not require signatures.
+                - You want to ensure that single-commit-changes are merged into the mainline branch to simplify the branch's history.
+                """,
+            ),
+        )
