63d10ee6eec29038917bec39bdbae325247a8cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:12 +0000

    Add package mm-print/0.1.0/mm_print-0.1.0-py3-none-any.whl

---
+++ packages/mm-print/mm_print-0.1.0-py3-none-any.whl/mm_print/__init__.py
@@ -0,0 +1,5 @@
+from .output import fatal as fatal
+from .output import print_json as json  # noqa: F401
+from .output import print_plain as plain  # noqa: F401
+from .output import print_table as table  # noqa: F401
+from .output import print_toml as toml  # noqa: F401
