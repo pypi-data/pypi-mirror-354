d0ecc89d2cb2a53e416ee62318941f1d4191d231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:07:02 +0000

    Add package my-pqr-package-ashjaa/1.1/my_pqr_package_ashjaa-1.1.tar.gz

---
+++ packages/my-pqr-package-ashjaa/my_pqr_package_ashjaa-1.1.tar.gz/my_pqr_package_ashjaa-1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="my_pqr_package_ashjaa",
+    description="this package is custom package with some functions",
+    version=1.01,
+    packages=find_packages(),
+    author="ashjaa khan",
+    author_email="ashjaakhan@gmail.com"
+)
