a0ea1e40b67b4350c7c98ef3c52e6bfb9b498656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:10 +0000

    Add package FDTDream/0.2.11/fdtdream-0.2.11.tar.gz

---
+++ packages/FDTDream/fdtdream-0.2.11.tar.gz/fdtdream-0.2.11/src/__init__.py
@@ -0,0 +1,4 @@
+from src.fdtdream.db_gui import FDTDiscover
+from .fdtdream import FDTDream, get_lumapi_location, set_lumapi_location, Materials, Simulation
+
+__all__ = ["FDTDiscover", "FDTDream", "get_lumapi_location", "set_lumapi_location", "Materials", "Simulation"]
