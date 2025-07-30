238551b04593cdb7fedcdee0e329e5fc48e3bea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:50 +0000

    Add package Kpa/1.0.48/kpa-1.0.48-py3-none-any.whl

---
+++ packages/Kpa/kpa-1.0.48-py3-none-any.whl/kpa/__init__.py
@@ -0,0 +1,10 @@
+
+def _f() -> None:
+    import sys
+    if sys.version_info < (3, 6):
+        print("Requires Python 3.6+")
+        sys.exit(1)
+_f()
+del _f
+
+from .dev_utils import run as lint
