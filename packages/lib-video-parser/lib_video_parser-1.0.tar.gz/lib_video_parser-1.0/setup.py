9f43c7e698aa377d7a4d10b7d84c99804c9c5440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:30 +0000

    Add package lib-video-parser/1.0/lib_video_parser-1.0.tar.gz

---
+++ packages/lib-video-parser/lib_video_parser-1.0.tar.gz/lib_video_parser-1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='lib_video_parser',
+	version='1.0',
+	packages=find_packages(),
+	install_requires=[
+		'requests',
+		'beautifulsoup4',
+		'fake_useragent',
+	],
+	description='Library for parse video files',
+	long_description=open('README.md', encoding='utf-8').read(),
+	long_description_content_type="text/markdown",
+	author='Yurij',
+	author_email='yuran.ignatenko@yandex.ru',
+	url='https://github.com/YuranIgnatenko/lib_video_parser',
+)
