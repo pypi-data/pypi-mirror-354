e379a60129fa915d253429ad1ec55d9efac6e865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:27 +0000

    Add package sonusai-torchl/1.0.2/sonusai_torchl-1.0.2.tar.gz

---
+++ packages/sonusai-torchl/sonusai_torchl-1.0.2.tar.gz/sonusai_torchl-1.0.2/README.rst
@@ -0,0 +1,5 @@
+SonusAI Torchl: PyTorch Lightning model tools for SonusAI
+
+
+Uses Poetry to manage and publish to Python PyPi.
+Note use poetry config pypi-token.pypi your-api-token, then poetry publish
---
+++ packages/sonusai-torchl/sonusai_torchl-1.0.2.tar.gz/sonusai_torchl-1.0.2/sonusai_torchl/data_generator/__init__.py
@@ -0,0 +1,6 @@
+# SonusAI PyTorch Lightning data generator classes
+# ruff: noqa: F401
+
+from .dataloader_utils import AawareDataLoader
+from .mixdb_dataset import TorchFromMixtureDatabase
+from .speaker_id_mixdb_dataset import TorchFromSpeakerIDMixtureDatabase
