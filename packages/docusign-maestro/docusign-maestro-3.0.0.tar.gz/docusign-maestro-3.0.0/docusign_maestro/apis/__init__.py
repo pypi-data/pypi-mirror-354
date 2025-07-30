33542b1dd44b7eced0cacbf8f6bd49690f719456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:08 +0000

    Add package docusign-maestro/3.0.0/docusign-maestro-3.0.0.tar.gz

---
+++ packages/docusign-maestro/docusign-maestro-3.0.0.tar.gz/docusign-maestro-3.0.0/docusign_maestro/apis/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from .workflow_instance_management_api import WorkflowInstanceManagementApi
+from .workflow_management_api import WorkflowManagementApi
+from .workflow_trigger_api import WorkflowTriggerApi
