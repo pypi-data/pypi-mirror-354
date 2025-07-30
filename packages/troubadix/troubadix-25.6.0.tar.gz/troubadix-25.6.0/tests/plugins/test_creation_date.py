818f30d944bbe3eed12cf9fbb1951c0b99baf06d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:09:48 +0000

    Add package troubadix/25.6.0/troubadix-25.6.0.tar.gz

---
+++ packages/troubadix/troubadix-25.6.0.tar.gz/troubadix-25.6.0/tests/plugins/test_creation_date.py
@@ -0,0 +1,258 @@
+# Copyright (C) 2021 Greenbone AG
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from pathlib import Path
+from unittest.mock import MagicMock
+
+from troubadix.plugin import LinterError
+from troubadix.plugins.creation_date import CheckCreationDate
+
+from . import PluginTestCase
+
+
+class CheckCreationDateTestCase(PluginTestCase):
+    def test_ok(self):
+        path = Path("some/file.nasl")
+        content = (
+            '  script_tag(name:"creation_date", value:"2013-05-14 11:24:55 '
+            '+0200 (Tue, 14 May 2013)");\n'
+        )
+        fake_context = self.create_file_plugin_context(
+            nasl_file=path, file_content=content
+        )
+        plugin = CheckCreationDate(fake_context)
+
+        results = list(plugin.run())
+        self.assertEqual(len(results), 0)
+
+    def test_exclude_inc_file(self):
+        path = Path("some/file.inc")
+        fake_context = self.create_file_plugin_context(nasl_file=path)
+        plugin = CheckCreationDate(fake_context)
+
+        results = list(plugin.run())
+
+        self.assertEqual(len(results), 0)
+
+    def test_missing(self):
+        path = Path("some/file.nasl")
+        content = (
+            '  script_tag(name:"cvss_base", value:"7.5");\n'
+            '  script_tag(name:"cvss_base_vector", '
+            'value:"AV:N/AC:L/Au:N/C:P/I:P/A:P");\n'
+        )
+        fake_context = self.create_file_plugin_context(
+            nasl_file=path, file_content=content
+        )
+        plugin = CheckCreationDate(fake_context)
+
+        results = list(plugin.run())
+
+        self.assertEqual(len(results), 1)
+
+        self.assertIsInstance(results[0], LinterError)
+        self.assertEqual(
+            "No creation date has been found.",
+            results[0].message,
+        )
+
+    def test_wrong_weekday(self):
+        path = Path("some/file.nasl")
+        content = (
+            '  script_tag(name:"creation_date", value:"2013-05-14 11:24:55 '
+            '+0200 (Mon, 14 May 2013)");\n'
+        )
+        fake_context = self.create_file_plugin_context(
+            nasl_file=path, file_content=content
+        )
+        plugin = CheckCreationDate(fake_context)
+
+        results = list(plugin.run())
+
+        self.assertEqual(len(results), 1)
+
+        self.assertIsInstance(results[0], LinterError)
+        self.assertEqual(
+            "Wrong day of week. Please change it from 'Mon' to 'Tue'.",
+            results[0].message,
+        )
+
+    def test_no_timezone(self):
+        path = Path("some/file.nasl")
+        content = (
+            '  script_tag(name:"creation_date", value:"2013-05-14 11:24:55 '
+            '(Tue, 14 May 2013)");\n'
+        )
+        fake_context = self.create_file_plugin_context(
+            nasl_file=path, file_content=content
+        )
+        plugin = CheckCreationDate(fake_context)
+
+        results = list(plugin.run())
+
+        self.assertEqual(len(results), 1)
+
+        self.assertIsInstance(results[0], LinterError)
+        self.assertEqual(
+            "False or incorrectly formatted creation_date.",
+            results[0].message,
+        )
+
+    def test_different_dates(self):
+        path = Path("some/file.nasl")
+        content = (
+            '  script_tag(name:"creation_date", value:"2013-05-14 11:24:55 '
+            '+0200 (Tue, 15 May 2013)");\n'
+        )
+        fake_context = self.create_file_plugin_context(
+            nasl_file=path, file_content=content
+        )
+        plugin = CheckCreationDate(fake_context)
+
+        results = list(plugin.run())
+
+        self.assertEqual(len(results), 1)
+
+        self.assertIsInstance(results[0], LinterError)
+        self.assertEqual(
+            "The creation_date consists of two different dates.",
+            results[0].message,
+        )
+
+    def test_wrong_length(self):
+        path = Path("some/file.nasl")
+        content = (
+            '  script_tag(name:"creation_date", value:"2013-05-14 11:24:55 '
+            '+0200 (Tue, 14 May 2013 )");\n'
+        )
+        fake_context = MagicMock()
+        fake_context.nasl_file = path
+        fake_context.file_content = content
+        plugin = CheckCreationDate(fake_context)
+
+        results = list(plugin.run())
+
+        self.assertEqual(len(results), 1)
+
+        self.assertIsInstance(results[0], LinterError)
+        self.assertEqual(
+            "False or incorrectly formatted creation_date.",
+            results[0].message,
+        )
+
+    def test_creation_date_greater_than_last_modification(self):
+        path = Path("some/file.nasl")
+        content = (
+            '  script_tag(name:"creation_date", value:"2025-01-01 00:00:01 '
+            '+0200 (Wed, 01 Jan 2025)");\n'
+            '  script_tag(name:"last_modification", value:"2025-01-01 00:00:00 '
+            '+0200 (Wed, 01 Jan 2025)");\n'
+        )
+        fake_context = self.create_file_plugin_context(
+            nasl_file=path, file_content=content
+        )
+        plugin = CheckCreationDate(fake_context)
+
+        results = list(plugin.run())
+
+        self.assertEqual(len(results), 1)
+
+        self.assertIsInstance(results[0], LinterError)
+        self.assertEqual(
+            "The creation_date must not be greater than the last modification date.",
+            results[0].message,
+        )
+
+    def test_creation_date_equal_last_modification(self):
+        path = Path("some/file.nasl")
+        content = (
+            '  script_tag(name:"creation_date", value:"2025-01-01 00:00:00 '
+            '+0200 (Wed, 01 Jan 2025)");\n'
+            '  script_tag(name:"last_modification", value:"2025-01-01 00:00:00 '
+            '+0200 (Wed, 01 Jan 2025)");\n'
+        )
+        fake_context = self.create_file_plugin_context(
+            nasl_file=path, file_content=content
+        )
+        plugin = CheckCreationDate(fake_context)
+
+        results = list(plugin.run())
+
+        self.assertEqual(len(results), 0)
+
+    def test_malformed_hour(self):
+        path = Path("some/file.nasl")
+        content = (
+            '  script_tag(name:"creation_date", value:"2013-05-14 111:24:55 '
+            '+0200 (Tue, 14 May 2013)");\n'
+        )
+        fake_context = MagicMock()
+        fake_context.nasl_file = path
+        fake_context.file_content = content
+        plugin = CheckCreationDate(fake_context)
+
+        results = list(plugin.run())
+
+        self.assertEqual(len(results), 1)
+
+        self.assertIsInstance(results[0], LinterError)
+        self.assertEqual(
+            "False or incorrectly formatted creation_date.",
+            results[0].message,
+        )
+
+    def test_malformed_second(self):
+        path = Path("some/file.nasl")
+        content = (
+            '  script_tag(name:"creation_date", value:"2013-05-14 11:24:55s '
+            '+0200 (Tue, 14 May 2013)");\n'
+        )
+        fake_context = MagicMock()
+        fake_context.nasl_file = path
+        fake_context.file_content = content
+        plugin = CheckCreationDate(fake_context)
+
+        results = list(plugin.run())
+
+        self.assertEqual(len(results), 1)
+
+        self.assertIsInstance(results[0], LinterError)
+        self.assertEqual(
+            "False or incorrectly formatted creation_date.",
+            results[0].message,
+        )
+
+    def test_malformed_day(self):
+        path = Path("some/file.nasl")
+        content = (
+            '  script_tag(name:"creation_date", value:"2013-05-14D 11:24:55 '
+            '+0200 (Tue, 14 May 2013)");\n'
+        )
+        fake_context = MagicMock()
+        fake_context.nasl_file = path
+        fake_context.file_content = content
+        plugin = CheckCreationDate(fake_context)
+
+        results = list(plugin.run())
+
+        self.assertEqual(len(results), 1)
+
+        self.assertIsInstance(results[0], LinterError)
+        self.assertEqual(
+            "False or incorrectly formatted creation_date.",
+            results[0].message,
+        )
---
+++ packages/troubadix/troubadix-25.6.0.tar.gz/troubadix-25.6.0/troubadix/plugins/creation_date.py
@@ -0,0 +1,110 @@
+# Copyright (C) 2022 Greenbone AG
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from datetime import datetime
+from pathlib import Path
+from typing import Iterator
+
+from troubadix.helper.patterns import ScriptTag, get_script_tag_pattern
+from troubadix.plugin import FileContentPlugin, LinterError, LinterResult
+
+LENGTH = 44
+
+
+class CheckCreationDate(FileContentPlugin):
+    name = "creation_date"
+
+    def check_content(
+        self,
+        nasl_file: Path,
+        file_content: str,
+    ) -> Iterator[LinterResult]:
+        if (
+            nasl_file.suffix == ".inc"
+            or "# troubadix: disable=template_nd_test_files_fps" in file_content
+        ):
+            return
+
+        if "creation_date" not in file_content:
+            yield LinterError(
+                "No creation date has been found.",
+                file=nasl_file,
+                plugin=self.name,
+            )
+            return
+
+        tag_pattern = get_script_tag_pattern(ScriptTag.CREATION_DATE)
+
+        # Example: "2017-11-29 13:56:41 +0100 (Wed, 29 Nov 2017)"
+        match = tag_pattern.search(file_content)
+
+        if not match:
+            yield LinterError(
+                "False or incorrectly formatted creation_date.",
+                file=nasl_file,
+                plugin=self.name,
+            )
+            return
+
+        try:
+            date_left = datetime.strptime(
+                match.group("value")[:25], "%Y-%m-%d %H:%M:%S %z"
+            )
+            # 2017-11-29 13:56:41 +0100 (error if no timezone)
+            date_right = datetime.strptime(
+                match.group("value")[27:43], "%a, %d %b %Y"
+            )
+            week_day_parsed = date_right.strftime("%a")
+        except ValueError:
+            yield LinterError(
+                "False or incorrectly formatted creation_date.",
+                file=nasl_file,
+                plugin=self.name,
+            )
+            return
+
+        week_day_str = match.group("value")[27:30]
+        # Wed, 29 Nov 2017
+        if date_left.date() != date_right.date():
+            yield LinterError(
+                "The creation_date consists of two different dates.",
+                file=nasl_file,
+                plugin=self.name,
+            )
+        # Check correct weekday
+        elif week_day_str != week_day_parsed:
+            formatted_date = week_day_parsed
+            yield LinterError(
+                f"Wrong day of week. Please change it from '{week_day_str}"
+                f"' to '{formatted_date}'.",
+                file=nasl_file,
+                plugin=self.name,
+            )
+
+        last_modification_pattern = get_script_tag_pattern(
+            ScriptTag.LAST_MODIFICATION
+        )
+        if match := last_modification_pattern.search(file_content):
+            last_modification = datetime.strptime(
+                match.group("value")[:25], "%Y-%m-%d %H:%M:%S %z"
+            )
+            if date_left > last_modification:
+                yield LinterError(
+                    "The creation_date must not be greater than the last modification date.",
+                    file=nasl_file,
+                    plugin=self.name,
+                )
