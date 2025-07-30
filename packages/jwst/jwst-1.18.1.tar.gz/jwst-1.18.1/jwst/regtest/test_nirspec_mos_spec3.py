b8816c1dd4405df3256b19df2818588ce5c07f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:09:12 +0000

    Add package jwst/1.18.1/jwst-1.18.1.tar.gz

---
+++ packages/jwst/jwst-1.18.1.tar.gz/jwst-1.18.1/jwst/regtest/test_nirspec_mos_spec3.py
@@ -0,0 +1,54 @@
+import pytest
+from astropy.io.fits.diff import FITSDiff
+import numpy as np
+from gwcs import wcstools
+
+from jwst.stpipe import Step
+from stdatamodels.jwst import datamodels
+
+
+@pytest.fixture(scope="module")
+def run_pipeline(rtdata_module):
+    """Run calwebb_spec3 on NIRSpec MOS data."""
+    rtdata = rtdata_module
+    rtdata.get_asn("nirspec/mos/jw01345-o066_20230831t181155_spec3_00002_asn.json")
+
+    # Run the calwebb_spec3 pipeline on the association
+    args = ["calwebb_spec3", rtdata.input]
+    Step.from_cmdline(args)
+
+    return rtdata
+
+
+@pytest.mark.bigdata
+@pytest.mark.parametrize("suffix", ["cal", "crf", "s2d", "x1d"])
+@pytest.mark.parametrize("source_id", ["b000000030", "b000000031",
+                                       "s000004385", "s000007380",
+                                       "v000000048", "v000000049",
+                                       "v000000053", "v000000056"])
+def test_nirspec_mos_spec3(run_pipeline, suffix, source_id, fitsdiff_default_kwargs):
+    """Check results of calwebb_spec3"""
+    rtdata = run_pipeline
+
+    output = f"jw01345-o066_{source_id}_nirspec_f170lp-g235m_{suffix}.fits"
+    rtdata.output = output
+    rtdata.get_truth(f"truth/test_nirspec_mos_spec3/{output}")
+
+    # Adjust tolerance for machine precision with float32 drizzle code
+    if suffix == "s2d":
+        fitsdiff_default_kwargs["rtol"] = 1e-4
+        fitsdiff_default_kwargs["atol"] = 1e-5
+
+    diff = FITSDiff(rtdata.output, rtdata.truth, **fitsdiff_default_kwargs)
+    assert diff.identical, diff.report()
+
+    # Check output wavelength array against its own wcs
+    if suffix == "s2d":
+        tolerance = 1e-03
+        dmr = datamodels.open(rtdata.output)
+
+        w = dmr.meta.wcs
+        x, y = wcstools.grid_from_bounding_box(w.bounding_box, step=(1, 1), center=True)
+        _, _, wave = w(x, y)
+        wlr = dmr.wavelength
+        assert np.all(np.isclose(wave, wlr, atol=tolerance))
---
+++ packages/jwst/jwst-1.18.1.tar.gz/jwst-1.18.1/jwst/scripts/tests/test_scripts.py
@@ -0,0 +1,26 @@
+import shutil
+
+import pytest
+
+SCRIPTS = [
+    'adjust_wcs',
+    'asn_edit',
+    'asn_gather',
+    'asn_make_pool',
+    'collect_pipeline_cfgs',
+    'create_data',
+    'pointing_summary',
+    'schemadoc',
+    'set_telescope_pointing',
+    'set_telescope_pointing.py',
+    'set_velocity_aberration',
+    'set_velocity_aberration.py',
+    'v1_calculate',
+    'verify_install_requires',
+    'world_coords',
+]
+
+
+@pytest.mark.parametrize('script', SCRIPTS)
+def test_script_installed(script):
+    assert shutil.which(script) is not None, f'`{script}` not installed'
