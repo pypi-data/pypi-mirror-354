3e82b186430b52fa2188484aeb9b1e8de37548ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:46:26 +0000

    Add package x-matrixlib/0.1/x_matrixlib-0.1.tar.gz

---
+++ packages/x-matrixlib/x_matrixlib-0.1.tar.gz/x_matrixlib-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="x_matrixlib",
+    version="0.1",
+    packages=find_packages(),
+    description="A simple library to multiply matrices of different dimensions",
+    author="Primes",
+    install_requires=[],
+)
