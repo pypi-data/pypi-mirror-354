df967335772f90514168fe11bfec5d158fa911ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:06 +0000

    Add package formidable/0.5/formidable-0.5-py3-none-any.whl

---
+++ packages/formidable/formidable-0.5-py3-none-any.whl/formidable/__init__.py
@@ -0,0 +1,16 @@
+"""
+Formable
+Copyright (c) 2025 Juan-Pablo Scaletti
+"""
+
+from .fields.boolean import BooleanField  # noqa
+from .fields.date import DateField  # noqa
+from .fields.datetime import DateTimeField  # noqa
+from .fields.formfield import FormField  # noqa
+from .fields.formset import FormSet  # noqa
+from .fields.list import ListField  # noqa
+from .fields.number import FloatField, IntegerField  # noqa
+from .fields.text import TextField  # noqa
+from .fields.time import TimeField  # noqa
+
+from .form import DELETED, Form  # noqa
