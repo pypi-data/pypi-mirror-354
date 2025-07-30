1e94c61978be014a6c819c8fc223f11d457b3354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:57 +0000

    Add package manojkanakam/0.2.0/manojkanakam-0.2.0-py3-none-any.whl

---
+++ packages/manojkanakam/manojkanakam-0.2.0-py3-none-any.whl/manojkanakam/resume.py
@@ -0,0 +1,39 @@
+def resume():
+    print("""
+Manoj Kanakam
++91 7981419024
+Warangal, Telangana (506005)
+manojkanakam9@gmail.com
+LinkedIn
+GitHub
+
+Education:
+Vaagdevi College of Engineering (VCE) - B.Tech in CSE (Data Science), CGPA 8.52
+SR Junior College - MPC (87.6%)
+Pathfinder School - SSC GPA 9.3
+
+Technical Skills:
+- Languages: Python, Java, C, SQL
+- Web: HTML, CSS, JS, MySQL
+- Data: Pandas, Numpy, Matplotlib
+- Tools: GitHub, VS Code, Docker, Power BI
+- Core: DSA, OS, CN, DBMS, ST, NSC
+
+Internship:
+Swecha – Research Intern (LLMs, Viswam.ai, OCR, AI pipelines)
+
+Projects:
+1. Semester Results Analyzer
+2. Optimal Travel Planner
+3. Audible PDF Companion
+
+Certifications:
+- Java (Udemy), Python (Udemy), BluePrism, AWS Cloud, HTML
+
+Achievements:
+- Designing Chairperson – IEEE VCE SB, Data Quest
+- Led 2+ technical events successfully
+
+Declaration:
+I hereby declare that the above information is true to the best of my knowledge.
+""")
