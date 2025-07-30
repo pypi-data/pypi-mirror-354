be63a0f6c5021c644a99f5a7f7ecb1fca12e2e15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:08:23 +0000

    Add package gpsea/0.9.8/gpsea-0.9.8.tar.gz

---
+++ packages/gpsea/gpsea-0.9.8.tar.gz/gpsea-0.9.8/src/gpsea/analysis/pscore/stats/__init__.py
@@ -0,0 +1,7 @@
+from ._stats import PhenotypeScoreStatistic
+from ._stats import MannWhitneyStatistic, TTestStatistic
+
+__all__ = [
+    'PhenotypeScoreStatistic',
+    'MannWhitneyStatistic', 'TTestStatistic',
+]
