e8af29d3b001ab76ebcb2c443a10343a76c45218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:07:02 +0000

    Add package tek-secrets/0.1.6.dev0/tek_secrets-0.1.6.dev0-py3-none-any.whl

---
+++ packages/tek-secrets/tek_secrets-0.1.6.dev0-py3-none-any.whl/tek_secrets/core/config.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv.main import load_dotenv
+from starlette.datastructures import Secret
+
+load_dotenv(".env")
+
+
+# API_URL = os.getenv("API_URL", "Tek Secrets API")
+# CLIENT_ID = Secret(os.getenv("CLIENT_ID"))
+CLIENT_ID = "Ov23liDWHyI85oFOk2b8"
+API_URL = "https://tek-secrets.onrender.com"
+GITHUB_AUTH_URL = "https://github.com/login/oauth/authorize"
+REDIRECT_URI = "http://localhost:8080"
