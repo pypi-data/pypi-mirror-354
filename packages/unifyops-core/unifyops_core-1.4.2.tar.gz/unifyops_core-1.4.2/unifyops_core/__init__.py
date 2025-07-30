5e63a4fe75e1a8e39c4c8e97b41712cec454cb51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:35:48 +0000

    Add package unifyops-core/1.4.2/unifyops_core-1.4.2.tar.gz

---
+++ packages/unifyops-core/unifyops_core-1.4.2.tar.gz/unifyops_core-1.4.2/unifyops_core/__init__.py
@@ -0,0 +1,10 @@
+"""
+UnifyOps Core - Shared utilities for UnifyOps Python projects
+"""
+
+# Import submodules to make them available through the package
+from unifyops_core import utils
+from unifyops_core import logging
+from unifyops_core import exceptions
+
+__version__ = "1.4.2"
