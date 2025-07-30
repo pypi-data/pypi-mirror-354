c70538bb05f87193364e920daf46ad2248589a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:41 +0000

    Add package samaneh/0.1/samaneh-0.1.tar.gz

---
+++ packages/samaneh/samaneh-0.1.tar.gz/samaneh-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+	name='samaneh',
+	version='0.1',
+	packages=find_packages(),
+	description='کتابخانه مساحت',
+         	author='sama',
+	author_email='bagherisama78@gmail.com',
+)
