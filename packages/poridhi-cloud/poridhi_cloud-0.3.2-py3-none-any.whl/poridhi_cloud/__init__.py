c66aac90994f46888e56668df7cba5d4ce5b35a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:58 +0000

    Add package poridhi-cloud/0.3.2/poridhi_cloud-0.3.2-py3-none-any.whl

---
+++ packages/poridhi-cloud/poridhi_cloud-0.3.2-py3-none-any.whl/poridhi_cloud/__init__.py
@@ -0,0 +1,10 @@
+from .client import PoridihCloud, PoridihCloudError
+# from .websocket import WebSocketGenerator
+
+__all__ = [
+    'PoridihCloud',
+    'PoridihCloudError',
+    'WebSocketGenerator'
+]
+
+__version__ = '0.3.2'
