d7addac298a815d48dc658a1a1cb8d3453677a43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:56 +0000

    Add package towbintools/0.3.5/towbintools-0.3.5.tar.gz

---
+++ packages/towbintools/towbintools-0.3.5.tar.gz/towbintools-0.3.5/towbintools/deep_learning/architectures/__init__.py
@@ -0,0 +1,11 @@
+from .archs import Unet
+from .archs import UnetPlusPlus
+from .models import PretrainedSegmentationModel
+from .models import SegmentationModel
+
+__all__ = [
+    "Unet",
+    "UnetPlusPlus",
+    "PretrainedSegmentationModel",
+    "SegmentationModel",
+]
