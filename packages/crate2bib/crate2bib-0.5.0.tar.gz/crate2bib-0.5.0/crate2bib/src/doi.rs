6850f107bc1425d99bb41c855e4bd721f5569fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:06 +0000

    Add package crate2bib/0.5.0/crate2bib-0.5.0.tar.gz

---
+++ packages/crate2bib/crate2bib-0.5.0.tar.gz/crate2bib-0.5.0/crate2bib/src/doi.rs
@@ -0,0 +1,20 @@
+/// Tries to obtain a bibtex entry from a given [DOI](https://www.doi.org/)
+pub async fn get_bibtex_doi(
+    doi: &str,
+    client: reqwest::Client,
+) -> crate::Result<Option<biblatex::Bibliography>> {
+    // let doi = "10.1021/acs.jpcc.0c05161";
+    let rq = format!("https://doi.org/{doi}");
+
+    let res = client
+        .request(reqwest::Method::GET, rq)
+        .header(reqwest::header::ACCEPT, "application/x-bibtex")
+        .send()
+        .await?;
+
+    if let Ok(bib) = res.text().await {
+        Ok(Some(biblatex::Bibliography::parse(&bib)?))
+    } else {
+        Ok(None)
+    }
+}
