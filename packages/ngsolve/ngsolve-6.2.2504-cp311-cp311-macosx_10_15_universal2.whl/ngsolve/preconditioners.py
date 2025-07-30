922c369918d5e1ba95b6e3656f2e968eeee326f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:56 +0000

    Add package ngsolve/6.2.2504/ngsolve-6.2.2504-cp311-cp311-macosx_10_15_universal2.whl

---
+++ packages/ngsolve/ngsolve-6.2.2504-cp311-cp311-macosx_10_15_universal2.whl/ngsolve/preconditioners.py
@@ -0,0 +1,10 @@
+from ngsolve.comp import MultiGridPreconditioner as MultiGrid
+from ngsolve.comp import LocalPreconditioner as Local
+from ngsolve.comp import HCurlAMG
+from ngsolve.comp import BDDCPreconditioner as BDDC
+
+
+
+
+
+
