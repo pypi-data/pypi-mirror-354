ef7efc3e04806b5bad734e6c30e8ce2cd85db7cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:07:21 +0000

    Add package cheetah-accelerator/0.7.3/cheetah_accelerator-0.7.3.tar.gz

---
+++ packages/cheetah-accelerator/cheetah_accelerator-0.7.3.tar.gz/cheetah_accelerator-0.7.3/tests/test_speed.py
@@ -0,0 +1,39 @@
+import time
+
+import pytest
+import torch
+
+import cheetah
+
+from .resources import ARESlatticeStage3v1_9 as ares
+
+# TODO: Test that Cheeath tracks faster than Ocelot
+
+
+@pytest.mark.filterwarnings(
+    "ignore:"
+    "Diagnostic screen was converted with default screen properties.:"
+    "cheetah.utils.PhysicsWarning"
+)
+def test_tracking_speed():
+    """Really only tests that Cheetah isn't super slow."""
+    cell = cheetah.converters.ocelot.subcell_of_ocelot(
+        ares.cell, "AREASOLA1", "AREABSCR1"
+    )
+    segment = cheetah.Segment.from_ocelot(cell)
+    segment.AREABSCR1.is_active = True  # Turn screen on and off
+
+    particles = cheetah.ParticleBeam.from_parameters(
+        num_particles=torch.tensor(int(1e5)),
+        sigma_x=torch.tensor(175e-6),
+        sigma_y=torch.tensor(175e-6),
+    )
+
+    t1 = time.time()
+
+    _ = segment.track(particles)
+    _ = segment.AREABSCR1.reading
+
+    t2 = time.time()
+
+    assert (t2 - t1) < 0.1
