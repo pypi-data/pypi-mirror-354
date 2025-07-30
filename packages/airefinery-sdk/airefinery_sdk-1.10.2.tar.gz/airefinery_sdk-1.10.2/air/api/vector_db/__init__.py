2e7440e02a180cfa54c9b955e84e91cc8f12f4b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:40 +0000

    Add package airefinery-sdk/1.10.2/airefinery_sdk-1.10.2.tar.gz

---
+++ packages/airefinery-sdk/airefinery_sdk-1.10.2.tar.gz/airefinery_sdk-1.10.2/air/api/vector_db/__init__.py
@@ -0,0 +1,9 @@
+"""
+Module for vector databases
+"""
+
+from air.api.vector_db.azure_aisearch import AzureAISearch
+from air.api.vector_db.vectordb_registry import VectorDBRegistry
+from air.api.vector_db.base_vectordb import BaseVectorDB, VectorDBConfig
+
+__all__ = ["VectorDBRegistry", "BaseVectorDB", "VectorDBConfig"]
