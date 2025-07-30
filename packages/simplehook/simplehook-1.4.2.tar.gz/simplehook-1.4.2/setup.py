f31ad4b135d4471f217ae3e69ce26b76588fcef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:11 +0000

    Add package simplehook/1.4.2/simplehook-1.4.2.tar.gz

---
+++ packages/simplehook/simplehook-1.4.2.tar.gz/simplehook-1.4.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import os
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="simplehook",
+    version="1.4.2",
+    description="Simple Discord webhook wrapper",
+    long_description=long_description,  
+    long_description_content_type="text/markdown", 
+    author="jstiin",
+    url="https://github.com/jstiin/simplehook",
+    packages=find_packages(),
+    install_requires=["aiofiles", "httpx"],
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords="discord webhook messaging",
+)
