22aade90c0f10be42fb737270478b13ec39872fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:59 +0000

    Add package sicoob-sdk/0.1.3/sicoob_sdk-0.1.3-py3-none-any.whl

---
+++ packages/sicoob-sdk/sicoob_sdk-0.1.3-py3-none-any.whl/sicoob/__init__.py
@@ -0,0 +1,52 @@
+"""Módulo principal do SDK Sicoob"""
+
+from .client import Sicoob
+from .conta_corrente import ContaCorrenteAPI
+from .pix import PixAPI
+from .boleto import BoletoAPI
+from .exceptions import (
+    SicoobError,
+    BoletoError,
+    BoletoEmissaoError,
+    BoletoConsultaError,
+    BoletoNaoEncontradoError,
+    ContaCorrenteError,
+    PixError,
+    AutenticacaoError,
+    ExtratoError,
+    SaldoError,
+    TransferenciaError,
+    CobrancaPixError,
+    CobrancaPixNaoEncontradaError,
+    CobrancaPixVencimentoError,
+    WebhookPixError,
+    WebhookPixNaoEncontradoError,
+    LoteCobrancaPixError,
+    QrCodePixError
+)
+
+__version__ = "0.1.3"
+__all__ = [
+    "Sicoob",
+    "ContaCorrenteAPI",
+    "PixAPI",
+    "BoletoAPI",
+    "SicoobError",
+    "BoletoError",
+    "BoletoEmissaoError",
+    "BoletoConsultaError",
+    "BoletoNaoEncontradoError",
+    "ContaCorrenteError",
+    "PixError",
+    "AutenticacaoError",
+    "ExtratoError",
+    "SaldoError",
+    "TransferenciaError",
+    "CobrancaPixError",
+    "CobrancaPixNaoEncontradaError",
+    "CobrancaPixVencimentoError",
+    "WebhookPixError",
+    "WebhookPixNaoEncontradoError",
+    "LoteCobrancaPixError",
+    "QrCodePixError"
+]
