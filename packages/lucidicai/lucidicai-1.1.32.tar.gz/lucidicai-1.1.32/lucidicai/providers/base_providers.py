aac29c34b1ee46dd41f2f55600dccedc55359231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:53 +0000

    Add package lucidicai/1.1.32/lucidicai-1.1.32.tar.gz

---
+++ packages/lucidicai/lucidicai-1.1.32.tar.gz/lucidicai-1.1.32/lucidicai/providers/base_providers.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+class BaseProvider(ABC):
+    def __init__(self, client):
+        self.client = client  # TODO: never store reference to client, always get fresh instance from Client()
+        self._provider_name = None
+    
+    @abstractmethod
+    def handle_response(self, response, kwargs, session: Optional = None):
+        """Handle responses from the LLM provider"""
+        pass
+        
+    @abstractmethod
+    def override(self):
+        """Override the provider's API methods"""
+        pass
+        
+    @abstractmethod
+    def undo_override(self):
+        """Restore original API methods"""
+        pass
