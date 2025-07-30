5564a719fd914cd3d3890bb0ffe9f72f943bfcb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:08 +0000

    Add package prefect/3.4.6.dev2/prefect-3.4.6.dev2.tar.gz

---
+++ packages/prefect/prefect-3.4.6.dev2.tar.gz/prefect-3.4.6.dev2/src/prefect/server/ui/assets/usePageTitle-lR93gd3J.js
@@ -0,0 +1,2 @@
+import{i as o,n as a,bw as c}from"./index-lpMZ-Mxi.js";function u(...t){const r=[...t,"Prefect Server"],n=o(()=>r.map(e=>a(e)).filter(e=>e!==null).join(" • "));c(()=>document.title=n.value)}export{u};
+//# sourceMappingURL=usePageTitle-lR93gd3J.js.map
