7b99ac5b6fb2557e506c5ead8208eefaa1954dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:23:35 +0000

    Add package tra-algorithm/1.0.0/tra_algorithm-1.0.0-py3-none-any.whl

---
+++ packages/tra-algorithm/tra_algorithm-1.0.0-py3-none-any.whl/tra_algorithm/version.py
@@ -0,0 +1,10 @@
+"""Version information for TRA Algorithm package."""
+
+__version__ = "1.0.0"
+__version_info__ = tuple(int(i) for i in __version__.split('.'))
+
+# Version history
+VERSION_HISTORY = {
+    "1.0.0": "Initial release with OptimizedTRA algorithm",
+    # Future versions will be added here
+}
