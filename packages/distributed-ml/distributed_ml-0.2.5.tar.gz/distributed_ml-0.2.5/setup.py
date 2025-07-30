3df2f4f8a95f4e6a8f86a73682aab99607cab41f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:42 +0000

    Add package distributed-ml/0.2.5/distributed_ml-0.2.5.tar.gz

---
+++ packages/distributed-ml/distributed_ml-0.2.5.tar.gz/distributed_ml-0.2.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="distributed-ml",
+    version="0.2.5",
+    author="Sumit Raut",
+    author_email="sumitcraut@gmail.com",
+    description="A package for distributed scikit-learn tasks.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sumitraut7/CS230-distributed-machine-learning",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=["requests", "setuptools", "numpy", "pandas", "tqdm", "scikit-learn"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
