f74f95e61edf1722bab10c1f658fccbc7cf87723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:18 +0000

    Add package gai-sdk/1.0.223/gai_sdk-1.0.223.tar.gz

---
+++ packages/gai-sdk/gai_sdk-1.0.223.tar.gz/gai_sdk-1.0.223/chat/test/integrationtest/curl/1_ping.sh
@@ -0,0 +1,1 @@
+curl http://gai-chat-svr:12031
---
+++ packages/gai-sdk/gai_sdk-1.0.223.tar.gz/gai_sdk-1.0.223/messages/src/gai/messages/__init__.py
@@ -0,0 +1,36 @@
+from .async_message_bus import AsyncMessageBus,MessageBusProtocol
+from .typing import (
+    MessageHeaderPydantic,
+    DefaultBodyPydantic,
+    SendBodyPydantic,
+    ReplyBodyPydantic,
+    MessagePydantic,
+)
+from .message_counter import MessageCounter
+from .message_helper import (
+    create_message,
+    convert_to_chat_messages,
+    create_user_send_message,
+    create_assistant_reply_chunk,
+    create_assistant_reply_content,
+    json,
+    unjson
+)
+
+__all__ = [
+    "AsyncMessageBus",
+    "MessageBusProtocol",
+    "MessageHeaderPydantic",
+    "DefaultBodyPydantic",
+    "SendBodyPydantic",
+    "ReplyBodyPydantic",
+    "MessagePydantic",
+    "MessageCounter",
+    "create_message",
+    "convert_to_chat_messages",
+    "create_user_send_message",
+    "create_assistant_reply_chunk",
+    "create_assistant_reply_content",
+    "json",
+    "unjson",
+]
