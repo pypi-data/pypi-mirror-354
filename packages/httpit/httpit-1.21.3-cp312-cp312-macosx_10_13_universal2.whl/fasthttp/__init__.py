642645755745ff9e270bbd4113ec620992608d90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:26 +0000

    Add package httpit/1.21.3/httpit-1.21.3-cp312-cp312-macosx_10_13_universal2.whl

---
+++ packages/httpit/httpit-1.21.3-cp312-cp312-macosx_10_13_universal2.whl/fasthttp/__init__.py
@@ -0,0 +1,15 @@
+"""
+httpit - Ultra-fast lightweight HTTP server
+
+High-performance C-based HTTP server with Python integration.
+Developed and maintained by RODMENA LIMITED (https://rodmena.co.uk)
+Visit https://httpit.rodmena.co.uk for documentation and support.
+"""
+
+from fasthttp.server import HTTPServer, WebfsdError
+
+__version__ = "1.21.3"
+__author__ = "RODMENA LIMITED"
+__email__ = "info@rodmena.co.uk"
+__url__ = "https://github.com/rodmena-limited/httpit"
+__all__ = ["HTTPServer", "WebfsdError"]
