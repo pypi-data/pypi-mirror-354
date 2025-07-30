b43151be6f5d074f8f1e3c886b7659150a91a570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:38 +0000

    Add package RobloxExtra/0.7/robloxextra-0.7.tar.gz

---
+++ packages/RobloxExtra/robloxextra-0.7.tar.gz/robloxextra-0.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RobloxExtra',  # must be unique on PyPI
+    version='0.7',
+    packages=find_packages(),
+    description='Roblox API Function Wrapper',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Carter',
+    author_email='carter@email.com',
+    url='https://github.com/yourname/yourlib',  # optional
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'requests==2.28.1',
+        'mss==7.0.1',
+        'Flask==2.2.2',
+        'pycryptodome'
+    ]
+)
