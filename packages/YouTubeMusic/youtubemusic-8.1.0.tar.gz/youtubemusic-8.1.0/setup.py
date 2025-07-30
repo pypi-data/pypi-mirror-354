6c6e1a79e49fcb0ec26f81594271fa81264f6f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:40 +0000

    Add package YouTubeMusic/8.1.0/youtubemusic-8.1.0.tar.gz

---
+++ packages/YouTubeMusic/youtubemusic-8.1.0.tar.gz/youtubemusic-8.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="YouTubeMusic",
+    version="8.1.0",
+    description="A YouTube Music Search Package With Artist, Channel, and Video Details",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="ABHISHEK THAKUR",
+    author_email="abhishekbanshiwal2005@gmail.com",
+    url="https://github.com/YouTubeMusicAPI/YouTubeMusic",
+    packages=find_packages(),
+    install_requires=[
+        "httpx[http2]",
+    ],
+    entry_points={
+        'console_scripts': [
+            'yt-music=YouTubeMusic.cli:cli',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
