c2aadc81fe702242a7abc4877976529ec438f4f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:22 +0000

    Add package nobinobi-child/0.1.4.30/nobinobi_child-0.1.4.30.tar.gz

---
+++ packages/nobinobi-child/nobinobi_child-0.1.4.30.tar.gz/nobinobi_child-0.1.4.30/tests/test_models.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_nobinobi-child
+------------
+
+Tests for `nobinobi-child` models module.
+"""
+
+from django.test import TestCase
+
+# from nobinobi_child import models
+
+
+class TestNobinobi_child(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
