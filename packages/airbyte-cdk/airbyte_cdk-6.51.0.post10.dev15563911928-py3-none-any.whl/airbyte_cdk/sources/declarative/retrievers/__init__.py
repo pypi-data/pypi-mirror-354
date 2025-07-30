35c22319a9597d5962038ad398cee10ea6c1df56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:49 +0000

    Add package airbyte-cdk/6.51.0.post10.dev15563911928/airbyte_cdk-6.51.0.post10.dev15563911928-py3-none-any.whl

---
+++ packages/airbyte-cdk/airbyte_cdk-6.51.0.post10.dev15563911928-py3-none-any.whl/airbyte_cdk/sources/declarative/retrievers/__init__.py
@@ -0,0 +1,19 @@
+#
+# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
+#
+
+from airbyte_cdk.sources.declarative.retrievers.async_retriever import AsyncRetriever
+from airbyte_cdk.sources.declarative.retrievers.retriever import Retriever
+from airbyte_cdk.sources.declarative.retrievers.simple_retriever import (
+    LazySimpleRetriever,
+    SimpleRetriever,
+    SimpleRetrieverTestReadDecorator,
+)
+
+__all__ = [
+    "Retriever",
+    "SimpleRetriever",
+    "SimpleRetrieverTestReadDecorator",
+    "AsyncRetriever",
+    "LazySimpleRetriever",
+]
