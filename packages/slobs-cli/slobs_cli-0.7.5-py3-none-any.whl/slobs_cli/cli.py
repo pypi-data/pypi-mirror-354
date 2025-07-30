5bc3de5c6ca6fb9412d523f8cfb2bc26c5bfdd72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:49 +0000

    Add package slobs-cli/0.7.5/slobs_cli-0.7.5-py3-none-any.whl

---
+++ packages/slobs-cli/slobs_cli-0.7.5-py3-none-any.whl/slobs_cli/cli.py
@@ -0,0 +1,47 @@
+import anyio
+import asyncclick as click
+from pyslobs import ConnectionConfig, SlobsConnection
+
+
+@click.group()
+@click.option(
+    "-d",
+    "--domain",
+    default="127.0.0.1",
+    show_default=True,
+    show_envvar=True,
+    help="The domain of the SLOBS server.",
+    envvar="SLOBS_DOMAIN",
+)
+@click.option(
+    "-p",
+    "--port",
+    default=59650,
+    show_default=True,
+    show_envvar=True,
+    help="The port of the SLOBS server.",
+    envvar="SLOBS_PORT",
+)
+@click.option(
+    "-t",
+    "--token",
+    help="The token for the SLOBS server.",
+    envvar="SLOBS_TOKEN",
+    show_envvar=True,
+    required=True,
+)
+@click.pass_context
+async def cli(ctx: click.Context, domain: str, port: int, token: str | None):
+    """Command line interface for Streamlabs Desktop."""
+    ctx.ensure_object(dict)
+    config = ConnectionConfig(
+        domain=domain,
+        port=port,
+        token=token,
+    )
+    ctx.obj["connection"] = SlobsConnection(config)
+
+
+def run():
+    """Run the CLI application."""
+    anyio.run(cli.main)
