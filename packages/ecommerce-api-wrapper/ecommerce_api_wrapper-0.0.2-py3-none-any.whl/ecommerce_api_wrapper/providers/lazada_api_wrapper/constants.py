ae5d4f46f20778f9fa63084a9dd6de16413877d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:47 +0000

    Add package ecommerce-api-wrapper/0.0.2/ecommerce_api_wrapper-0.0.2-py3-none-any.whl

---
+++ packages/ecommerce-api-wrapper/ecommerce_api_wrapper-0.0.2-py3-none-any.whl/ecommerce_api_wrapper/providers/lazada_api_wrapper/constants.py
@@ -0,0 +1,17 @@
+class LazadaConstants:
+    TIMEOUT = 3
+    READ_TIMEOUT = 5
+    MAX_RETRY = 10
+    MAX_PAGE = 25
+    SLEEP_TIME = 10
+
+    HEADERS = {}
+
+    API_URLS = {
+        "id": "https://www.lazada.co.id",
+        "vn": "https://www.lazada.vn",
+        "ph": "https://www.lazada.com.ph",
+        "sg": "https://www.lazada.sg",
+        "my": "https://www.lazada.com.my",
+        "default": "https://www.lazada.co.",
+    }
