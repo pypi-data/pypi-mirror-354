697bdb42c1a63d9177fbacf08ef99bf832d38be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:13 +0000

    Add package audmath/1.4.2/audmath-1.4.2.tar.gz

---
+++ packages/audmath/audmath-1.4.2.tar.gz/audmath-1.4.2/audmath/core/utils.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from collections.abc import Sequence
+
+
+def polyval(
+    x: float,
+    coefs: Sequence,
+) -> float:
+    r"""Evaluation of polynomial.
+
+    Args:
+        x: input value
+        coefs: polynomial coefficients
+
+    Returns:
+        evaluated polynomial
+
+    """
+    answer = 0
+    power = len(coefs) - 1
+    for coef in coefs:
+        try:
+            answer += coef * x**power
+        except OverflowError:  # pragma: nocover
+            pass
+        power -= 1
+    return answer
