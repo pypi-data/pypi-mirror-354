0571e20058abe261d385ffb2d2067521ac5e34d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:20 +0000

    Add package py-ballisticcalc.exts/2.1.1b3/py_ballisticcalc_exts-2.1.1b3-cp311-cp311-win32.whl

---
+++ packages/py-ballisticcalc.exts/py_ballisticcalc_exts-2.1.1b3-cp311-cp311-win32.whl/py_ballisticcalc_exts/__init__.py
@@ -0,0 +1,17 @@
+__author__ = "o-murphy"
+__copyright__ = (
+    "Copyright 2023 Dmytro Yaroshenko (https://github.com/o-murphy)",
+    "Copyright 2024 David Bookstaber (https://github.com/dbookstaber)"
+)
+
+__credits__ = ["o-murphy", "dbookstaber"]
+
+from .trajectory_calc import TrajectoryCalc
+from .vector import Vector
+from .trajectory_data import TrajectoryData
+
+__all__ = (
+    'TrajectoryCalc',
+    'Vector',
+    'TrajectoryData',
+)
