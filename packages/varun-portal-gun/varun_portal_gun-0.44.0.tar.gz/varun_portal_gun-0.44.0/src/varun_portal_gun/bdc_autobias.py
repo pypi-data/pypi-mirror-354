e9a32328db12a3c1396f80410b1759a3a39a9058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:46:18 +0000

    Add package varun-portal-gun/0.44.0/varun_portal_gun-0.44.0.tar.gz

---
+++ packages/varun-portal-gun/varun_portal_gun-0.44.0.tar.gz/varun_portal_gun-0.44.0/src/varun_portal_gun/bdc_autobias.py
@@ -0,0 +1,69 @@
+import joblib
+import pandas as pd
+
+from sklearn.preprocessing import OrdinalEncoder
+import random
+
+
+def bdc_autobias():
+    columns=['pol', 'dac_name', 'TEMP_BDC_COMBINED_SCALED'
+         ,'CURRENT_BDC_LNA_COMB_RAW', 'CURRENT_BDC_RFA_COMB_RAW', 'CURRENT_BDC_IFA_1_COMB_RAW', 'CURRENT_BDC_IFA_2_COMB_RAW', 'CURRENT_BDC_IFA_3_COMB_RAW']
+    
+    df = pd.DataFrame(columns=columns)
+
+    #fill input df 1 for room temp correction
+    # i know the 5 currents and dac_name, i need pol and temp_bdc from user files
+    # essentially check autobias_fw_bdc_*_25 file.
+    CURRENT_BDC_LNA_COMB_RAW = 1035.0
+    CURRENT_BDC_RFA_COMB_RAW = 1035.0
+    CURRENT_BDC_IFA_1_COMB_RAW = 414.0
+    CURRENT_BDC_IFA_2_COMB_RAW = 1345.0
+    CURRENT_BDC_IFA_3_COMB_RAW = 1345.0
+
+    print("Here 1")
+    for i in range(5):
+        #### HERE
+        pol = 'lhcp'
+
+        dac_name = 0.0
+        match i:
+            case 0:
+                dac_name = 0.0
+            case 1:
+                dac_name = 1.0
+            case 2:
+                dac_name = 2.0
+            case 3:
+                dac_name = 3.0
+            case 4:
+                dac_name = 4.0
+            case _:
+                break
+        
+        #### HERE
+        TEMP_BDC_COMBINED_SCALED = 25
+
+        df.loc[i] = [pol, dac_name, TEMP_BDC_COMBINED_SCALED, CURRENT_BDC_LNA_COMB_RAW, CURRENT_BDC_RFA_COMB_RAW, CURRENT_BDC_IFA_1_COMB_RAW, CURRENT_BDC_IFA_2_COMB_RAW, CURRENT_BDC_IFA_3_COMB_RAW]
+
+    print("Here 2")
+    # encoder = OrdinalEncoder(categories=[['lhcp', 'rhcp']])
+    # df['pol'] = encoder.fit_transform(df[['pol']])
+
+    # loaded_model = joblib.load("/Users/vyelluru/Desktop/bdc_autobias_v5.sav")
+    # result = loaded_model.predict(df)
+    
+    # arr = []
+    # arr2 = []
+    # for i in range(5):
+    #     arr.append(result[i][0])
+    #     arr2.append(result[i][1])
+
+
+    # df['sweep_dac_value'] = arr
+    # df['TMP_BDC_LNA_COMBINED_RAW'] = arr2
+
+    # print(df)
+
+
+    #fill input df 2 for normal calibration
+    # i know 5 currents, dac_name, temp_bdc. i just need pol from user files.
