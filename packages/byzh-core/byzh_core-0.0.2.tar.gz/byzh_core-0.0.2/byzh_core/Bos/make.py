870071c4fadaab0107a54278511ee21db76987ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:06:36 +0000

    Add package byzh-core/0.0.2/byzh_core-0.0.2.tar.gz

---
+++ packages/byzh-core/byzh_core-0.0.2.tar.gz/byzh_core-0.0.2/byzh_core/Bos/make.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import os
+import shutil
+
+def is_dir(path):
+    path = Path(path)
+    name = path.name
+    if '.' in name:
+        return False
+    return True
+
+def is_file(path):
+    path = Path(path)
+    name = path.name
+    if '.' in name:
+        return True
+    return False
+def b_makedir(path):
+    path = Path(path)
+
+    if is_dir(path):
+        os.makedirs(path, exist_ok=True)
+    if is_file(path):
+        os.makedirs(path.parent, exist_ok=True)
+
+
---
+++ packages/byzh-core/byzh_core-0.0.2.tar.gz/byzh_core-0.0.2/byzh_core/Btqdm/__init__.py
@@ -0,0 +1,4 @@
+
+from .b_tqdm import B_Tqdm
+
+__all__ = ['B_Tqdm']
