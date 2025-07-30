31df9d11fe6e5e5f27a6965fb59e1122e03f31a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:53 +0000

    Add package ephor-cli/0.7.0/ephor_cli-0.7.0-py3-none-any.whl

---
+++ packages/ephor-cli/ephor_cli-0.7.0-py3-none-any.whl/ephor_cli/services/__init__.py
@@ -0,0 +1,12 @@
+from .agent import AgentService
+from .conversation import ConversationService
+from .event import EventService
+from .message import MessageService
+from .task import TaskService
+
+
+agent_service = AgentService()
+conversation_service = ConversationService()
+event_service = EventService()
+message_service = MessageService()
+task_service = TaskService()
