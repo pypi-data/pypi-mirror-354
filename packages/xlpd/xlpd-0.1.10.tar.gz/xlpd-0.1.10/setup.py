861ff6f69e49478914320af801e50a159dcc32d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:41 +0000

    Add package xlpd/0.1.10/xlpd-0.1.10.tar.gz

---
+++ packages/xlpd/xlpd-0.1.10.tar.gz/xlpd-0.1.10/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xlpd",
+    version="0.1.10",
+    author="Yinchu Xia",
+    author_email="sales@pytorchsoft.com",
+    description="Xiu Lian Pin De",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/pytorchsoft/xlpd",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=[],
+    classifiers=[],
+    python_requires=">=3.7",
+    entry_points={
+        "console_scripts": [
+            "xlpd = xlpd.cli:app",
+        ]
+    },
+)
