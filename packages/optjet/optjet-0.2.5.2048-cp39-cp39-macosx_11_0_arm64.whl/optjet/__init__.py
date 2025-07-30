a0a5c093c5d631d1b3d54496c5835800119a1a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:39 +0000

    Add package optjet/0.2.5.2048/optjet-0.2.5.2048-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/optjet/optjet-0.2.5.2048-cp39-cp39-macosx_11_0_arm64.whl/optjet/__init__.py
@@ -0,0 +1,8 @@
+# optjet
+import optjet.quant_engine as quant_engine
+import optjet.md_encoder as md_encoder
+
+
+__all__ = ["quant_engine", "md_encoder"]
+#
+__version__ = quant_engine.get_qs_version()
