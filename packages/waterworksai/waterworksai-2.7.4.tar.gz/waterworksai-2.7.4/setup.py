fa57dafda2e623fcc7f813bc50074868d75c77da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:01 +0000

    Add package waterworksai/2.7.4/waterworksai-2.7.4.tar.gz

---
+++ packages/waterworksai/waterworksai-2.7.4.tar.gz/waterworksai-2.7.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='waterworksai',
+    version='2.7.4',
+    author='D. Rehn',
+    description='The official waterworks.ai python API package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),  # Automatically find the packages in your project,
+    include_package_data=True,
+    package_data={"": ["assets/*.js"]},
+    install_requires=['dash','plotly','pandas','requests','waitress','dash-bootstrap-components','scikit-learn','dash-leaflet','dash-extensions','pysheds', 'geopandas', 'rasterio', 'shapely', 'osmnx'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',  # Specify your required Python version
+)
+
