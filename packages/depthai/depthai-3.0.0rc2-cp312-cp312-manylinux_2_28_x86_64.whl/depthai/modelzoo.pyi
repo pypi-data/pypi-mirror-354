1fe9b6ac69264fa68027d0ae7c8e10f9643f16ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:14:30 +0000

    Add package depthai/3.0.0rc2/depthai-3.0.0rc2-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/depthai/depthai-3.0.0rc2-cp312-cp312-manylinux_2_28_x86_64.whl/depthai/modelzoo.pyi
@@ -0,0 +1,48 @@
+def getDefaultCachePath() -> str:
+    """getDefaultCachePath() -> str
+
+    Get the default cache path (where models are cached)
+    """
+def getDefaultModelsPath() -> str:
+    """getDefaultModelsPath() -> str
+
+    Get the default models path (where yaml files are stored)
+    """
+def getDownloadEndpoint() -> str:
+    """getDownloadEndpoint() -> str
+
+    Get the download endpoint (for model querying)
+    """
+def getHealthEndpoint() -> str:
+    """getHealthEndpoint() -> str
+
+    Get the health endpoint (for internet check)
+    """
+def setDefaultCachePath(path: str) -> None:
+    """setDefaultCachePath(path: str) -> None
+
+    Set the default cache path (where models are cached)
+
+    Parameter ``path``:
+    """
+def setDefaultModelsPath(path: str) -> None:
+    """setDefaultModelsPath(path: str) -> None
+
+    Set the default models path (where yaml files are stored)
+
+    Parameter ``path``:
+    """
+def setDownloadEndpoint(endpoint: str) -> None:
+    """setDownloadEndpoint(endpoint: str) -> None
+
+    Set the download endpoint (for model querying)
+
+    Parameter ``endpoint``:
+    """
+def setHealthEndpoint(endpoint: str) -> None:
+    """setHealthEndpoint(endpoint: str) -> None
+
+    Set the health endpoint (for internet check)
+
+    Parameter ``endpoint``:
+    """
