85481cb8a0c3b7ce0eb077d97e34aeb1b76bbb06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:59 +0000

    Add package zynk-lite/0.1.10/zynk_lite-0.1.10-py3-none-any.whl

---
+++ packages/zynk-lite/zynk_lite-0.1.10-py3-none-any.whl/zynk_lite/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2025-present Guille <guilleleiratemes@gmail.com>
+#
+# SPDX-License-Identifier: GPLv3
+
+from . import interpreter
+from . import compiler
+from . import errors
+from . import tokens
+from . import ast
+from . import bytecode
+from . import frontend
+from . import native_funcs
