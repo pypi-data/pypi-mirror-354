549d283070db7650c127c42dc21783528178e615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:06 +0000

    Add package vaapi/0.6.20/vaapi-0.6.20-py3-none-any.whl

---
+++ packages/vaapi/vaapi-0.6.20-py3-none-any.whl/vaapi/__init__.py
@@ -0,0 +1,4 @@
+"""This package provides a client class that serves as wrapper for operations that
+you can perform with the API of the visual analytics tool by Berlin United for the Robocup SPL"""
+
+__version__ = "0.6.20"
