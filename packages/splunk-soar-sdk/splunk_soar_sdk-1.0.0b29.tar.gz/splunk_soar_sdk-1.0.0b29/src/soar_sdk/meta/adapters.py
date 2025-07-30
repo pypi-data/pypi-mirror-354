067e64397d88e2557894dabfec7a8d214d075bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:46:10 +0000

    Add package splunk-soar-sdk/1.0.0b29/splunk_soar_sdk-1.0.0b29.tar.gz

---
+++ packages/splunk-soar-sdk/splunk_soar_sdk-1.0.0b29.tar.gz/splunk_soar_sdk-1.0.0b29/src/soar_sdk/meta/adapters.py
@@ -0,0 +1,29 @@
+import toml
+
+from .app import AppMeta
+
+
+class TOMLDataAdapter:
+    @staticmethod
+    def load_data(filepath: str) -> AppMeta:
+        with open(filepath) as f:
+            toml_data = toml.load(f)
+
+        uv_app_data = toml_data.get("project", {})
+        project_name = uv_app_data.get("name")
+        package_name = (
+            f"phantom_{project_name}"
+            if project_name and not project_name.startswith("phantom_")
+            else project_name
+        )
+
+        return AppMeta(
+            **dict(
+                description=uv_app_data.get("description"),
+                app_version=uv_app_data.get("version"),
+                license=uv_app_data.get("license"),
+                package_name=package_name,
+                project_name=project_name,
+                main_module=uv_app_data.get("main_module"),
+            )
+        )
---
+++ packages/splunk-soar-sdk/splunk_soar_sdk-1.0.0b29.tar.gz/splunk_soar_sdk-1.0.0b29/tests/example_app/src/__init__.py
@@ -0,0 +1,6 @@
+# TODO: This boilerplate is needed to support webhooks prior to Borg release.
+# It should be removed once Borg is n-2.
+
+from . import app
+
+__ALL__ = [app]
