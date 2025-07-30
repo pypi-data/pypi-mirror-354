f927e7be4f868c3485fd0b8d15330ce11809677c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:17 +0000

    Add package genoa/0.2.6/genoa-0.2.6.tar.gz

---
+++ packages/genoa/genoa-0.2.6.tar.gz/genoa-0.2.6/genoa/__init__.py
@@ -0,0 +1,5 @@
+from .Genoa import Genoa
+
+if __name__ == "__main__" or __name__.startswith("genoa"):
+    print('Thanks for using GENOA! Made by MrPsyghost.')
+    print('Visit https://mrpsyghost.netlify.app/genoa for more information.')
