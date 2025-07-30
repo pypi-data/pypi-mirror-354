05a596b99d0f746f219a3e7ffeb7a09e25674b3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:45 +0000

    Add package shc-utilities/0.0.3/shc_utilities-0.0.3-py3-none-any.whl

---
+++ packages/shc-utilities/shc_utilities-0.0.3-py3-none-any.whl/shc_utilities/__init__.py
@@ -0,0 +1,5 @@
+# config_utils/__init__.py
+
+__version__ = "0.1.1"  # Define the version here
+
+from .config_utils import config_log_loader  # Expose config_log_loader
---
+++ packages/shc-utilities/shc_utilities-0.0.3-py3-none-any.whl/shc_utilities/config_utils/__init__.py
@@ -0,0 +1,5 @@
+# config_utils/__init__.py
+
+__version__ = "0.1.1"  # Define the version here
+
+from .config_log_loader import config, logger
