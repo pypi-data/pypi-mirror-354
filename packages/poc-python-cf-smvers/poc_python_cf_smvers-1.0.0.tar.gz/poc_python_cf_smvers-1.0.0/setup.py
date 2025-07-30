0237e6e6380c80f2cc051ea58d90ea6f5da108bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:49 +0000

    Add package poc-python-cf-smvers/1.0.0/poc_python_cf_smvers-1.0.0.tar.gz

---
+++ packages/poc-python-cf-smvers/poc_python_cf_smvers-1.0.0.tar.gz/poc_python_cf_smvers-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name="poc-python-cf-smvers",
+  version="1.0.0",
+  description="A simple Python Cloud Function with versioning",
+  author="MitVo",
+  author_email=" ",
+  packages=["src"]
+)
