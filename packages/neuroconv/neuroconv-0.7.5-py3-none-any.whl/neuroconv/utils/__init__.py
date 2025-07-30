6f6e576d594f23d2a44c54c37c1ab37ccbfaf37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:23 +0000

    Add package neuroconv/0.7.5/neuroconv-0.7.5-py3-none-any.whl

---
+++ packages/neuroconv/neuroconv-0.7.5-py3-none-any.whl/neuroconv/utils/__init__.py
@@ -0,0 +1,63 @@
+from .checks import calculate_regular_series_rate
+from .dict import (
+    DeepDict,
+    append_replace_dict_in_list,
+    dict_deep_update,
+    exist_dict_in_list,
+    load_dict_from_file,
+)
+from .json_schema import (
+    NWBMetaDataEncoder,
+    fill_defaults,
+    get_base_schema,
+    get_metadata_schema_for_icephys,
+    get_schema_from_hdmf_class,
+    get_json_schema_from_method_signature,
+    unroot_schema,
+    get_json_schema_from_method_signature,
+)
+from .types import (
+    ArrayType,
+    IntType,
+    OptionalArrayType,
+)
+
+
+# TODO: remove after 3/1/2025
+def __getattr__(name):
+    from warnings import warn
+
+    from pydantic import FilePath, DirectoryPath
+
+    if name == "FilePath":
+        message = (
+            "The 'FilePath' type has been deprecated and will be removed after 3/1/2025. "
+            "Please use `pydantic.FilePath` instead."
+        )
+        warn(message=message, category=DeprecationWarning, stacklevel=2)
+
+        return FilePath
+    if name == "OptionalFilePath":
+        message = (
+            "The 'OptionalFilePath' type has been deprecated and will be removed after 3/1/2025. "
+            "Please use `pydantic.FilePath | None` instead."
+        )
+        warn(message=message, category=DeprecationWarning, stacklevel=2)
+
+        return FilePath | None
+    if name == "FolderPathType":
+        message = (
+            "The 'FolderPathType' type has been deprecated and will be removed after 3/1/2025. "
+            "Please use `pydantic.DirectoryPath` instead."
+        )
+        warn(message=message, category=DeprecationWarning, stacklevel=2)
+
+        return DirectoryPath
+    if name == "OptionalFolderPathType":
+        message = (
+            "The 'OptionalFolderPathType' type has been deprecated and will be removed after 3/1/2025. "
+            "Please use `pydantic.DirectoryPath | None` instead."
+        )
+        warn(message=message, category=DeprecationWarning, stacklevel=2)
+
+        return DirectoryPath | None
