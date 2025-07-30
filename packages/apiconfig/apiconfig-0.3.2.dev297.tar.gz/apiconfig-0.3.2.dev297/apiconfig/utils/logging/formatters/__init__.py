3bd46e53efc9c2e78ae5f82bef029867541aeaf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:37 +0000

    Add package apiconfig/0.3.2.dev297/apiconfig-0.3.2.dev297.tar.gz

---
+++ packages/apiconfig/apiconfig-0.3.2.dev297.tar.gz/apiconfig-0.3.2.dev297/apiconfig/utils/logging/formatters/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""Custom logging formatters."""
+
+from apiconfig.utils.redaction.body import redact_body
+from apiconfig.utils.redaction.headers import (
+    DEFAULT_SENSITIVE_HEADER_PREFIXES,
+    DEFAULT_SENSITIVE_HEADERS,
+    REDACTED_VALUE,
+    redact_headers,
+)
+
+from .detailed import DetailedFormatter
+from .redacting import (
+    RedactingFormatter,
+    redact_message_helper,
+    redact_structured_helper,
+)
+
+__all__: list[str] = [
+    "DetailedFormatter",
+    "RedactingFormatter",
+    "redact_body",
+    "redact_headers",
+    "REDACTED_VALUE",
+    "DEFAULT_SENSITIVE_HEADERS",
+    "DEFAULT_SENSITIVE_HEADER_PREFIXES",
+    "redact_message_helper",
+    "redact_structured_helper",
+]
