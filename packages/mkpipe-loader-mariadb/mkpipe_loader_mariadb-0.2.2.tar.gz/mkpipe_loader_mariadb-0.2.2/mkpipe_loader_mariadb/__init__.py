ed2eaf19286f38ea8765eef3726c594d105051c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:25 +0000

    Add package mkpipe-loader-mariadb/0.2.2/mkpipe_loader_mariadb-0.2.2.tar.gz

---
+++ packages/mkpipe-loader-mariadb/mkpipe_loader_mariadb-0.2.2.tar.gz/mkpipe_loader_mariadb-0.2.2/mkpipe_loader_mariadb/__init__.py
@@ -0,0 +1,14 @@
+from mkpipe.functions_spark import BaseLoader
+
+
+class MariadbLoader(BaseLoader):
+    def __init__(self, config, settings):
+        super().__init__(
+            config,
+            settings,
+            driver_name='mariadb',
+            driver_jdbc='org.mariadb.jdbc.Driver',
+        )
+
+    def build_jdbc_url(self):
+        return f'jdbc:{self.driver_name}://{self.host}:{self.port}/{self.database}?user={self.username}&password={self.password}&sessionVariables=sql_mode=ANSI_QUOTES'
