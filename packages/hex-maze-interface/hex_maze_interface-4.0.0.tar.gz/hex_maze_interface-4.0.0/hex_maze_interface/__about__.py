74bc90c301ea672a39978e85720db9ad8adc5f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:59 +0000

    Add package hex-maze-interface/4.0.0/hex_maze_interface-4.0.0.tar.gz

---
+++ packages/hex-maze-interface/hex_maze_interface-4.0.0.tar.gz/hex_maze_interface-4.0.0/hex_maze_interface/__about__.py
@@ -0,0 +1,11 @@
+"""Python interface to the Voigts lab hex maze."""
+# This file is generated automatically from metadata
+# File edits may be overwritten!
+
+__version__ = '4.0.0'
+__description__ = 'Python interface to the Voigts lab hex maze.'
+__license__ = 'BSD-3-Clause'
+__url__ = 'https://github.com/janelia-python/hex_maze_interface_python'
+__author__ = 'Peter Polidoro'
+__email__ = 'peter@polidoro.io'
+__copyright__ = '2025 Howard Hughes Medical Institute'
---
+++ packages/hex-maze-interface/hex_maze_interface-4.0.0.tar.gz/hex_maze_interface-4.0.0/hex_maze_interface/__init__.py
@@ -0,0 +1,13 @@
+"""Python interface to the Voigts lab hex maze."""
+# This file is generated automatically from metadata
+# File edits may be overwritten!
+from hex_maze_interface.__about__ import (
+    __author__,
+    __copyright__,
+    __email__,
+    __license__,
+    __description__,
+    __url__,
+    __version__)
+
+from .hex_maze_interface import HexMazeInterface, MazeException, HomeParameters, ControllerParameters
