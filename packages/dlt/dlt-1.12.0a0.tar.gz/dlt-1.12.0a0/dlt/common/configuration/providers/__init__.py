65873b431c130f14eeb4a9ec68d901e9bdabb82c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:27 +0000

    Add package dlt/1.12.0a0/dlt-1.12.0a0.tar.gz

---
+++ packages/dlt/dlt-1.12.0a0.tar.gz/dlt-1.12.0a0/dlt/common/configuration/providers/__init__.py
@@ -0,0 +1,32 @@
+from .provider import ConfigProvider
+from .environ import EnvironProvider
+from .dictionary import DictionaryProvider
+from .toml import (
+    SecretsTomlProvider,
+    ConfigTomlProvider,
+    SettingsTomlProvider,
+    CONFIG_TOML,
+    SECRETS_TOML,
+    StringTomlProvider,
+)
+from .doc import CustomLoaderDocProvider
+from .vault import SECRETS_TOML_KEY, VaultDocProvider
+from .google_secrets import GoogleSecretsProvider
+from .context import ContextProvider
+
+__all__ = [
+    "ConfigProvider",
+    "EnvironProvider",
+    "DictionaryProvider",
+    "SecretsTomlProvider",
+    "ConfigTomlProvider",
+    "SettingsTomlProvider",
+    "CONFIG_TOML",
+    "SECRETS_TOML",
+    "StringTomlProvider",
+    "SECRETS_TOML_KEY",
+    "ContextProvider",
+    "CustomLoaderDocProvider",
+    "VaultDocProvider",
+    "GoogleSecretsProvider",
+]
