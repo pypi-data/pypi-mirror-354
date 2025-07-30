c63ef5352e052f0e0580db2b330f30e3d6aa7853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:37 +0000

    Add package descarga-datos/0.4.2/descarga_datos-0.4.2.tar.gz

---
+++ packages/descarga-datos/descarga_datos-0.4.2.tar.gz/descarga_datos-0.4.2/descarga_datos/cli.py
@@ -0,0 +1,17 @@
+from .internals import Analysis, read_json
+from .network import download_file_from_repo
+import sys
+
+
+def descarga_archivo(file_name, destination_folder, path):
+    lista_analisis = read_json("analyses.json")
+    for diccionario_analisis in lista_analisis:
+        analisis = Analysis(**diccionario_analisis)
+        if analisis.is_dependent_on_datafile(path, file_name):
+            download_file_from_repo(
+                analisis.get_url_to_datafile(path, file_name), destination_folder
+            )
+
+
+def cli():
+    descarga_archivo(*sys.argv[1:])
