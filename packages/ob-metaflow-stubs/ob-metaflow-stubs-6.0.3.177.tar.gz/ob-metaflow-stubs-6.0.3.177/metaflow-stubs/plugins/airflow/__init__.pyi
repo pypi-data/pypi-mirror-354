17d3b38f9c4729380c5462cd95cb1b82e4ba1d2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:46 +0000

    Add package ob-metaflow-stubs/6.0.3.177/ob-metaflow-stubs-6.0.3.177.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.177.tar.gz/ob-metaflow-stubs-6.0.3.177/metaflow-stubs/plugins/airflow/__init__.pyi
@@ -0,0 +1,13 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.14.1+obcheckpoint(0.2.1);ob(v1)                                                   #
+# Generated on 2025-06-10T19:56:51.784231                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import exception as exception
+from . import airflow_utils as airflow_utils
+from . import sensors as sensors
+
