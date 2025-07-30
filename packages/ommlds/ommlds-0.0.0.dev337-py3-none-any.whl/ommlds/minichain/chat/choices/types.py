f9a43f19ba4d16251ff2a6fbc1bcaca19f0bc866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:12 +0000

    Add package ommlds/0.0.0.dev337/ommlds-0.0.0.dev337-py3-none-any.whl

---
+++ packages/ommlds/ommlds-0.0.0.dev337-py3-none-any.whl/ommlds/minichain/chat/choices/types.py
@@ -0,0 +1,21 @@
+"""
+TODO:
+ - ChatResponseOutputs really go on each AiChoice...
+"""
+import typing as ta
+
+from omlish import dataclasses as dc
+from omlish import lang
+
+from ..messages import AiMessage
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+class AiChoice(lang.Final):
+    m: AiMessage
+
+
+AiChoices: ta.TypeAlias = ta.Sequence[AiChoice]
