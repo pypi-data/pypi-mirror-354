60ae255637be2a1fb5e13bfd1a1763c9be1f646e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:08:21 +0000

    Add package paddle-python-sdk/1.6.1/paddle_python_sdk-1.6.1-py3-none-any.whl

---
+++ packages/paddle-python-sdk/paddle_python_sdk-1.6.1-py3-none-any.whl/paddle_billing/Resources/Discounts/Operations/CreateDiscount.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+
+from paddle_billing.Operation import Operation
+from paddle_billing.Undefined import Undefined
+from paddle_billing.Entities.DateTime import DateTime
+from paddle_billing.Entities.Discounts import DiscountType
+from paddle_billing.Entities.Shared import CurrencyCode, CustomData
+
+
+@dataclass
+class CreateDiscount(Operation):
+    amount: str
+    description: str
+    type: DiscountType
+    enabled_for_checkout: bool
+    recur: bool
+    currency_code: CurrencyCode
+    code: str | None | Undefined = Undefined()
+    maximum_recurring_intervals: int | None | Undefined = Undefined()
+    usage_limit: int | None | Undefined = Undefined()
+    restrict_to: list[str] | None | Undefined = Undefined()
+    expires_at: DateTime | None | Undefined = Undefined()
+    custom_data: CustomData | None | Undefined = Undefined()
