58ef04711d1db5d384c0b75f4143ebb6292f7a85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:05 +0000

    Add package gguf-py/0.0.4/gguf_py-0.0.4.tar.gz

---
+++ packages/gguf-py/gguf_py-0.0.4.tar.gz/gguf_py-0.0.4/src/gguf_py/__init__.py
@@ -0,0 +1,19 @@
+# !/usr/bin/env python3
+
+__version__ = '0.0.4'
+
+def __init__():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
+    subparsers = parser.add_subparsers(title="subcommands", dest="subcommand", help="choose a subcommand:")
+    subparsers.add_parser('a', help='[a] assembler')
+    subparsers.add_parser('c', help='[c] checker')
+    subparsers.add_parser('d', help='[d] decomposer')
+    args = parser.parse_args()
+    if args.subcommand == 'a':
+        from gguf_connector import f
+    if args.subcommand == 'c':
+        from gguf_connector import r2
+    if args.subcommand == 'd':
+        from pig_gguf import d
