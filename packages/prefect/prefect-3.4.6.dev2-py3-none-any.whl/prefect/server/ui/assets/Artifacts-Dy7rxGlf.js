4cd4cb94879833ea64b53ed71a60a4e514e44aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:07 +0000

    Add package prefect/3.4.6.dev2/prefect-3.4.6.dev2-py3-none-any.whl

---
+++ packages/prefect/prefect-3.4.6.dev2-py3-none-any.whl/prefect/server/ui/assets/Artifacts-Dy7rxGlf.js
@@ -0,0 +1,2 @@
+import{d as o,j as r,c,o as n,k as a,l as e,n as t,aP as _,aQ as f}from"./index-lpMZ-Mxi.js";import{u as l}from"./usePageTitle-lR93gd3J.js";const C=o({__name:"Artifacts",setup(p){return l("Artifacts"),(u,i)=>{const s=r("p-layout-default");return n(),c(s,{class:"artifacts"},{header:a(()=>[e(t(f))]),default:a(()=>[e(t(_))]),_:1})}}});export{C as default};
+//# sourceMappingURL=Artifacts-Dy7rxGlf.js.map
