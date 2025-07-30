2fedf6d777c1eb4d3935b3182fc3e4c769bbe8f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:46 +0000

    Add package odoo-addon-energetica-website-sale-order/16.0.2.0.0.10/odoo_addon_energetica_website_sale_order-16.0.2.0.0.10-py3-none-any.whl

---
+++ packages/odoo-addon-energetica-website-sale-order/odoo_addon_energetica_website_sale_order-16.0.2.0.0.10-py3-none-any.whl/odoo/addons/energetica_website_sale_order/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Contract generation",
+    "version": "16.0.2.0.0",
+    "depends": [
+        "base_rest",
+        "portal",
+        "sale",
+        "web",
+        "website",
+    ],
+    "author": "Coopdevs Treball SCCL",
+    "category": "Sale Order",
+    "website": "https://coopdevs.org",
+    "license": "AGPL-3",
+    "summary": """
+        Contract generation from website to sale order.
+    """,
+    "data": [
+        "views/lead_template.xml",
+        "views/sale_order.xml",
+    ],
+    "installable": True,
+}
