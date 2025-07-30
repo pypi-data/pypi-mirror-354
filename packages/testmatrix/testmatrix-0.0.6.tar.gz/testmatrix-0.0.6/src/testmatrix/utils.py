ad48235688ad1537f6030c30adeaef1ac38a6268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:35:04 +0000

    Add package testmatrix/0.0.6/testmatrix-0.0.6.tar.gz

---
+++ packages/testmatrix/testmatrix-0.0.6.tar.gz/testmatrix-0.0.6/src/testmatrix/utils.py
@@ -0,0 +1,6 @@
+# Maubot server sanity checker
+# ©Sebastian Spaeth & contributors
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+# User Agent and possibly other required headers that we need for all requests
+req_headers = {"User-Agent": "Mozilla/5.0 (Linux; x64)"}
