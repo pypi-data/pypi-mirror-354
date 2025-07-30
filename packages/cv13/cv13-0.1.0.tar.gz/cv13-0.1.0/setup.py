d6f6e00ac907df2e3df450059366d0081f76e872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:39 +0000

    Add package cv13/0.1.0/cv13-0.1.0.tar.gz

---
+++ packages/cv13/cv13-0.1.0.tar.gz/cv13-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cv13",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "pyperclip>=1.8.2",
+        "openai>=1.84.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
