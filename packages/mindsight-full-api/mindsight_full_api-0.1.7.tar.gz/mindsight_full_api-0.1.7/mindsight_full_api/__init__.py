8806953b4e42ebf658b3a62b7eae020c7d9c5c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:07:12 +0000

    Add package mindsight-full-api/0.1.7/mindsight_full_api-0.1.7.tar.gz

---
+++ packages/mindsight-full-api/mindsight_full_api-0.1.7.tar.gz/mindsight_full_api-0.1.7/mindsight_full_api/__init__.py
@@ -0,0 +1,29 @@
+"""This module take all methods of scripts.__init__.py"""
+
+from mindsight_full_api.scripts import (
+    Absence,
+    Assessments,
+    Bonus,
+    CostCenters,
+    ExtraHours,
+    People,
+    PerformanceEvaluations,
+    PerformanceEvaluationRounds,
+    PerformanceIndicators,
+    PerformanceSections,
+    PerformanceLabels,
+    PerformanceMissions,
+    RaiseType,
+    Salary,
+    Shares,
+    SharesValue,
+    Trainings,
+    User,
+    VaraibleSalary,
+    AccessRules
+)
+
+__doc__ = """
+**mindsight_full_api** is a python package to providing
+methods to use Full Analytics rest api.
+"""
