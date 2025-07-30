6e102e09c4301b3620e68f1de14a1892969b02a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:45 +0000

    Add package nfetoolkit/0.2.7/nfetoolkit-0.2.7.tar.gz

---
+++ packages/nfetoolkit/nfetoolkit-0.2.7.tar.gz/nfetoolkit-0.2.7/test/test_nfe_rep_export.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import unittest
+
+# Necessário para que o arquivo de testes encontre
+test_root = os.path.dirname(os.path.abspath(__file__))
+os.chdir(test_root)
+sys.path.insert(0, os.path.dirname(test_root))
+sys.path.insert(0, test_root)
+
+from core import NFeRepository, NFeOrganizer, NFeHandler
+
+class TestNFeRepExport(unittest.TestCase):
+           
+    def test_rep(self):
+        
+        nfe_rep = NFeRepository()
+        
+        for xml in NFeOrganizer.find_all("C:\\temp\\INTERBRILHO\\xml\\nfe"):
+            
+            match (NFeHandler.xml_type(xml)):
+                case 'nfe_type':
+                    nfe_rep.store_nfe(NFeHandler.nfe_from_path(xml))
+                case 'canc_type':
+                    nfe_rep.store_evt(NFeHandler.evento_canc_from_path(xml))
+                case 'cce_type':
+                    nfe_rep.store_evt(NFeHandler.evento_cce_from_path(xml))
+
+        with open('nfe_data.txt', 'w') as file:
+            nfe_rep.write_to(file)
+            
+        self.assertIsNotNone(str(nfe_rep))
+        
+if __name__ == '__main__':
+    unittest.main()
