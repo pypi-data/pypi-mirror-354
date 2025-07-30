727e6e374de4eac45238644322e62097a443527f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:09:02 +0000

    Add package mosaic-mmrag/0.1.0/mosaic_mmrag-0.1.0.tar.gz

---
+++ packages/mosaic-mmrag/mosaic_mmrag-0.1.0.tar.gz/mosaic_mmrag-0.1.0/mosaic/schemas.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+from typing import Optional, Dict
+
+
+class Document(BaseModel):
+    pdf_id: str
+    page_number: int
+    rank: Optional[int]
+    score: Optional[float]
+    metadata: Optional[Dict]
+    base64_image: Optional[str]
+    pdf_abs_path: Optional[str]
