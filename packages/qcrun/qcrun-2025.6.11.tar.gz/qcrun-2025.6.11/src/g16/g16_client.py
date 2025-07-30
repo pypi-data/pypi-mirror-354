7cb7019bc63bd925b8d7d3bb3e24222d2138c879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:01 +0000

    Add package qcrun/2025.6.11/qcrun-2025.6.11.tar.gz

---
+++ packages/qcrun/qcrun-2025.6.11.tar.gz/qcrun-2025.6.11/src/g16/g16_client.py
@@ -0,0 +1,14 @@
+from src.task import Task
+from pathlib import Path
+from .g16_struct import Params, Structure
+
+
+class G16(Task):
+    params: Params
+    structure: Structure
+
+    def gen_input_content(self) -> str:
+        key_word_str = self.params.to_str_lines()
+        structure_str = self.structure.to_str_lines()
+        return key_word_str + "\n" + structure_str + "\n"
+
