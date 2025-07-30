6949cdb518deaea0e1045f3477cc13b9a15f6535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:35 +0000

    Add package coffea/2025.6.0rc0/coffea-2025.6.0rc0-py3-none-any.whl

---
+++ packages/coffea/coffea-2025.6.0rc0-py3-none-any.whl/coffea/processor/__init__.py
@@ -0,0 +1,39 @@
+"""A framework for analysis scale-out"""
+
+from .accumulator import (
+    Accumulatable,
+    AccumulatorABC,
+    accumulate,
+    column_accumulator,
+    defaultdict_accumulator,
+    dict_accumulator,
+    list_accumulator,
+    set_accumulator,
+    value_accumulator,
+)
+from .executor import (
+    DaskExecutor,
+    FuturesExecutor,
+    IterativeExecutor,
+    ParslExecutor,
+    Runner,
+)
+from .processor import ProcessorABC
+
+__all__ = [
+    "ProcessorABC",
+    "IterativeExecutor",
+    "FuturesExecutor",
+    "DaskExecutor",
+    "ParslExecutor",
+    "Runner",
+    "accumulate",
+    "Accumulatable",
+    "AccumulatorABC",
+    "value_accumulator",
+    "list_accumulator",
+    "set_accumulator",
+    "dict_accumulator",
+    "defaultdict_accumulator",
+    "column_accumulator",
+]
