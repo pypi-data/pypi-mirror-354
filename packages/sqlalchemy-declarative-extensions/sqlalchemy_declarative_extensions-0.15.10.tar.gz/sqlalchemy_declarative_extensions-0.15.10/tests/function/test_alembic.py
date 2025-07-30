291b720f5b0b5766970001c73d523bb12e3f0bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:42 +0000

    Add package sqlalchemy-declarative-extensions/0.15.10/sqlalchemy_declarative_extensions-0.15.10.tar.gz

---
+++ packages/sqlalchemy-declarative-extensions/sqlalchemy_declarative_extensions-0.15.10.tar.gz/sqlalchemy_declarative_extensions-0.15.10/tests/function/test_alembic.py
@@ -0,0 +1,28 @@
+import pytest
+
+from tests.utilities import successful_test_run
+
+
+@pytest.mark.alembic
+def test_function_create(pytester):
+    successful_test_run(pytester, count=1)
+
+
+@pytest.mark.alembic
+def test_function_update(pytester):
+    successful_test_run(pytester, count=1)
+
+
+@pytest.mark.alembic
+def test_function_drop(pytester):
+    successful_test_run(pytester, count=1)
+
+
+@pytest.mark.alembic
+def test_function_leading_whitespace(pytester):
+    successful_test_run(pytester, count=1)
+
+
+@pytest.mark.alembic
+def test_function_rewriter(pytester):
+    successful_test_run(pytester, count=1)
