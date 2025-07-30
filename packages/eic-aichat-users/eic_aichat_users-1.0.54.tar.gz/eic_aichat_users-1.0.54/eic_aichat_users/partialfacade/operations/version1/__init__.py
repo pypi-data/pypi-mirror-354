3a3a8ef0fe365f1480bd9cc1850d0fb07fc19d63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:24 +0000

    Add package eic-aichat-users/1.0.54/eic_aichat_users-1.0.54.tar.gz

---
+++ packages/eic-aichat-users/eic_aichat_users-1.0.54.tar.gz/eic_aichat_users-1.0.54/eic_aichat_users/partialfacade/operations/version1/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+__all__ = [
+        'AccountsOperations', 
+        'ActivitiesOperations', 
+        'SettingsOperations', 
+        'PasswordsOperations', 
+        'RolesOperations', 
+        'SessionsOperations', 
+        'SessionUserV1', 
+        'AuthorizerV1'
+        ]
+
+from .AccountsOperations import AccountsOperations
+from .ActivitiesOperations import ActivitiesOperations
+from .SettingsOperations import SettingsOperations
+from .PasswordsOperations import PasswordsOperations
+from .RolesOperations import RolesOperations
+from .SessionsOperations import SessionsOperations
+from .SessionUserV1 import SessionUserV1
+from .Authorize import AuthorizerV1
