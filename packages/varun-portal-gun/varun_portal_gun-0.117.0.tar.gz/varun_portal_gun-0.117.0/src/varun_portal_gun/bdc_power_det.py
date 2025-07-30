d159bcee57961608e8491f54ef0fc1d85ed6a70a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:35 +0000

    Add package varun-portal-gun/0.117.0/varun_portal_gun-0.117.0.tar.gz

---
+++ packages/varun-portal-gun/varun_portal_gun-0.117.0.tar.gz/varun_portal_gun-0.117.0/src/varun_portal_gun/bdc_power_det.py
@@ -0,0 +1,63 @@
+import joblib
+import pandas as pd
+
+from sklearn.preprocessing import OrdinalEncoder
+import random
+
+import warnings
+warnings.filterwarnings("ignore")
+
+#Task: no correction needed, just generate results for -40 and 55 C, use sgt_attn_cmd from bdc_gain df. 
+
+def bdc_power_det():
+    columns = ['pol', 'sgt_attn_cmd', 'p_out', 'TEMP_BDC_COMBINED_SCALED']
+
+    df = pd.DataFrame(columns=columns)
+
+    ### HERE - bdc power det room temp
+    pol = 'lhcp'
+
+    ###HERE - bdc power det room temp
+    p_out = 1
+
+
+    TEMP_BDC_COMBINED_SCALED = -40
+
+
+    temporary_p_out = 80
+    for i in range(13):
+        #sgt_attn_cmd - diff by temperature, check bdc_gain 40 df
+        sgt_attn_cmd = 20.5
+        df.loc[i] = [pol, sgt_attn_cmd, temporary_p_out, TEMP_BDC_COMBINED_SCALED]
+        temporary_p_out = temporary_p_out - 4
+
+    encoder = OrdinalEncoder(categories=[['lhcp', 'rhcp']])
+    df['pol'] = encoder.fit_transform(df[['pol']])
+
+    print(df)
+
+    loaded_model = joblib.load("/Users/vyelluru/Desktop/bdc_power_det_v5.sav")
+    result = loaded_model.predict(df)
+
+    print(result)
+
+
+
+    TEMP_BDC_COMBINED_SCALED = 55
+
+    temporary_p_out = 80
+    for i in range(13):
+        #sgt_attn_cmd - diff by temperature, check bdc_gain 55 df
+        sgt_attn_cmd = 20.5
+        df.loc[i] = [pol, sgt_attn_cmd, temporary_p_out, TEMP_BDC_COMBINED_SCALED]
+        temporary_p_out = temporary_p_out - 4
+    
+    encoder = OrdinalEncoder(categories=[['lhcp', 'rhcp']])
+    df['pol'] = encoder.fit_transform(df[['pol']])
+
+
+    loaded_model = joblib.load("/Users/vyelluru/Desktop/bdc_power_det_v5.sav")
+    result = loaded_model.predict(df)
+
+
+    print(result)
