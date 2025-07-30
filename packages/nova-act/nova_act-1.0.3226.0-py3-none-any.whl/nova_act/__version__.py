a6d0ff5da84485cfdeff983619b05cca4ce63403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:51 +0000

    Add package nova-act/1.0.3226.0/nova_act-1.0.3226.0-py3-none-any.whl

---
+++ packages/nova-act/nova_act-1.0.3226.0-py3-none-any.whl/nova_act/__version__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Amazon Inc
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+VERSION = "1.0.3226.0"  # pragma: no cover
+EXTENSION_VERSION = "Built on 2025-06-09T18:25:31.373Z off of Package Version: 1.0.6979.0 (10e2d41f91e257c495089850c4e2aeed2de14d0d)"  # pragma: no cover
