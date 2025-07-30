81bd6501fca51ff9f1672ae72dde2b2c021c04ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:07:20 +0000

    Add package bluer-ugv/6.69.1/bluer_ugv-6.69.1-py3-none-any.whl

---
+++ packages/bluer-ugv/bluer_ugv-6.69.1-py3-none-any.whl/bluer_ugv/swallow/README.py
@@ -0,0 +1,13 @@
+dict_of_images = {
+    "https://github.com/kamangir/assets/blob/main/bluer-ugv/bluer-light.png?raw=true": "",
+    "../../diagrams/bluer-swallow/3d-design.png": "../../diagrams/bluer-swallow/3d-design.stl",
+    "https://github.com/kamangir/assets2/blob/main/bluer-swallow/20250605_180136.jpg?raw=true": "",
+    "https://github.com/kamangir/assets2/blob/main/bluer-swallow/20250608_144453.jpg?raw=true": "",
+    "https://github.com/kamangir/assets2/blob/main/bluer-swallow/20250609_164433.jpg?raw=true": "",
+    "https://github.com/kamangir/assets2/blob/main/bluer-swallow/20250611_100917.jpg?raw=true": "",
+}
+
+items = [
+    "[![image]({})]({})".format(image, url if url else image)
+    for image, url in dict_of_images.items()
+]
