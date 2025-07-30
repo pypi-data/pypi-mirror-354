0110617b84038ab9d7fc96e0355f7c4a8ee14e05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:28 +0000

    Add package retab/0.0.36/retab-0.0.36-py3-none-any.whl

---
+++ packages/retab/retab-0.0.36-py3-none-any.whl/uiform/types/standards.py
@@ -0,0 +1,39 @@
+from typing import Any, Generic, List, Literal, Optional, Tuple, TypeVar
+
+from pydantic import BaseModel, Field
+
+# API Standards
+
+# Define a type variable to represent the content type
+T = TypeVar("T")
+
+
+# Define the ErrorDetail model
+class ErrorDetail(BaseModel):
+    code: str
+    message: str
+    details: Optional[dict] = None
+
+
+class StandardErrorResponse(BaseModel):
+    detail: ErrorDetail
+
+
+class StreamingBaseModel(BaseModel):
+    streaming_error: ErrorDetail | None = None
+
+
+class DocumentPreprocessResponseContent(BaseModel):
+    messages: list[dict[str, Any]] = Field(..., description="Messages generated during the preprocessing of the document")
+    json_schema: dict[str, Any] = Field(..., description="Generated JSON Schema for Structured Output OpenAI Completions")
+
+
+class PreparedRequest(BaseModel):
+    method: Literal["POST", "GET", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS", "CONNECT", "TRACE"]
+    url: str
+    data: dict | None = None
+    params: dict | None = None
+    form_data: dict | None = None
+    files: dict | List[Tuple[str, Tuple[str, bytes, str]]] | None = None
+    idempotency_key: str | None = None
+    raise_for_status: bool = False
