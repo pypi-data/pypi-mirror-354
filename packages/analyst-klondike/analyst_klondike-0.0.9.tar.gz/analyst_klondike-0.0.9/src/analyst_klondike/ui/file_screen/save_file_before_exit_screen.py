8112d82ae19fa6943f8c568ea0f74ce51ac5145e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:41 +0000

    Add package analyst-klondike/0.0.9/analyst_klondike-0.0.9.tar.gz

---
+++ packages/analyst-klondike/analyst_klondike-0.0.9.tar.gz/analyst_klondike-0.0.9/src/analyst_klondike/ui/file_screen/save_file_before_exit_screen.py
@@ -0,0 +1,33 @@
+from typing import Literal
+from textual import on
+from textual.app import ComposeResult
+from textual.screen import ModalScreen
+from textual.containers import Grid
+from textual.widgets import Label, Button
+
+SaveOrExitModalResult = Literal["save", "not_save", "cancel"]
+
+
+class SaveOnExitModal(ModalScreen[SaveOrExitModalResult]):
+
+    CSS_PATH = "save_on_exit_modal.tcss"
+
+    def compose(self) -> ComposeResult:
+        with Grid(id="dialog"):
+            yield Label("Сохранить файл?")
+            with Grid(id="buttons"):
+                yield Button(id="save", label="Сохранить", variant="success")
+                yield Button(id="not_save", label="Не сохранять", variant="error")
+                yield Button(id="cancel", label="Отмена", variant="default")
+
+    @on(Button.Pressed, "#save")
+    def on_save_button_clicked(self) -> None:
+        self.dismiss("save")
+
+    @on(Button.Pressed, "#not_save")
+    def on_not_save_button_clicked(self) -> None:
+        self.dismiss("not_save")
+
+    @on(Button.Pressed, "#cancel")
+    def on_cancel_button_clicked(self) -> None:
+        self.dismiss("cancel")
