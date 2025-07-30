aae936aa4764b0602eb4f7be413ec21280ab2790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:33 +0000

    Add package whisper-ssh/1.0.2/whisper_ssh-1.0.2.tar.gz

---
+++ packages/whisper-ssh/whisper_ssh-1.0.2.tar.gz/whisper_ssh-1.0.2/whisper_ssh/__init__.py
@@ -0,0 +1,36 @@
+"""
+Whisper SSH - Whisper messages to remote Linux machines via SSH.
+
+A Python library and command-line tool for sending desktop notifications
+to remote Ubuntu/Linux machines using SSH and notify-send.
+
+Example usage:
+    >>> from whisper_ssh import RemoteNotificationManager
+    >>> manager = RemoteNotificationManager()
+    >>> manager.send_notification("192.168.1.10", "user", "password", "Hello!")
+
+Command line usage:
+    $ whisper-ssh
+    $ whisper  # short alias
+"""
+
+__version__ = "1.0.2"
+__author__ = "Josh Meesey"
+__license__ = "MIT"
+
+from .core import RemoteNotificationManager
+from .config import ConfigManager
+
+# Define what gets imported with "from whisper_ssh import *"
+__all__ = [
+    "RemoteNotificationManager",
+    "ConfigManager",
+    "__version__",
+]
+
+# Package metadata
+__title__ = "whisper-ssh"
+__description__ = "Whisper messages to remote Linux machines via SSH"
+__url__ = "https://github.com/JdMasuta/whisper-ssh"
+__maintainer__ = __author__
+__keywords__ = ["ssh", "notification", "whisper", "remote", "desktop", "linux", "ubuntu"]
