15203ef21e7c44fc032efada3ebe22a546254c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:57 +0000

    Add package synapse-sdk/1.0.0a53/synapse_sdk-1.0.0a53-py3-none-any.whl

---
+++ packages/synapse-sdk/synapse_sdk-1.0.0a53-py3-none-any.whl/synapse_sdk/clients/ray/serve.py
@@ -0,0 +1,20 @@
+from synapse_sdk.clients.base import BaseClient
+from synapse_sdk.clients.exceptions import ClientError
+
+
+class ServeClientMixin(BaseClient):
+    def list_serve_applications(self, params=None):
+        path = 'api/serve/applications/'
+        response = self._get(path, params=params)
+        for key, item in response['applications'].items():
+            response['applications'][key]['deployments'] = list(item['deployments'].values())
+        return list(response['applications'].values())
+
+    def get_serve_application(self, pk, params=None):
+        path = 'api/serve/applications/'
+        response = self._get(path, params=params)
+        try:
+            response['applications'][pk]['deployments'] = list(response['applications'][pk]['deployments'].values())
+            return response['applications'][pk]
+        except KeyError:
+            raise ClientError(404, 'Serve Application Not Found')
