cf1e61f71017803b9a66040d428237b06b018dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:57 +0000

    Add package musicmaster/1.4.1/musicmaster-1.4.1-py3-none-any.whl

---
+++ packages/musicmaster/musicmaster-1.4.1-py3-none-any.whl/music/context/options.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from typing import Annotated
+
+import typer
+
+from music.models.path import Path
+
+clean_download_ids_help = (
+    "Spotify changes the ids of its tracks from time to time.\n"
+    "This command saves the most recent id of all downloaded tracks"
+)
+
+clean_download_ids_option = typer.Option(help=clean_download_ids_help)
+
+
+@dataclass
+class Options:
+    config_path: Path = Path.config
+    upload_to_phone: bool = True
+    clean_download_ids: Annotated[bool, clean_download_ids_option] = False
+    fix_mtimes_on_phone: bool = False
