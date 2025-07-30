0d917b08e573e8941fe027d677b4db38329cae86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:08:27 +0000

    Add package xlpd/0.1.6/xlpd-0.1.6.tar.gz

---
+++ packages/xlpd/xlpd-0.1.6.tar.gz/xlpd-0.1.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xlpd",
+    version="0.1.6",
+    author="Yinchu Xia",
+    author_email="sales@pytorchsoft.com",
+    description="xiu lian ping de",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/pytorchsoft/xlpd",
+    packages=find_packages("src"),
+    install_requires=[
+        "numpy",  # List your dependencies here
+        "typer",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    entry_points={
+        "console_scripts": [
+            "xlpd = xlpd.cli:app",
+        ]
+    },
+)
