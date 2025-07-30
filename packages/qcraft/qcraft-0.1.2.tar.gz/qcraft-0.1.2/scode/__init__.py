5e57b0c99ed097988998258b2ec8ffb427ba6aa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:21 +0000

    Add package qcraft/0.1.2/qcraft-0.1.2.tar.gz

---
+++ packages/qcraft/qcraft-0.1.2.tar.gz/qcraft-0.1.2/scode/__init__.py
@@ -0,0 +1,5 @@
+# Remove problematic imports to avoid ModuleNotFoundError on package import
+# from .orchestrator import Orchestrator
+# from . import code_switcher
+# from .heuristic_layer.surface_code import SurfaceCode
+# from .code_switcher.code_switcher import CodeSwitcher 
---
+++ packages/qcraft/qcraft-0.1.2.tar.gz/qcraft-0.1.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qcraft',
+    version='0.1.2',
+    description='Qcraft: Quantum Circuit Design, Optimization, and Surface Code Mapping Platform',
+    author='Debasis Mondal',
+    packages=find_packages(),
+    install_requires=[
+        'PySide6',
+        'PyYAML',
+        'jsonschema',
+        'networkx',
+        'matplotlib',
+        'numpy',
+        'stable-baselines3',
+        'scikit-learn',
+        'pandas',
+        'torch',
+        'gymnasium',
+        'stim',
+        'pymatching',
+        'qiskit>=1.0',
+        'qiskit-aer',
+        'qiskit-ibm-runtime',
+        'python-dotenv'
+    ],
+    entry_points={
+        'console_scripts': [
+            'qcraft = circuit_designer.gui_main:main',
+        ],
+    },
+    include_package_data=True,
+    package_data={
+        'scode': ['code_switcher/*.py'],
+        'configs': ['*.json', '*.yaml'],
+    },
+) 
