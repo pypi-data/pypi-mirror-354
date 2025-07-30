6528cc48b7b537331b9ddd846b5b0c293262e9a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:43 +0000

    Add package slobs-cli/0.8.2/slobs_cli-0.8.2-py3-none-any.whl

---
+++ packages/slobs-cli/slobs_cli-0.8.2-py3-none-any.whl/slobs_cli/errors.py
@@ -0,0 +1,13 @@
+import asyncclick as click
+
+
+class SlobsCliError(click.ClickException):
+    """Base class for all Slobs CLI errors."""
+
+    def __init__(self, message: str):
+        super().__init__(message)
+        self.exit_code = 1
+
+    def show(self):
+        """Display the error message in red."""
+        click.secho(f"Error: {self.message}", fg="red", err=True)
