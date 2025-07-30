e61967269295ea688cfe3968d52604379e5e67f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:57 +0000

    Add package numpyp3/0.0.1/numpyp3-0.0.1.tar.gz

---
+++ packages/numpyp3/numpyp3-0.0.1.tar.gz/numpyp3-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md', encoding='utf-8') as f:
+    return f.read()
+
+
+setup(
+  name='numpyp3',
+  version='0.0.1',
+  description='This is the simplest module for quick work with files.',
+  license='MIT',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  packages=find_packages(),
+  package_data={
+    'numpyp3.theory': ['*.png','*.pdf'], 'numpyp3': ['.env'],
+    },
+  install_requires=[
+    'IPython','pyperclip','python-telegram-bot',
+    'python-dotenv'
+  ],
+)
+
+
