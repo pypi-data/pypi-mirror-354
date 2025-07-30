65946ec6e83f96a6fa306367cbe1a0afb8fad19b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:24 +0000

    Add package scalenav/0.0.6/scalenav-0.0.6.tar.gz

---
+++ packages/scalenav/scalenav-0.0.6.tar.gz/scalenav-0.0.6/README.md
@@ -0,0 +1,35 @@
+# Navigating spatial scales
+
+![GitHub Actions Workflow
+Status](https://img.shields.io/github/actions/workflow/status/nismod/scale-nav/run-package.yml?style=flat)
+![PyPI -
+Version](https://img.shields.io/pypi/v/scalenav?style=flat&logoColor=grey&logoSize=auto&labelColor=blue&color=gray&link=https%3A%2F%2Fpypi.org%2Fproject%2Fscalenav%2F)
+![PyPI - Python
+Version](https://img.shields.io/pypi/pyversions/scalenav?style=flat.png)
+![PyPI - Wheel](https://img.shields.io/pypi/wheel/scalenav.png)
+
+## Introduction
+
+This is a python module that helps change scales of spatial data sets.
+It relies on the [H3](https://h3geo.org) hexagonal grid’s hierarchical
+indexing functionalities and aims to add to that the data
+transformations that need to be done when navigating between levels of
+the hierarchy.
+
+A presentation on the topic can be found
+<a href="https://ischlo.github.io/presentations/down_scaling"
+target="_blank">here</a>
+
+### THIS MODULE IS UNDER DEVELOPMENT
+
+The package website introduces the use cases and the different functionalities that are covered so far.
+
+## Main functionalities
+
+### Rastapar
+
+The command line tool allows for efficient, multi core ingestion of rasters into parquet tables. 
+
+### Scalenav modules
+
+A set of modules provides python functions for performing a operations with data sets expressed as H3 cells. Additionaly, a module with the equivalent high performance functions written with ibis and duckdb.
---
+++ packages/scalenav/scalenav-0.0.6.tar.gz/scalenav-0.0.6/src/scalenav/plotting.py
@@ -0,0 +1,53 @@
+# plotting.py
+from numpy import max, nan_to_num, log1p
+from pandas import Series
+from matplotlib.colors import LinearSegmentedColormap, ListedColormap
+from pypalettes import load_cmap
+
+# from pypalettes import
+
+
+def cmap(input: [Series, list], palette: LinearSegmentedColormap | ListedColormap | str, log: bool = False, pydeck: bool = True, factorize: bool = False, var_range=None) -> list[int]:  # type: ignore
+    """Provide colormap for deckgl plotting.
+
+    Parameters
+    ----------
+
+    input : a list or pandas.Series of numeric values
+
+    palette : a palette from matplotlib.colors, pypalettes, or a palette name from https://python-graph-gallery.com/color-palette-finder/.
+
+    log : whether the scale should be logarithmic
+
+    pydeck : whether or not the colors are for pydeck maps. If true, the returned color values are integers between 0 and 255 instead of floats between 0 and 1.
+
+    Returns
+    ---------
+    a list containting lists of length 3 with r,g,b values between 0 and 255 for each value from input.
+    """
+    #  if factorize: can be done outside of the function as well. Just provide a different color palette.
+
+    if log:
+        input = nan_to_num(log1p(input)).tolist()
+    else:
+        input = nan_to_num(input).tolist()
+
+    if palette is str:
+        palette = load_cmap(palette)
+
+    if var_range is not None:
+        if log:
+            m = log1p(var_range[1])
+        else:
+            m = var_range[1]
+    else:
+        m = max(input)
+
+    l = palette.N
+
+    print(f"Max input : {m:.2f}, palette colors : {l}")
+
+    if pydeck:
+        return [[int(255 * j) for j in palette(int(x / m * l))] for x in input]
+    else:
+        return [palette(int(x / m * l)) for x in input]
