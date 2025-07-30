6871f3f693c34eb59832e24793014d2042712a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:39 +0000

    Add package varun-portal-gun/0.47.0/varun_portal_gun-0.47.0-py3-none-any.whl

---
+++ packages/varun-portal-gun/varun_portal_gun-0.47.0-py3-none-any.whl/varun_portal_gun/main.py
@@ -0,0 +1,51 @@
+import typer
+from pathlib import Path
+from typing import Optional
+
+from typing_extensions import Annotated
+import os
+
+from .bdc_power_det import bdc_power_det
+from .bdc_autobias import bdc_autobias
+
+
+app = typer.Typer()
+
+
+@app.callback()
+def callback():
+    pass
+
+
+@app.command()
+def predict(path: Annotated[str, typer.Option()] = None):
+    print("Here are your input files: " + path)
+
+    for filename in os.listdir(path):
+        print(filename)
+        
+
+
+#Setting up inference X_tests
+#plan have enough in df to run prediction then build out the rest based on pol
+
+'''
+BDC autobias
+2-calls to model, one for room temp, and one for 40/55 C
+'''
+@app.command()
+def bdc_autobias_test():
+    print("Calling")
+    bdc_autobias()
+    print("BDC Autobias Success")
+
+
+
+'''
+BDC Power - -80 to -32 p_in * 2 pol's * 2 temps (52 rows)
+creating an X_test df
+'''
+@app.command()
+def bdc_power_det_test():
+    bdc_power_det()
+    print("BDC Power Det")
