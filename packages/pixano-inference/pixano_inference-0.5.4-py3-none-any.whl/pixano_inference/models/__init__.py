17e2053c4cd1ca8b1dff357f23fc7cf5e94ab44f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:00 +0000

    Add package pixano-inference/0.5.4/pixano_inference-0.5.4-py3-none-any.whl

---
+++ packages/pixano-inference/pixano_inference-0.5.4-py3-none-any.whl/pixano_inference/models/__init__.py
@@ -0,0 +1,13 @@
+# =================================
+# Copyright: CEA-LIST/DIASI/SIALV
+# Author : pixano@cea.fr
+# License: CECILL-C
+# =================================
+
+# ruff: noqa: F401
+# ruff: noqa: D104
+
+from .base import BaseInferenceModel
+from .sam2 import Sam2Model
+from .transformers import TransformerModel
+from .vllm import VLLMModel
