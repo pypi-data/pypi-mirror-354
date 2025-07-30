402a6c90ad9455ce049913756ac7bdbabe826fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:29 +0000

    Add package tidy3d/2.9.0rc1/tidy3d-2.9.0rc1-py3-none-any.whl

---
+++ packages/tidy3d/tidy3d-2.9.0rc1-py3-none-any.whl/tidy3d/plugins/autograd/primitives/misc.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+import scipy.ndimage
+from autograd.extend import defvjp, primitive
+
+gaussian_filter = primitive(scipy.ndimage.gaussian_filter)
+defvjp(
+    gaussian_filter,
+    lambda ans, x, *args, **kwargs: lambda g: gaussian_filter(g, *args, **kwargs),
+)
