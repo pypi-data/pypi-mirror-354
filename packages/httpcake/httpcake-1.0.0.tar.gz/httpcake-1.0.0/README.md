3f36c2d93a505d36b598f2d470f5689a21a1de2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:06 +0000

    Add package httpcake/1.0.0/httpcake-1.0.0.tar.gz

---
+++ packages/httpcake/httpcake-1.0.0.tar.gz/httpcake-1.0.0/README.md
@@ -0,0 +1,4 @@
+# httpcake 🍰
+
+**httpcake** is a lightweight, fast Python HTTP client with a built-in DNS resolver using `xbox-dns.ru`.  
+It works like `requests`, but with the speed of `aiohttp` — no dependencies, no asyncio required.
---
+++ packages/httpcake/httpcake-1.0.0.tar.gz/httpcake-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='httpcake',
+    version='1.0.0',
+    packages=['httpcake'],
+    description='Library for receiving/sending requests to a site with some anonymization',
+    author='FMZNkdv',
+    author_email='telegram@FMZNkdv.com',
+    url='https://LibCake.t.me',
+    license='MIT',
+)
