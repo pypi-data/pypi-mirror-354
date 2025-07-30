ad0b4d436212d1f237da7082ccef735d5cb9be94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:48 +0000

    Add package thsdk/1.3.6/thsdk-1.3.6.tar.gz

---
+++ packages/thsdk/thsdk-1.3.6.tar.gz/thsdk-1.3.6/thsdk/example/tick_level1.py
@@ -0,0 +1,13 @@
+from thsdk import THS, Interval, Adjust
+import pandas as pd
+import time
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+with THS() as ths:
+    response = ths.tick_level1("USZA300033")
+    print("历史当日分钟数据:")
+    if response.errInfo != "":
+        print(f"错误信息: {response.errInfo}")
+    print(pd.DataFrame(response.payload.result))
+    time.sleep(1)
