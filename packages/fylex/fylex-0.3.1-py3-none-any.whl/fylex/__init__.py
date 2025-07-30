749c3da51d4b965463d5357952711d900bb2291b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:24 +0000

    Add package fylex/0.3.1/fylex-0.3.1-py3-none-any.whl

---
+++ packages/fylex/fylex-0.3.1-py3-none-any.whl/fylex/__init__.py
@@ -0,0 +1,5 @@
+from .fylex import copy_files, move_files, JUNK_EXTENSIONS
+from .exceptions import *
+
+__version__ = "0.3.1"
+__all__ = ["copy_files", "move_files", "JUNK_EXTENSIONS", "FylexError", "InvalidPathError", "CopyFailedError"]
