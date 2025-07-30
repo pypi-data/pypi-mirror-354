6a8a3aa2d1fcf675aa3cfe5af2b6e62e0906d498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:59 +0000

    Add package vaiz-sdk/0.1.0/vaiz_sdk-0.1.0-py3-none-any.whl

---
+++ packages/vaiz-sdk/vaiz_sdk-0.1.0-py3-none-any.whl/vaiz/models/__init__.py
@@ -0,0 +1,19 @@
+from .base import TaskFollower, TaskPriority, CustomField
+from .tasks import Task, TaskResponse, CreateTaskRequest, EditTaskRequest
+from vaiz.models.boards import Board, BoardsResponse, BoardResponse
+from vaiz.models.profile import Profile, ProfileResponse
+
+__all__ = [
+    'TaskFollower',
+    'TaskPriority',
+    'CustomField',
+    'CreateTaskRequest',
+    'EditTaskRequest',
+    'Task',
+    'TaskResponse',
+    'Board',
+    'BoardsResponse',
+    'BoardResponse',
+    'Profile',
+    'ProfileResponse'
+] 
