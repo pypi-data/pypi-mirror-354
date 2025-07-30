23bb49a5d78f35d524b794dda59867be906d833a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:21 +0000

    Add package vitalx-types/0.2.2/vitalx_types-0.2.2.tar.gz

---
+++ packages/vitalx-types/vitalx_types-0.2.2.tar.gz/vitalx_types-0.2.2/vitalx/types/__init__.py
@@ -0,0 +1,4 @@
+from .environments import VitalEnvironmentT, VitalRegionT, api_base_url
+from .providers import Labs, Providers
+
+__all__ = ["Providers", "Labs", "VitalEnvironmentT", "VitalRegionT", "api_base_url"]
