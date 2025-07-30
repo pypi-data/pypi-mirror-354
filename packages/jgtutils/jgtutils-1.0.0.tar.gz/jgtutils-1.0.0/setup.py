c5fbef7f7db6b9785bb9fb49ef5f45c2099e62dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:11 +0000

    Add package jgtutils/1.0.0/jgtutils-1.0.0.tar.gz

---
+++ packages/jgtutils/jgtutils-1.0.0.tar.gz/jgtutils-1.0.0/setup.py
@@ -0,0 +1,43 @@
+import re
+
+from setuptools import find_packages, setup
+
+
+def read_version():
+    with open("jgtutils/__init__.py") as f:
+        content=f.read()
+        version_match = re.search(r"version=['\"]([^'\"]*)['\"]", content)
+        return version_match.group(1)
+
+version = read_version()
+
+setup(
+    name='jgtutils',
+    version=version,
+    packages=find_packages(),
+    #install_requires=INSTALL_REQUIRES,
+    description='A utility package common to other JGT projects.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Guillaume Isabelle',
+    author_email='jgi@jgwill.com',
+    url='https://github.com/jgwill/jgtutils',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],    
+    # entry_points={
+    #     'console_scripts': ['jgtutr=jgtutils.cli_tlid_range:main'],
+    # },
+    keywords='utilities',
+    project_urls={
+        'Bug Reports': 'https://github.com/jgwill/jgtutils/issues',
+        'Source': 'https://github.com/jgwill/jgtutils',
+    },
+)
