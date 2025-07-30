8a16197228f20af05428a1ded3f46429ee0e9b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:05 +0000

    Add package shapes-geometry/0.1/shapes_geometry-0.1.tar.gz

---
+++ packages/shapes-geometry/shapes_geometry-0.1.tar.gz/shapes_geometry-0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools 
+
+setuptools.setup( 
+    name='shapes_geometry', 
+    version='0.1', 
+    author="Arunkumar", 
+    author_email="arun5412ten@gmail.com", 
+    description="This package provides a collection of functions to calculate geometric properties of various 2D and 3D shapes. It simplifies the computation of area, perimeter, volume, surface area, and other shape-specific dimensions using a modular, well-organized structure.", 
+    packages=setuptools.find_packages(), 
+    classifiers=[ 
+        "Programming Language :: Python :: 3", 
+        "License :: OSI Approved :: MIT License", 
+        "Operating System :: OS Independent", 
+        ], 
+    )
