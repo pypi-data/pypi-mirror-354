d7ca3014d2240bc543299e08cb800c22b693344e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:24:10 +0000

    Add package openedu-ai-workspace/0.1.29/openedu_ai_workspace-0.1.29-py3-none-any.whl

---
+++ packages/openedu-ai-workspace/openedu_ai_workspace-0.1.29-py3-none-any.whl/ai_workspace/exceptions/errors/workspace.py
@@ -0,0 +1,16 @@
+class WorkspaceError(Exception):
+    """Custom exception for Workspace-related errors."""
+
+    pass
+
+
+class DocumentError(Exception):
+    """Custom exception for Document-related errors."""
+
+    pass
+
+
+class CostError(Exception):
+    """Custom exception for Cost-related errors."""
+
+    pass
