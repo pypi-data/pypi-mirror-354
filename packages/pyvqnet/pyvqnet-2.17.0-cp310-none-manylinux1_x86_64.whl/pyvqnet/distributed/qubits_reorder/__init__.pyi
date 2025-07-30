c182a25abcd51cc83027c69a4f2970a4815e3939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:31 +0000

    Add package pyvqnet/2.17.0/pyvqnet-2.17.0-cp310-none-manylinux1_x86_64.whl

---
+++ packages/pyvqnet/pyvqnet-2.17.0-cp310-none-manylinux1_x86_64.whl/pyvqnet/distributed/qubits_reorder/__init__.pyi
@@ -0,0 +1,2 @@
+from .swap_q1 import swap_global_global_q1 as swap_global_global_q1, swap_local_global_q1 as swap_local_global_q1
+from .swap_qubits import QubitReorder as QubitReorder, QubitsPermutation as QubitsPermutation
---
+++ packages/pyvqnet/pyvqnet-2.17.0-cp310-none-manylinux1_x86_64.whl/pyvqnet/nn/__init__.pyi
@@ -0,0 +1,18 @@
+from .activation import ELU as ELU, Gelu as Gelu, HardSigmoid as HardSigmoid, LeakyReLu as LeakyReLu, ReLu as ReLu, SiLU as SiLU, Sigmoid as Sigmoid, Softmax as Softmax, Softplus as Softplus, Softsign as Softsign, Tanh as Tanh
+from .batch_norm import BatchNorm1d as BatchNorm1d, BatchNorm2d as BatchNorm2d
+from .conv import Conv1D as Conv1D, Conv2D as Conv2D, ConvT2D as ConvT2D
+from .dropout import DropPath as DropPath, Dropout as Dropout
+from .embedding import Embedding as Embedding
+from .fuse import fuse_module as fuse_module
+from .group_norm import GroupNorm as GroupNorm
+from .gru import Dynamic_GRU as Dynamic_GRU, GRU as GRU
+from .interpolate import Interpolate as Interpolate, interpolate as interpolate
+from .layer_norm import LayerNorm1d as LayerNorm1d, LayerNorm2d as LayerNorm2d, LayerNormNd as LayerNormNd
+from .linear import Identity as Identity, Linear as Linear, Torch_Linear as Torch_Linear
+from .loss import BinaryCrossEntropy as BinaryCrossEntropy, CategoricalCrossEntropy as CategoricalCrossEntropy, CrossEntropyLoss as CrossEntropyLoss, MeanSquaredError as MeanSquaredError, NLL_Loss as NLL_Loss, SoftmaxCrossEntropy as SoftmaxCrossEntropy
+from .lstm import Dynamic_LSTM as Dynamic_LSTM, LSTM as LSTM
+from .module import Module as Module, ModuleDict as ModuleDict, ModuleList as ModuleList, ParameterDict as ParameterDict, ParameterList as ParameterList, Sequential as Sequential
+from .parameter import Parameter as Parameter
+from .pixel_shuffle import Pixel_Shuffle as Pixel_Shuffle, Pixel_Unshuffle as Pixel_Unshuffle
+from .pooling import AvgPool1D as AvgPool1D, AvgPool2D as AvgPool2D, MaxPool1D as MaxPool1D, MaxPool2D as MaxPool2D
+from .rnn import Dynamic_RNN as Dynamic_RNN, RNN as RNN
