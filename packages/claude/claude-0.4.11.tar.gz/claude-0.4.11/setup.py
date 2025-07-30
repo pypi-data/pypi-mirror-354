67bd48c7cae84c29af62af8e3ae75138c0c35940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:35:44 +0000

    Add package claude/0.4.11/claude-0.4.11.tar.gz

---
+++ packages/claude/claude-0.4.11.tar.gz/claude-0.4.11/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="claude",
+    version="0.4.11",
+    description="Reserved",
+    author="Lina Tawfik",
+    author_email="",
+    py_modules=["placeholder"],
+)
