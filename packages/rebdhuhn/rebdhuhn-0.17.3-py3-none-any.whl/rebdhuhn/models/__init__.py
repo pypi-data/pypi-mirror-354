9265fb21615af8204992589fa3c8ad2397e83feb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:26 +0000

    Add package rebdhuhn/0.17.3/rebdhuhn-0.17.3-py3-none-any.whl

---
+++ packages/rebdhuhn/rebdhuhn-0.17.3-py3-none-any.whl/rebdhuhn/models/__init__.py
@@ -0,0 +1,37 @@
+"""
+The models sub-package contains the data models used:
+1. a representation of the scraped EBD tables
+2. the data model for the result of the conversion
+"""
+
+from rebdhuhn.models.ebd_graph import (
+    DecisionNode,
+    EbdGraph,
+    EbdGraphEdge,
+    EbdGraphMetaData,
+    EbdGraphNode,
+    EndNode,
+    OutcomeNode,
+    StartNode,
+    ToNoEdge,
+    ToYesEdge,
+)
+from rebdhuhn.models.ebd_table import EbdCheckResult, EbdTable, EbdTableMetaData, EbdTableRow, EbdTableSubRow
+
+__all__ = [
+    "DecisionNode",
+    "EbdGraph",
+    "EbdGraphEdge",
+    "EbdGraphMetaData",
+    "EbdGraphNode",
+    "EndNode",
+    "OutcomeNode",
+    "StartNode",
+    "ToNoEdge",
+    "ToYesEdge",
+    "EbdCheckResult",
+    "EbdTable",
+    "EbdTableMetaData",
+    "EbdTableRow",
+    "EbdTableSubRow",
+]
