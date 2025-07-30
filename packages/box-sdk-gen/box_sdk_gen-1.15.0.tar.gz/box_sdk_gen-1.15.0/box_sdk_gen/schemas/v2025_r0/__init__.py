10c6e183cd72c858063d27a69fd29190fb26a867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:23 +0000

    Add package box-sdk-gen/1.15.0/box_sdk_gen-1.15.0.tar.gz

---
+++ packages/box-sdk-gen/box_sdk_gen-1.15.0.tar.gz/box_sdk_gen-1.15.0/box_sdk_gen/schemas/v2025_r0/__init__.py
@@ -0,0 +1,63 @@
+from box_sdk_gen.schemas.v2025_r0.client_error_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.doc_gen_batch_base_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.doc_gen_document_generation_data_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.doc_gen_job_base_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.doc_gen_tag_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.doc_gen_tags_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.doc_gen_tags_processing_message_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.enterprise_reference_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.file_reference_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.doc_gen_template_create_request_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.doc_gen_template_base_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.doc_gen_template_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.doc_gen_templates_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.file_version_base_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.doc_gen_job_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.doc_gen_jobs_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.doc_gen_batch_create_request_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.shield_list_mini_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.shield_lists_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.shield_list_content_country_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.shield_list_content_domain_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.shield_list_content_email_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.shield_list_content_integration_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.shield_list_content_ip_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.shield_list_content_request_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.shield_lists_update_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.shield_lists_create_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.shield_list_content_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.shield_list_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.user_base_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.doc_gen_job_full_v2025_r0 import *
+
+from box_sdk_gen.schemas.v2025_r0.doc_gen_jobs_full_v2025_r0 import *
