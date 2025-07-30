32eae5de51a6a9cc34a24060cc1c91f6d84030d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:40 +0000

    Add package mosec/0.9.5/mosec-0.9.5-py3-none-musllinux_1_2_i686.whl

---
+++ packages/mosec/mosec-0.9.5-py3-none-musllinux_1_2_i686.whl/mosec/runtime.py
@@ -0,0 +1,283 @@
+# Copyright 2023 MOSEC Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Managers to control Coordinator and Mosec process."""
+
+import multiprocessing as mp
+import subprocess
+from multiprocessing.context import ForkContext, SpawnContext
+from multiprocessing.process import BaseProcess
+from multiprocessing.synchronize import Event
+from pathlib import Path
+from time import monotonic, sleep
+from typing import Callable, Dict, Iterable, List, Optional, Type, Union, cast
+
+from mosec.coordinator import Coordinator
+from mosec.env import env_var_context, validate_env, validate_float_ge, validate_int_ge
+from mosec.log import get_internal_logger
+from mosec.utils import get_mosec_path
+from mosec.worker import Worker
+
+logger = get_internal_logger()
+
+NEW_PROCESS_METHOD = {"spawn", "fork"}
+
+
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=too-many-arguments
+# pylint: disable=too-few-public-methods
+class Runtime:
+    """The wrapper with one worker and its arguments."""
+
+    # count how many runtime instances have been created
+    _stage_id: int = 0
+
+    def __init__(
+        self,
+        worker: Type[Worker],
+        num: int = 1,
+        max_batch_size: int = 1,
+        max_wait_time: int = 0,
+        timeout: float = 0.0,
+        start_method: str = "spawn",
+        env: Union[None, List[Dict[str, str]]] = None,
+    ):
+        """Initialize the mosec coordinator.
+
+        Args:
+            worker: subclass of `mosec.Worker` implemented by users.
+            num: number of workers
+            max_batch_size: the maximum batch size allowed (>=1), will enable the
+                dynamic batching if it > 1
+            max_wait_time: the maximum wait time (millisecond)
+                for dynamic batching, needs to be used with `max_batch_size`
+                to enable the feature. If not configure, will use the CLI
+                argument `--wait` (default=10ms)
+            timeout: timeout (second) for the `forward` function.
+                If not set, will use the CLI argument `--timeout` (default=3s)
+            start_method: the process starting method ("spawn" or "fork")
+            env: the environment variables to set before starting the process
+
+        """
+        self.worker = worker
+        self.num = num
+        self.max_batch_size = max_batch_size
+        self.max_wait_time = max_wait_time
+        self.timeout_sec = float(timeout)
+        self.start_method = start_method
+        self.env = env
+
+        Runtime._stage_id += 1
+        # adding the stage id in case the worker class is added to multiple stages
+        self.name = f"{self.worker.__name__}_{self._stage_id}"
+        self._pool: List[Union[BaseProcess, None]] = [None for _ in range(self.num)]
+
+        self._validate()
+
+    def _align_timeout_wait(self, timeout_sec: float, max_wait_time: int):
+        """Align the ``timeout`` and ``max_wait_time`` arguments."""
+        self.timeout_sec = self.timeout_sec if self.timeout_sec > 0 else timeout_sec
+        self.max_wait_time = (
+            self.max_wait_time if self.max_wait_time > 0 else max_wait_time
+        )
+
+    @staticmethod
+    def _process_healthy(process: Union[BaseProcess, None]) -> bool:
+        """Check if the child process is healthy.
+
+        ref: https://docs.python.org/3/library/multiprocessing.html
+
+            The exit code will be None if the process has not yet terminated.
+        """
+        return process is not None and process.exitcode is None
+
+    def _healthy(self, method: Callable[[Iterable[object]], bool]) -> bool:
+        """Check if all/any of the child processes are healthy."""
+        return method(self._pool)
+
+    def _start_process(
+        self,
+        worker_id: int,
+        work_path: str,
+        shutdown: Event,
+        shutdown_notify: Event,
+    ):
+        """Start a worker process in the context."""
+        context = mp.get_context(self.start_method)
+        context = cast(Union[SpawnContext, ForkContext], context)
+        coordinator_process = context.Process(
+            target=Coordinator,
+            args=(
+                self.worker,
+                self.max_batch_size,
+                shutdown,
+                shutdown_notify,
+                work_path,
+                self.name,
+                worker_id + 1,
+                self.timeout_sec,
+            ),
+            daemon=True,
+        )
+
+        with env_var_context(self.env, worker_id):
+            coordinator_process.start()
+
+        self._pool[worker_id] = coordinator_process
+
+    def _check(
+        self,
+        work_path: str,
+        shutdown: Event,
+        shutdown_notify: Event,
+        init: bool,
+    ) -> bool:
+        """Check and start the worker process if it has not started yet.
+
+        Args:
+            work_path: path of working directory
+            shutdown: Event of server shutdown
+            shutdown_notify: Event of server will shutdown
+            init: whether the worker is tried to start at the first time
+
+        Returns:
+            Whether the worker is started successfully
+
+        """
+        # for every sequential stage
+        self._pool = [p if self._process_healthy(p) else None for p in self._pool]
+        if self._healthy(all):
+            # this stage is healthy
+            return True
+        if not init and not self._healthy(any):
+            # this stage might contain bugs
+            return False
+
+        need_start_id = [
+            worker_id for worker_id in range(self.num) if self._pool[worker_id] is None
+        ]
+        for worker_id in need_start_id:
+            # for every worker in each stage
+            self._start_process(worker_id, work_path, shutdown, shutdown_notify)
+        return True
+
+    def _validate(self):
+        """Validate arguments of worker runtime."""
+        validate_env(self.env, self.num)
+        assert issubclass(self.worker, Worker), (
+            "worker must be inherited from mosec.Worker"
+        )
+        validate_int_ge(self.num, "worker number")
+        validate_int_ge(self.max_batch_size, "maximum batch size")
+        validate_int_ge(self.max_wait_time, "maximum wait time", 0)
+        validate_float_ge(self.timeout_sec, "forward timeout", 0)
+        assert self.start_method in NEW_PROCESS_METHOD, (
+            f"start method must be one of {NEW_PROCESS_METHOD}"
+        )
+
+
+class PyRuntimeManager:
+    """The manager to control coordinator process."""
+
+    def __init__(self, work_path: str, shutdown: Event, shutdown_notify: Event):
+        """Initialize a coordinator manager.
+
+        Args:
+            work_path: path of working directory
+            shutdown: Event of server shutdown
+            shutdown_notify: Event of server will shutdown
+
+        """
+        self.runtimes: List[Runtime] = []
+
+        self._work_path = work_path
+        self.shutdown = shutdown
+        self.shutdown_notify = shutdown_notify
+
+    @property
+    def worker_count(self) -> int:
+        """Get number of workers."""
+        return len(self.runtimes)
+
+    @property
+    def workers(self) -> List[Type[Worker]]:
+        """Get List of workers."""
+        return [r.worker for r in self.runtimes]
+
+    def append(self, runtime: Runtime):
+        """Sequentially appends workers to the workflow pipeline."""
+        self.runtimes.append(runtime)
+
+    def check_and_start(self, init: bool) -> Union[Runtime, None]:
+        """Check all worker processes and try to start failed ones.
+
+        Args:
+            init: whether the worker is tried to start at the first time
+
+        """
+        for worker_runtime in self.runtimes:
+            if not worker_runtime._check(  # pylint: disable=protected-access
+                self._work_path, self.shutdown, self.shutdown_notify, init
+            ):
+                return worker_runtime
+        return None
+
+
+class RsRuntimeManager:
+    """The manager to control Mosec process."""
+
+    def __init__(self, timeout: int):
+        """Initialize a Mosec manager.
+
+        Args:
+            timeout: service timeout in milliseconds
+
+        """
+        self.process: Optional[subprocess.Popen] = None
+
+        self.timeout = timeout
+
+    def halt(self):
+        """Graceful shutdown."""
+        # terminate controller first and wait for a graceful period
+        if self.process is None:
+            return
+        self.process.terminate()
+        graceful_period = monotonic() + self.timeout / 1000
+        while monotonic() < graceful_period:
+            ctr_exitcode = self.process.poll()
+            if ctr_exitcode is None:
+                sleep(0.1)
+                continue
+            # exited
+            if ctr_exitcode:  # on error
+                logger.error("mosec service halted on error [%d]", ctr_exitcode)
+            else:
+                logger.info("mosec service halted normally [%d]", ctr_exitcode)
+            break
+
+        if monotonic() > graceful_period:
+            logger.error("failed to terminate mosec service, will try to kill it")
+            self.process.kill()
+
+    def start(self, config_path: Path) -> subprocess.Popen:
+        """Start the Mosec process.
+
+        Args:
+            config_path: configuration path of mosec
+
+        """
+        # pylint: disable=consider-using-with
+        self.process = subprocess.Popen([str(get_mosec_path()), config_path])
+        return self.process
---
+++ packages/mosec/mosec-0.9.5-py3-none-musllinux_1_2_i686.whl/mosec/worker.py
@@ -0,0 +1,261 @@
+# Copyright 2022 MOSEC Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""MOSEC worker interface.
+
+This module provides the interface to define a worker with such behaviors:
+
+    1. initialize
+    2. serialize/deserialize data to/from another worker
+    3. serialize/deserialize data to/from the client side
+    4. data processing
+"""
+
+from __future__ import annotations
+
+import abc
+import json
+import pickle
+from typing import TYPE_CHECKING, Any, Dict, Sequence, Tuple
+
+from mosec.errors import DecodingError, EncodingError
+from mosec.utils import ParseTarget
+
+MOSEC_REF_TEMPLATE = "#/components/schemas/{name}"
+
+if TYPE_CHECKING:
+    from queue import SimpleQueue
+    from threading import Semaphore
+
+
+class Worker(abc.ABC):
+    """MOSEC worker interface.
+
+    It provides default IPC (de)serialization methods, stores the worker
+    meta data including its stage and maximum batch size, and leaves the
+    ``forward`` method to be implemented by the users.
+
+    By default, we use `JSON <https://www.json.org/>`__ encoding. But users
+    are free to customize via simply overriding the ``deserialize`` method
+    in the **first** stage (we term it as *ingress* stage) and/or the
+    ``serialize`` method in the **last** stage (we term it as *egress*
+    stage).
+
+    For the encoding customization, there are many choices including
+    `MessagePack <https://msgpack.org/index.html>`__, `Protocol
+    Buffer <https://developers.google.com/protocol-buffers>`__ and many
+    other out-of-the-box protocols. Users can even define their own protocol
+    and use it to manipulate the raw bytes! A naive customization can be
+    found in this :doc:`PyTorch example </examples/pytorch>`.
+    """
+
+    # pylint: disable=no-self-use
+
+    example: Any = None
+    multi_examples: Sequence[Any] = []
+    resp_mime_type = "application/json"
+    _worker_id: int = 0
+    _stage: str = ""
+    _max_batch_size: int = 1
+
+    def __init__(self):
+        """Initialize the worker.
+
+        This method doesn't require the child class to override.
+        """
+        super().__init__()
+
+    def serialize_ipc(self, data: Any) -> bytes:
+        """Define IPC serialization method.
+
+        Args:
+            data: returned data from :py:meth:`forward`
+
+        """
+        return pickle.dumps(data, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def deserialize_ipc(self, data: bytes) -> Any:
+        """Define IPC deserialization method.
+
+        Args:
+            data: input data for :py:meth:`forward`
+
+        """
+        return pickle.loads(data)
+
+    @property
+    def stage(self) -> str:
+        """Return the stage name."""
+        return self._stage
+
+    @stage.setter
+    def stage(self, stage):
+        self._stage = stage
+
+    @property
+    def max_batch_size(self) -> int:
+        """Return the maximum batch size."""
+        return self._max_batch_size
+
+    @max_batch_size.setter
+    def max_batch_size(self, max_batch_size):
+        self._max_batch_size = max_batch_size
+
+    @property
+    def worker_id(self) -> int:
+        """Return the ID of this worker instance.
+
+        This property returns the worker ID in the range of [1, ... , ``num``]
+        (``num`` as configured in
+        :py:meth:`append_worker(num) <mosec.server.Server.append_worker>`)
+        to differentiate workers in the same stage.
+        """
+        return self._worker_id
+
+    @worker_id.setter
+    def worker_id(self, worker_id):
+        self._worker_id = worker_id
+
+    def serialize(self, data: Any) -> bytes:
+        """Serialize the last stage (egress).
+
+        Default response serialization method: JSON.
+
+        Check :py:mod:`mosec.mixin` for more information.
+
+        Arguments:
+            data: the same type as the output of the :py:meth:`forward`
+
+        Returns:
+            the bytes you want to put into the response body
+
+        Raises:
+            EncodingError: if the data cannot be serialized with JSON
+
+        """
+        try:
+            data_bytes = json.dumps(data, indent=2).encode()
+        except Exception as err:
+            raise EncodingError from err
+        return data_bytes
+
+    def deserialize(self, data: bytes) -> Any:
+        """Deserialize the first stage (ingress).
+
+        Default request deserialization method: JSON.
+
+        Check :py:mod:`mosec.mixin` for more information.
+
+        Arguments:
+            data: the raw bytes extracted from the request body
+
+        Returns:
+            the same type as the input of the :py:meth:`forward`
+
+        Raises:
+            DecodingError: if the data cannot be deserialized with JSON
+
+        """
+        try:
+            data_json = json.loads(data) if data else {}
+        except Exception as err:
+            raise DecodingError from err
+        return data_json
+
+    @abc.abstractmethod
+    def forward(self, data: Any) -> Any:
+        """Model inference, data processing or computation logic.
+
+        Args:
+            data: input data to be processed
+
+        **Must be overridden** by the subclass.
+
+        If any code in this :py:meth:`forward` needs to access other resources (e.g.
+        a model, a memory cache, etc.), the user should initialize these resources
+        as attributes of the class in the :py:class:`__init__ <mosec.worker.Worker>`.
+
+        .. note::
+
+            For a stage that enables dynamic batching, please return the results
+            that have the same length and the same order of the input data.
+
+        .. note::
+
+            - for a single-stage worker, data will go through
+                ``<deserialize> -> <forward> -> <serialize>``
+
+            - for a multi-stage worker that is neither `ingress` not `egress`, data
+                will go through ``<deserialize_ipc> -> <forward> -> <serialize_ipc>``
+
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def get_forward_json_schema(
+        cls,
+        target: ParseTarget,
+        ref_template: str,  # pylint: disable=unused-argument
+    ) -> Tuple[Dict[str, Any], Dict[str, Any]]:
+        """Retrieve the JSON schema for the `forward` method of the class.
+
+        Args:
+            cls : The class object.
+            target : The target variable to parse the schema for.
+            ref_template : A template to use when generating ``"$ref"`` fields.
+
+        Returns:
+            A tuple containing the schema and the component schemas.
+
+        The :py:meth:`get_forward_json_schema` method is a class method that returns the
+        JSON schema for the :py:meth:`forward` method of the :py:class:`cls` class.
+        It takes a :py:obj:`target` param specifying the target to parse the schema for.
+
+        The returned value is a tuple containing the schema and the component schema.
+
+        .. note::
+
+            Developer must implement this function to retrieve the JSON schema
+            to enable openapi spec.
+
+        .. note::
+
+            The :py:const:`MOSEC_REF_TEMPLATE` constant should be used as a reference
+            template according to openapi standards.
+
+        """
+        return {}, {}
+
+
+class SSEWorker(Worker):
+    """MOSEC worker with Server-Sent Events (SSE) support."""
+
+    _stream_queue: SimpleQueue
+    _stream_semaphore: Semaphore
+    resp_mime_type = "text/event-stream"
+
+    def send_stream_event(self, text: str, index: int = 0):
+        """Send a stream event to the client.
+
+        Args:
+            text: the text to be sent, needs to be UTF-8 compatible
+            index: the index of the stream event. For the single request, this will
+                always be 0. For dynamic batch request, this should be the index of
+                the request in this batch.
+
+        """
+        if self._stream_queue is None or self._stream_semaphore is None:
+            raise RuntimeError("the worker stream or semaphore is not initialized")
+        self._stream_semaphore.acquire()
+        self._stream_queue.put((text, index))
