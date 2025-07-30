b90b9985287b48f0d2e78e1a660d28fa82fd97fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:32 +0000

    Add package exifdata/0.5.4/exifdata-0.5.4.tar.gz

---
+++ packages/exifdata/exifdata-0.5.4.tar.gz/exifdata-0.5.4/source/exifdata/framework/__init__.py
@@ -0,0 +1,7 @@
+from exifdata.framework.namespace import Namespace
+from exifdata.framework.groupspace import Groupspace
+from exifdata.framework.structure import Structure
+from exifdata.framework.type import Type
+from exifdata.framework.field import Field
+from exifdata.framework.value import Value
+from exifdata.framework.metadata import Metadata
