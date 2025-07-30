3ed6be460bcf16641a82afa7c0f5c730f849ef93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:28 +0000

    Add package ingestify/0.7.0/ingestify-0.7.0-py3-none-any.whl

---
+++ packages/ingestify/ingestify-0.7.0-py3-none-any.whl/ingestify/__init__.py
@@ -0,0 +1,11 @@
+# detect if we are imported from the setup procedure (borrowed from numpy code)
+try:
+    __INGESTIFY_SETUP__
+except NameError:
+    __INGESTIFY_SETUP__ = False
+
+if not __INGESTIFY_SETUP__:
+    from .infra import retrieve_http
+    from .source_base import Source, DatasetResource
+
+__version__ = "0.7.0"
