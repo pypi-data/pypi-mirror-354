785ca2580f20649415f5a6667114e9356537f28b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:57 +0000

    Add package hzzzwl-agriculture-mcp-x/0.2.7/hzzzwl_agriculture_mcp_x-0.2.7-py3-none-any.whl

---
+++ packages/hzzzwl-agriculture-mcp-x/hzzzwl_agriculture_mcp_x-0.2.7-py3-none-any.whl/hzzzwl_agriculture_mcp_x/__init__.py
@@ -0,0 +1,13 @@
+import asyncio
+
+#from .server import main
+from .server import main as async_main
+
+__version__ = "0.2.7"
+
+def main():
+    """Main entry point for the package."""
+    asyncio.run(async_main())
+
+if __name__ == "__main__":
+    main()
