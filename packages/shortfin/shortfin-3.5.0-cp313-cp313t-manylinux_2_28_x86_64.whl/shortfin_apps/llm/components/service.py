6814cf79a3f134600ee626ffc3490705e94ebba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:07 +0000

    Add package shortfin/3.5.0/shortfin-3.5.0-cp313-cp313t-manylinux_2_28_x86_64.whl

---
+++ packages/shortfin/shortfin-3.5.0-cp313-cp313t-manylinux_2_28_x86_64.whl/shortfin_apps/llm/components/service.py
@@ -0,0 +1,189 @@
+# Copyright 2024 Advanced Micro Devices, Inc.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import logging
+
+from dataclasses import dataclass
+from typing import List
+from threading import Lock
+import shortfin as sf
+
+
+from .batcher import PrefillBatcherProcess, DecodeBatcherProcess
+from .config_struct import ModelParams, ServerParams
+from .kvcache.base_attention_cache import (
+    BasePagedAttentionCache,
+)
+from .kvcache.trie_attention_cache import TriePagedAttentionCache
+from .kvcache.page_pool import PagePoolConfig, PagePool
+from .manager import LlmSystemManager
+from .service_debug_dumper import SERVICE_DEBUG_DUMPER
+from .tokenizer import Tokenizer
+from .token_selection_strategy import is_multi_response
+
+from ...utils import GenerateService
+from .fiber_pool import FiberPool
+
+logger = logging.getLogger(__name__)
+
+
+class LlmGenerateService(GenerateService):
+    """Top level service interface for generating text against a model."""
+
+    inference_program: sf.Program
+    prefill_functions: dict[int, sf.ProgramFunction]
+    decode_functions: dict[int, sf.ProgramFunction]
+
+    def __init__(
+        self,
+        *,
+        name: str,
+        sysman: LlmSystemManager,
+        tokenizer: Tokenizer,
+        model_params: ModelParams,
+        server_params: "ServerParams",
+        program_isolation: str = "per_call",
+        max_queue_size: int = 3,  # Maximum number of requests in queue
+    ):
+        super().__init__(sysman)
+        self.name = name
+        self.tokenizer = tokenizer
+        self.model_params = model_params
+        self.server_params = server_params
+        self.max_queue_size = max_queue_size
+        self.current_queue_size = 0
+        self.main_fiber_pool = FiberPool(
+            self.sysman, self.max_queue_size, resizable=True
+        )
+
+        self.set_isolation(program_isolation)
+        self._initialize_worker_and_fiber()
+        self._initialize_queues()
+        self._initialize_page_cache()
+        self._lock = Lock()
+
+    def _initialize_queues(self):
+        """Initialize request and response queues"""
+        if self.model_params.decode_batch_sizes:
+            self.max_queue_size = max(self.model_params.decode_batch_sizes)
+            logger.debug(f"Max queue size: {self.max_queue_size}")
+
+    def add_to_queue(self, num_beams: int) -> bool:
+        """Try to add a request to the queue. Returns True if successful, False if queue is full."""
+        with self._lock:
+            if self.current_queue_size >= self.max_queue_size:
+                return False
+            self.current_queue_size += num_beams
+            logger.debug(f"Adding to queue, queue size: {self.current_queue_size}")
+            return True
+
+    def remove_from_queue(self, num_beams: int):
+        """Remove a request from the queue."""
+        with self._lock:
+            if self.current_queue_size >= num_beams:
+                self.current_queue_size -= num_beams
+                logger.debug(
+                    f"Removing from queue, queue size: {self.current_queue_size}"
+                )
+
+    def _initialize_worker_and_fiber(self):
+        num_workers = self.server_params.workers
+        fibers_per_worker = self.server_params.fibers_per_worker
+
+        logger.info(
+            f"Creating {num_workers} workers, with {fibers_per_worker} fibers per worker..."
+        )
+
+        self.main_worker = self.sysman.ls.create_worker(f"{self.name}-inference-main-0")
+        self.main_fiber = self.sysman.ls.create_fiber(self.main_worker)
+
+        self.prefill_worker = self.sysman.ls.create_worker(
+            f"{self.name}-inference-prefill-0"
+        )
+        self.prefill_fiber = self.sysman.ls.create_fiber(self.prefill_worker)
+
+        self.decode_worker = self.sysman.ls.create_worker(
+            f"{self.name}-inference-decode-0"
+        )
+        self.decode_fiber = self.sysman.ls.create_fiber(self.decode_worker)
+
+        self.devices = self.prefill_fiber.devices_dict.values()
+
+    def _initialize_page_cache(self):
+        """Initialize page pool and attention cache."""
+        page_pool_config = PagePoolConfig(
+            dtype=self.model_params.paged_kv_cache.kv_cache_dtype,
+            alloc_page_count=self.model_params.paged_kv_cache.device_block_count,
+            paged_kv_block_size_elements=self.model_params.paged_kv_block_size_elements,
+        )
+        page_pool = PagePool(devices=self.devices, config=page_pool_config)
+
+        if self.server_params.prefix_sharing_algorithm == "trie":
+            self.page_cache = TriePagedAttentionCache(
+                page_pool=page_pool,
+                tokens_per_page=self.model_params.paged_kv_cache.block_seq_stride,
+            )
+        elif self.server_params.prefix_sharing_algorithm == "none":
+            self.page_cache = BasePagedAttentionCache(
+                page_pool=page_pool,
+                tokens_per_page=self.model_params.paged_kv_cache.block_seq_stride,
+                use_ref_counts=is_multi_response(
+                    self.server_params.decode_config,
+                ),
+            )
+        else:
+            raise ValueError(
+                f"Unknown prefix_sharing_algorithm {self.server_params.prefix_sharing_algorithm}. Currently only supporting 'trie' and 'none'."
+            )
+
+    def start(self):
+        component_modules = self.initialize_program_modules("main")
+        self.inference_program = self.create_program(
+            modules=component_modules, devices=self.sysman.ls.devices
+        )
+        self.initialize_function_references()
+
+        self.prefill_batcher = PrefillBatcherProcess(
+            self.prefill_fiber,
+            self.page_cache,
+            self.model_params,
+            self.prefill_functions,
+            self.prog_isolation,
+        )
+
+        self.decode_batcher = DecodeBatcherProcess(
+            self.decode_fiber,
+            self.page_cache,
+            self.model_params,
+            self.decode_functions,
+            self.prog_isolation,
+        )
+
+        self.prefill_batcher.launch()
+        self.decode_batcher.launch()
+
+    def initialize_function_references(self):
+        self.prefill_functions = {}
+        for bs in self.model_params.prefill_batch_sizes:
+            self.prefill_functions[bs] = self.inference_program[
+                f"{self.model_params.module_name}.prefill_bs{bs}"
+            ]
+        # Resolve decode entrypoints.
+        self.decode_functions = {}
+        for bs in self.model_params.decode_batch_sizes:
+            self.decode_functions[bs] = self.inference_program[
+                f"{self.model_params.module_name}.decode_bs{bs}"
+            ]
+
+    def __repr__(self):
+        return (
+            f"ServiceManager(\n"
+            f"  model_params={self.model_params}\n"
+            f"  server_params={self.server_params}\n"
+            f"  inference_modules={self.inference_modules}\n"
+            f"  page_cache={self.page_cache}\n"
+            f")"
+        )
---
+++ packages/shortfin/shortfin-3.5.0-cp313-cp313t-manylinux_2_28_x86_64.whl/shortfin_apps/llm/components/token_selection_strategy/beam_group.py
@@ -0,0 +1,260 @@
+# Copyright 2025 Advanced Micro Devices, Inc.
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+import logging
+import numpy as np
+from typing import Union
+
+import shortfin.array as sfnp
+
+from abc import ABC, abstractmethod
+from asyncio import gather
+from dataclasses import dataclass, field
+from typing import Callable, Dict, List, Set
+from uuid import uuid4
+
+from .base_token_selection_strategy import DecodeConfig
+from .config import LogitsNormalization
+from .sampler import Sampler
+from ..messages import LlmInferenceExecRequest
+
+logger = logging.getLogger(__name__)
+
+
+# TODO: Define `top_p` function in base class when enabled in
+# shortfin.
+@dataclass
+class Beam(ABC):
+    exec_req: LlmInferenceExecRequest
+
+    decode_config: DecodeConfig
+
+    sampler: Sampler = field(default_factory=Sampler)
+
+    score: float = 0.0
+    accumulated_normalization: float = 0.0
+    last_token: int | None = None
+
+    def apply_temperature(self, logits: np.array) -> np.array:
+        """Apply temperature to the logits of a decode invocation.
+
+        Args:
+            temperature (float): Value to use for `temperature`.
+        """
+        if self.decode_config.temperature == 1.0:
+            return logits
+        return np.divide(logits, self.decode_config.temperature)
+
+    def _softmax(self, logits: Union[np.array, sfnp.device_array]) -> np.array:
+        if isinstance(logits, sfnp.device_array):
+            logits = np.array(logits)
+
+        x_max = np.max(logits)
+        e_x = np.exp(logits - x_max)
+        return e_x / np.sum(e_x)
+
+    def _log_softmax(self, logits: Union[np.array, sfnp.device_array]) -> np.array:
+        if isinstance(logits, sfnp.device_array):
+            logits = np.array(logits)
+
+        c = logits.max()
+        shifted_logits = logits - c
+        sumexp = np.log(np.exp(shifted_logits).sum())
+        return shifted_logits - sumexp
+
+    def convert_logits_normalization(
+        self,
+        current: LogitsNormalization,
+        target: LogitsNormalization,
+        logits: np.array,
+        **kwargs,
+    ) -> np.array:
+        logits_conversion_map = {
+            LogitsNormalization.NONE: {
+                LogitsNormalization.LOG_SOFTMAX: self._log_softmax,
+                LogitsNormalization.SOFTMAX: self._softmax,
+                LogitsNormalization.NONE: lambda logits: logits,
+            },
+            LogitsNormalization.SOFTMAX: {
+                LogitsNormalization.LOG_SOFTMAX: np.log,
+                LogitsNormalization.SOFTMAX: lambda logits: logits,
+            },
+            LogitsNormalization.LOG_SOFTMAX: {
+                LogitsNormalization.SOFTMAX: np.exp,
+                LogitsNormalization.LOG_SOFTMAX: lambda logits: logits,
+            },
+        }
+
+        target_conversions = logits_conversion_map.get(current)
+        if target_conversions is None:
+            raise KeyError(f"Cannot convert current normalization: {current}")
+
+        conversion_function = target_conversions.get(target)
+        if conversion_function is None:
+            raise KeyError(f"Cannot convert {current} to {target}")
+
+        if kwargs:
+            converted_logits = conversion_function(logits, **kwargs)
+        else:
+            converted_logits = conversion_function(logits)
+
+        return converted_logits
+
+    @abstractmethod
+    def sample_logits(self):
+        """Define how to sample and select tokens for a give `Beam`"""
+        pass
+
+    def _to_softmax(
+        self,
+        values: np.array,
+        logits_normalization: LogitsNormalization,
+    ):
+        probs = self.convert_logits_normalization(
+            logits_normalization,
+            LogitsNormalization.SOFTMAX,
+            values,
+        )
+
+        return probs
+
+    def _sample_logits_top_k(
+        self,
+        logits: np.array,
+        indices: Union[np.array, None],
+        top_k: int,
+        num_selections: int,
+    ):
+        tokens, values = self.sampler.select_top_k(logits, indices, -top_k)
+
+        probs = self._to_softmax(
+            values,
+            self.decode_config.logits_normalization,
+        )
+
+        if indices is None:
+            sorted_order = np.argsort(probs)[::-1]
+            tokens = tokens[sorted_order]
+            probs = probs[sorted_order]
+
+        return self.sampler.sample_top_k(
+            tokens=tokens,
+            probs=probs,
+            k=num_selections,
+        )
+
+    def _sample_logits_top_p(
+        self, tokens, probs, top_p, num_selections, return_probs: bool = False
+    ):
+        return self.sampler.sample_top_p(
+            tokens=tokens,
+            probs=probs,
+            p=top_p,
+            k=num_selections,
+            return_probs=return_probs,
+        )
+
+    @abstractmethod
+    def update_score(self, value: float):
+        """Update the score of a `beam`.
+
+        Args:
+            value (float): Value to update the score with.
+        """
+        pass
+
+    @abstractmethod
+    def update_exec_req(self):
+        """Update an `LlmInferenceExecRequest`, after a decode loop"""
+        pass
+
+    @abstractmethod
+    def normalize_score(self, value: float):
+        """Normalize the score of a `beam`.
+
+        Args:
+            value (float): Value to normalize the score with.
+        """
+        pass
+
+    @abstractmethod
+    def update_final_score(self):
+        """Define a `final_score` for a given beam, if applicable."""
+        pass
+
+
+class BeamGroup:
+    def __init__(
+        self,
+        eos_token_id: int,
+        num_beams: int,
+        beams: List[Beam],
+        selection_callback: Callable[
+            [List[Beam], List[Beam]],
+            List[Beam],
+        ],
+    ):
+        self.beam_group_id = str(uuid4())
+        self.eos_token_id = eos_token_id
+        self.num_beams = num_beams
+        self.active_beams = beams
+        self.selection_callback = selection_callback
+        self.completed_beams: List[Beam] = []
+
+    @property
+    def active_beam_count(self):
+        return len(self.active_beams)
+
+    async def wait(self):
+        done_signals = [beam.exec_req.done for beam in self.active_beams]
+        return await gather(*done_signals)
+
+    def process_beams(self):
+        beam_selections = self.selection_callback(
+            self.active_beams, self.completed_beams
+        )
+        visited_reqs: Dict[str, LlmInferenceExecRequest] = {}
+        active_beams: List[Beam] = []
+        active_reqs: Set[LlmInferenceExecRequest] = set()
+        completed_beams: List[Beam] = []
+        completed_reqs: Set[LlmInferenceExecRequest] = set()
+
+        for i in range(len(beam_selections)):
+            beam = beam_selections[i]
+            new_req, token = beam.exec_req, beam.last_token
+
+            if new_req.instance_id in visited_reqs:
+                visited_req = visited_reqs[new_req.instance_id]
+                new_req = LlmInferenceExecRequest.copy_exec_request(visited_req)
+                beam.exec_req = new_req
+
+            visited_reqs[new_req.instance_id] = new_req
+            if token == self.eos_token_id:
+                completed_beams.append(beam)
+                completed_reqs.add(new_req)
+            else:
+                active_beams.append(beam)
+                active_reqs.add(new_req)
+
+        for beam in completed_beams + active_beams:
+            beam.update_exec_req()
+            if beam.exec_req in completed_reqs:
+                beam.exec_req.free_cache_pages()
+
+        # Free cache pages of reqs we don't need anymore
+        for beam in self.active_beams:
+            if beam.exec_req not in active_reqs and beam.exec_req not in completed_reqs:
+                beam.exec_req.free_cache_pages()
+
+        self.active_beams = active_beams
+        self.completed_beams.extend(completed_beams)
+
+    def clean_up(self):
+        logger.debug(f"Cleaning up {self.beam_group_id}...")
+
+        # Ensure all requests have freed their cache pages
+        for beam in self.active_beams + self.completed_beams:
+            beam.exec_req.free_cache_pages()
